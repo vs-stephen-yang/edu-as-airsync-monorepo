7bad70319a393f27cf21d5d6cf213a7096f78ae9
Loren Chang <loren.chang@tw.viewsonic.com>
2022-05-10 21:17:15 +0800

    Implement home screen (main info, title)

--- apps/receiver/lib/screens/home.dart
+++ apps/receiver/lib/screens/home.dart
@@ -1,11 +1,9 @@
-import 'package:display_flutter/app_instance_create.dart';
-
-import 'package:display_flutter/blocs/display_code/display_code_bloc.dart';
 import 'package:display_flutter/native_view/webrtc.dart';
-import 'package:display_flutter/settings/app_config.dart';
+import 'package:display_flutter/widgets/bottom_bar.dart';
+import 'package:display_flutter/widgets/main_info.dart';
+import 'package:display_flutter/widgets/tittle_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,100 +14,59 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late WebRTCNativeViewController controller;
-  late DisplayCodeBloc _displayCodeBloc;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _displayCodeBloc = DisplayCodeBloc(
-        AppConfig.of(context)!.settings.apiGateway,
-        AppInstanceCreate().displayInstanceID,
-        AppConfig.of(context)!.appVersion);
-    if (_displayCodeBloc.state is DisplayCodeInitial) {
-      _displayCodeBloc.add(GetDisplayCode());
-    }
-  }
+  bool viewCreated = false;
 
   @override
   Widget build(BuildContext context) {
-    AppConfig? appConfig = AppConfig.of(context);
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: BlocProvider(
-          create: (context) => _displayCodeBloc,
-          child: BlocBuilder<DisplayCodeBloc, DisplayCodeState>(
-            builder: (context, state) {
-              if (state is DisplayCodeSuccess) {
-                controller.channel.invokeMethod("connectControlSocket", <String, String>{
-                  'id': AppInstanceCreate().instanceID,
-                  'displayCode': _displayCodeBloc.displayCode,
-                  'token': _displayCodeBloc.token,
-                  'name': _displayCodeBloc.name,
-                });
-              }
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: WebRTCNativeView(
-                        onWebRTCNativeViewCreatedCallback:
-                            _webRTCNativeViewCreatedCallback,
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        appConfig?.settings.mainDisplayUrl ?? ' ',
-                        style:
-                            const TextStyle(color: Colors.blue, fontSize: 30),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        'Display Code: ${_displayCodeBloc.displayCode}',
-                        style:
-                            const TextStyle(color: Colors.blue, fontSize: 30),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        'OTP: ${_displayCodeBloc.otp}', //$_otpCode'
-                        style:
-                            const TextStyle(color: Colors.blue, fontSize: 30),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        'version: ${appConfig?.appVersion ?? ' '}',
-                        style:
-                            const TextStyle(color: Colors.blue, fontSize: 30),
-                      ),
-                    ),
-                    FittedBox(
-                      child: Text(
-                        'InstanceId: ${AppInstanceCreate().instanceID}\n Registered: ${AppInstanceCreate().isRegistered}',
-                        style:
-                            const TextStyle(color: Colors.blue, fontSize: 30),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ));
+      body: ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: Stack(
+          children: <Widget>[
+            WebRTCNativeView(
+              onWebRTCNativeViewCreatedCallback:
+                  _webRTCNativeViewCreatedCallback,
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints.expand(),
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  const Positioned(
+                    left: 0,
+                    top: 0,
+                    right: 0,
+                    child: TitleBar(),
+                  ),
+                  Positioned(
+                      child: viewCreated
+                          ? MainInfo(
+                              controller: controller,
+                              isEnrolled: false, // todo: Moderator mode switch
+                            )
+                          : const Text(' ')),
+                  const Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: BottomBar(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   _webRTCNativeViewCreatedCallback(WebRTCNativeViewController controller) {
     this.controller = controller;
+    setState(() {
+      // todo: Temporary solution: wait move control socket mechanism to Dart level.
+      viewCreated = true;
+    });
     controller.channel.setMethodCallHandler((MethodCall call) async {
-      setState(() {
-      });
       return;
     });
   }
---
+++ apps/receiver/lib/widgets/tittle_bar.dart
@@ -0,0 +1,42 @@
+import 'package:display_flutter/settings/app_config.dart';
+import 'package:display_flutter/widgets/text_clock.dart';
+import 'package:flutter/material.dart';
+
+class TitleBar extends StatelessWidget {
+  const TitleBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    AppConfig? appConfig = AppConfig.of(context);
+    return Container(
+      padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Ver ${appConfig?.appVersion ?? ' '}',
+                style: const TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 0.5),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const TextClock(),
+            ],
+          ),
+          Text(
+            appConfig?.settings.mainDisplayUrl ?? ' ',
+            style: const TextStyle(
+              color: Color(0xFFF7F7F7),
+              fontSize: 36,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
