d4e3d5879623b0c97db9cb3793a85ca9ac2ca3e7
Loren Chang <loren.chang@tw.viewsonic.com>
2022-05-13 18:47:08 +0800

    Implement Split screen layout. Refactor stream_function.dart icon and add split screen feature.

--- apps/receiver/lib/screens/home.dart
+++ apps/receiver/lib/screens/home.dart
@@ -1,8 +1,8 @@
 import 'package:display_flutter/app_instance_create.dart';
-
 import 'package:display_flutter/blocs/display_code/display_code_bloc.dart';
 import 'package:display_flutter/model/connect_timer.dart';
 import 'package:display_flutter/native_view/webrtc.dart';
+import 'package:display_flutter/screens/split_screen.dart';
 import 'package:display_flutter/widgets/bottom_bar.dart';
 import 'package:display_flutter/widgets/stream_function.dart';
 import 'package:display_flutter/widgets/main_info.dart';
@@ -11,7 +11,6 @@ import 'package:display_flutter/widgets/vbs_ota.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -24,16 +23,107 @@ class _HomeState extends State<Home> {
   late WebRTCNativeViewController controller;
   bool viewCreated = false;
 
+  double _fullWidth = 0, _fullHeight = 0, _halfWidth = 0, _halfHeight = 0;
+  final List<Widget> _webRtcWidget = <Widget>[];
+  final List<bool> _isSelectedList = List.filled(4, false, growable: false);
+
+  @override
+  void initState() {
+    super.initState();
+    _webRtcWidget.add(WebRTCNativeView(
+      onWebRTCNativeViewCreatedCallback: _webRTCNativeViewCreatedCallback,
+    ));
+    // todo: design other native view.
+    _webRtcWidget.add(Container(color: Colors.red));
+    _webRtcWidget.add(Container(color: Colors.green));
+    _webRtcWidget.add(Container(color: Colors.blue));
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    _fullWidth = size.width;
+    _fullHeight = size.height;
+    _halfWidth = size.width / 2;
+    _halfHeight = size.height / 2;
     return Scaffold(
       body: ConstrainedBox(
         constraints: const BoxConstraints.expand(),
         child: Stack(
           children: <Widget>[
-            WebRTCNativeView(
-              onWebRTCNativeViewCreatedCallback:
-                  _webRTCNativeViewCreatedCallback,
+            ValueListenableBuilder(
+              valueListenable: SplitScreen.splitScreenEnabled,
+              builder: (BuildContext context, bool value, Widget? child) {
+                _updateSizeForSelected(int selection) {
+                  setState(() {
+                    if (value) {
+                      for (int i = 0; i < _isSelectedList.length; i++) {
+                        if (i == selection) {
+                          _isSelectedList[i] = !_isSelectedList[i];
+                        } else {
+                          _isSelectedList[i] = false;
+                        }
+                      }
+                    } else {
+                      _isSelectedList.fillRange(
+                          0, _isSelectedList.length, false);
+                      _isSelectedList[0] = true;
+                    }
+                  });
+                }
+
+                List<Widget> webrtcWidgets =
+                    List.generate(value ? 4 : 1, (index) {
+                  double? left, top, right, bottom;
+                  if (index == 1) {
+                    right = 0;
+                    top = 0;
+                  } else if (index == 2) {
+                    left = 0;
+                    bottom = 0;
+                  } else if (index == 3) {
+                    right = 0;
+                    bottom = 0;
+                  } else {
+                    // index 0 and default.
+                    left = 0;
+                    top = 0;
+                  }
+
+                  return Positioned(
+                    left: left,
+                    top: top,
+                    right: right,
+                    bottom: bottom,
+                    child: GestureDetector(
+                      onDoubleTap: () => _updateSizeForSelected(index),
+                      child: AnimatedContainer(
+                        width: _isSelectedList[index]
+                            ? _fullWidth
+                            : !_isSelectedList.contains(true)
+                                ? _halfWidth
+                                : 0,
+                        height: _isSelectedList[index]
+                            ? _fullHeight
+                            : !_isSelectedList.contains(true)
+                                ? _halfHeight
+                                : 0,
+                        alignment: _isSelectedList[index]
+                            ? Alignment.center
+                            : Alignment.topLeft,
+                        curve: Curves.linear,
+                        duration:
+                            Duration(seconds: _isSelectedList[index] ? 1 : 0),
+                        child: _webRtcWidget[index],
+                      ),
+                    ),
+                  );
+                });
+
+                return Stack(
+                  children: webrtcWidgets,
+                );
+              },
             ),
             ConstrainedBox(
               constraints: const BoxConstraints.expand(),
