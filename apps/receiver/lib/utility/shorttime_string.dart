0308971b2370bbcc6cd17d66fdbf86c0b4ffa115
Zoey Chen <royinung@gmail.com>
2022-05-20 18:41:55 +0800

    Merge random_string, shorttime_string and splitQueryString

--- apps/receiver/lib/utility/shorttime_string.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-getShortTimeString(String keyStr, String data) {
-  DateFormat inputFormat = DateFormat("MM-dd-HH-mm-ss", "en_US");
-  return inputFormat.format(DateTime.now());
-}
