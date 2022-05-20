904f541f3526666c8d430b43c205b5c0390f45b0
Zoey Chen <royinung@gmail.com>
2022-05-20 19:11:37 +0800

    Add the moderator's messagebox and button

--- apps/receiver/lib/app_colors.dart
+++ apps/receiver/lib/app_colors.dart
@@ -12,4 +12,18 @@ class AppColors {
   static const Color primary_grey_dark = Color.fromRGBO(0x38, 0x38, 0x38, 1.0);
 
   static const Color neutral1 = Color.fromRGBO(0x7E, 0x7E, 0x7E, 1.0);
+
+  static const Color neutral4 = Color.fromRGBO(0x38, 0x38, 0x38, 1.0);
+
+  static const Color semantic1 = Color.fromRGBO(0x2E, 0x7D, 0x32, 1.0);
+
+  static const Color semantic2 = Color.fromRGBO(0xD3, 0x2F, 0x2F, 1.0);
+
+  static const Color primary_red = Color.fromRGBO(0xFD, 0x1A, 0x2D, 1.0);
+
+  static const Color toggle_bg = Color.fromRGBO(0x38, 0x38, 0x38, 1.0);
+
+  static const Color primary_blue = Color.fromRGBO(0x29, 0x79, 0xFF, 1.0);
+
+  static const Color primary_dialog = Color.fromRGBO(0x88, 0x88, 0x88, 1.0);
 }
