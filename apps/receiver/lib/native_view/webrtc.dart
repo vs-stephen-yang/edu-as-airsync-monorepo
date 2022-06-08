00a97c68a383431b3aac4dfe4a377d54d0ed7078
Loren Chang <loren.chang@tw.viewsonic.com>
2022-06-08 20:27:16 +0800

    Refactor webrtc.dart to implement hybrid view support (initExpensiveAndroidView)

--- apps/receiver/lib/native_view/webrtc.dart
+++ apps/receiver/lib/native_view/webrtc.dart
@@ -1,36 +1,64 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 typedef WebRTCNativeViewCreatedCallback = void Function(
     WebRTCNativeViewController webRTCNativeViewController);
 
-class WebRTCNativeView extends StatefulWidget {
+class WebRTCNativeView extends StatelessWidget {
   const WebRTCNativeView(
-      {Key? key, required this.onWebRTCNativeViewCreatedCallback})
+      {Key? key,
+      required this.useHybrid,
+      required this.onWebRTCNativeViewCreatedCallback})
       : super(key: key);
 
+  final bool useHybrid;
   final WebRTCNativeViewCreatedCallback onWebRTCNativeViewCreatedCallback;
 
   @override
-  State<StatefulWidget> createState() => _WebRTCNativeViewState();
-}
-
-class _WebRTCNativeViewState extends State<WebRTCNativeView> {
-  @override
   Widget build(BuildContext context) {
+    // This is used in the platform side to register the view.
+    const String viewType = 'com.mvbcast.crosswalk/webrtc_native_view';
     if (defaultTargetPlatform == TargetPlatform.android) {
-      return AndroidView(
-        viewType: 'com.mvbcast.crosswalk/webrtc_native_view',
-        onPlatformViewCreated: _onPlatformViewCreated,
-      );
+      if (useHybrid) {
+        return PlatformViewLink(
+          viewType: viewType,
+          surfaceFactory:
+              (BuildContext context, PlatformViewController controller) {
+            return AndroidViewSurface(
+              controller: controller as AndroidViewController,
+              gestureRecognizers: const <
+                  Factory<OneSequenceGestureRecognizer>>{},
+              hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+            );
+          },
+          onCreatePlatformView: (PlatformViewCreationParams params) {
+            ExpensiveAndroidViewController viewController =
+                PlatformViewsService.initExpensiveAndroidView(
+                    id: params.id,
+                    viewType: viewType,
+                    layoutDirection: TextDirection.ltr);
+            viewController.addOnPlatformViewCreatedListener((id) {
+              onWebRTCNativeViewCreatedCallback(
+                  WebRTCNativeViewController._(id));
+            });
+            viewController.create();
+            return viewController;
+          },
+        );
+      } else {
+        return AndroidView(
+          viewType: viewType,
+          onPlatformViewCreated: (id) {
+            onWebRTCNativeViewCreatedCallback(WebRTCNativeViewController._(id));
+          },
+        );
+      }
     }
     return Text(
         '$defaultTargetPlatform is not yet supported by the webrtc_native_view plugin');
-  }
-
-  void _onPlatformViewCreated(int id) {
-    widget.onWebRTCNativeViewCreatedCallback(WebRTCNativeViewController._(id));
   }
 }
 
