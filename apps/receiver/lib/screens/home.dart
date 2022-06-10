f26000cea53fc6ddab0f386f997fdf48cdb96586
Loren Chang <loren.chang@tw.viewsonic.com>
2022-06-10 20:20:08 +0800

    Revamp home UI structure

--- apps/receiver/lib/screens/home.dart
+++ apps/receiver/lib/screens/home.dart
@@ -1,24 +1,49 @@
 import 'package:display_flutter/app_instance_create.dart';
+import 'package:display_flutter/blocs/display_code/display_code_bloc.dart';
 import 'package:display_flutter/model/control_socket.dart';
 import 'package:display_flutter/native_view/webrtc.dart';
 import 'package:display_flutter/screens/split_screen.dart';
+import 'package:display_flutter/settings/app_config.dart';
 import 'package:display_flutter/widgets/bottom_bar.dart';
 import 'package:display_flutter/widgets/main_info.dart';
 import 'package:display_flutter/widgets/stream_function.dart';
 import 'package:display_flutter/widgets/tittle_bar.dart';
 import 'package:display_flutter/widgets/vbs_ota.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+  static ValueNotifier<bool> showTitleBottomBar = ValueNotifier(true);
+  static ValueNotifier<bool> showMainInfo = ValueNotifier(true);
+  static ValueNotifier<bool> showStreamFunction = ValueNotifier(true);
 
   @override
   State<StatefulWidget> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  late DisplayCodeBloc _displayCodeBloc;
   double _fullWidth = 0, _fullHeight = 0, _halfWidth = 0, _halfHeight = 0;
   final List<bool> _isSelectedList = List.filled(4, false, growable: false);
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _displayCodeBloc = DisplayCodeBloc(
+        AppConfig.of(context)!.settings.apiGateway,
+        AppInstanceCreate().displayInstanceID,
+        AppConfig.of(context)!.appVersion);
+    if (_displayCodeBloc.state is DisplayCodeInitial) {
+      _displayCodeBloc.add(GetDisplayCode());
+    }
+  }
+
+  @override
+  void dispose() {
+    ControlSocket().disconnectControlSocket();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +56,7 @@ class _HomeState extends State<Home> {
       body: ConstrainedBox(
         constraints: const BoxConstraints.expand(),
         child: Stack(
+          alignment: Alignment.center,
           children: <Widget>[
             ValueListenableBuilder(
               valueListenable: SplitScreen.splitScreenEnabled,
@@ -118,37 +144,54 @@ class _HomeState extends State<Home> {
                 );
               },
             ),
-            ConstrainedBox(
-              constraints: const BoxConstraints.expand(),
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  const Positioned(
-                    left: 0,
-                    top: 0,
-                    right: 0,
-                    child: TitleBar(),
+            ValueListenableBuilder(
+              valueListenable: Home.showTitleBottomBar,
+              builder: (BuildContext context, bool value, Widget? child) {
+                return Visibility(
+                  visible: value,
+                  child: Stack(
+                    children: <Widget>[
+                      const Positioned(
+                          left: 0, top: 0, right: 0, child: TitleBar()),
+                      const Positioned(
+                          left: 0, right: 0, bottom: 0, child: BottomBar()),
+                      Visibility(
+                        visible: AppInstanceCreate().isInstalledInVBS100,
+                        child: const Positioned(
+                            left: 0, right: 0, bottom: 0, child: VbsOTA()),
+                      ),
+                    ],
                   ),
-                  const Positioned(child: MainInfo()),
-                  const Positioned(
-                      left: 20, bottom: 140, child: StreamFunction()),
-                  const Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    child: BottomBar(),
-                  ),
-                  Visibility(
-                    visible: AppInstanceCreate().isInstalledInVBS100,
-                    child: const Positioned(
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: VbsOTA(),
-                    ),
-                  ),
-                ],
+                );
+              },
+            ),
+            BlocProvider(
+              create: (context) => _displayCodeBloc,
+              child: BlocBuilder<DisplayCodeBloc, DisplayCodeState>(
+                builder: (context, state) {
+                  if (state is DisplayCodeSuccess) {
+                    ControlSocket().connect(AppConfig.of(context));
+                  }
+                  return ValueListenableBuilder(
+                    valueListenable: Home.showMainInfo,
+                    builder: (BuildContext context, bool value, Widget? child) {
+                      return Visibility(
+                          visible: value,
+                          child: MainInfo(otpCode: _displayCodeBloc.otp));
+                    },
+                  );
+                },
               ),
+            ),
+            ValueListenableBuilder(
+              valueListenable: Home.showStreamFunction,
+              builder: (BuildContext context, bool value, Widget? child) {
+                return Visibility(
+                  visible: value,
+                  child: const Positioned(
+                      left: 20, bottom: 140, child: StreamFunction()),
+                );
+              },
             ),
           ],
         ),
