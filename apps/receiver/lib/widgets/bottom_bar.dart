936d83ff519012eb7f1aefee0fbd49b6d89c9083
Loren Chang <loren.chang@tw.viewsonic.com>
2022-06-10 13:59:26 +0800

    Remove logo test background

--- apps/receiver/lib/widgets/bottom_bar.dart
+++ apps/receiver/lib/widgets/bottom_bar.dart
@@ -13,21 +13,15 @@ class BottomBar extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                color: Colors.green,
-                child: Image.asset(
-                  'assets/images/ic_logo_my_viewboard.png',
-                  width: 276,
-                  height: 78,
-                ),
+              Image.asset(
+                'assets/images/ic_logo_my_viewboard.png',
+                width: 276,
+                height: 78,
               ),
-              Container(
-                color: Colors.cyan,
-                child: Image.asset(
-                  'assets/images/ic_logo_build_by.png',
-                  width: 234,
-                  height: 88,
-                ),
+              Image.asset(
+                'assets/images/ic_logo_build_by.png',
+                width: 234,
+                height: 88,
               ),
             ],
           ),
@@ -35,5 +29,4 @@ class BottomBar extends StatelessWidget {
       ),
     );
   }
-
 }
