7e34c0e9ecc2c8bc2fd7089eee5fe13caf373649
Zoey Chen <zoey.chen@myviewboard.com>
2022-07-11 18:30:19 +0800

    modify the error snackbar position

--- apps/receiver/lib/utility/ApiResponseFactory.dart
+++ apps/receiver/lib/utility/ApiResponseFactory.dart
@@ -1,29 +1,18 @@
-import 'dart:convert';
-import 'dart:developer';
 
-import 'package:display_flutter/utility/ApiException.dart';
-import 'package:http/http.dart' as http;
 
 class ApiResponseFactory {
-  static dynamic returnResponse(http.StreamedResponse streamResponse) async {
-    var response = await http.Response.fromStream(streamResponse);
-    switch (response.statusCode) {
+  static bool returnResponse(statusCode) {
+    switch (statusCode) {
       case 201:
       case 200:
-        var responseJson = json.decode(response.body.toString());
-        log('responseJson: $responseJson');
-        return responseJson;
+        return true;
       case 400:
-        throw BadRequestException(response.body.toString());
       case 404:
-        throw NotFoundException(response.body.toString());
       case 401:
       case 403:
-        throw UnauthorisedException(response.body.toString());
       case 500:
       default:
-        throw FetchDataException(
-            'Error occurred while Communication with Server with StatusCode : ${response.statusCode}');
+        return false;
     }
   }
 }
