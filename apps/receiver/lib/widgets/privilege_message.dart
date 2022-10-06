d2fcf76ba61c29da2f975e1e3ced6cdf24f9e0c1
Loren Chang <loren.chang@tw.viewsonic.com>
2022-10-06 19:24:18 +0800

    [Implement] UI_UX 40351: DA moves enroll QR code to advanced feature dialog window

--- apps/receiver/lib/widgets/privilege_message.dart
+++ apps/receiver/lib/widgets/privilege_message.dart
@@ -1,13 +1,22 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:display_flutter/app_colors.dart';
+import 'package:display_flutter/app_instance_create.dart';
+import 'package:display_flutter/app_preferences.dart';
 import 'package:display_flutter/generated/l10n.dart';
+import 'package:display_flutter/settings/app_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class PrivilegeDialog extends StatelessWidget {
-  const PrivilegeDialog({Key? key}) : super(key: key);
+  const PrivilegeDialog({Key? key, required this.title}) : super(key: key);
+
+  static ValueNotifier<bool> showPrivilegeQrCode = ValueNotifier(false);
+  final String title;
 
   @override
   Widget build(BuildContext context) {
+    AppConfig? appConfig = AppConfig.of(context);
     return Dialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
@@ -20,22 +29,66 @@ class PrivilegeDialog extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Flexible(
-              flex: 5,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: SingleChildScrollView(
+              flex: 1,
+              child: AutoSizeText(
+                title,
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+                maxLines: 1,
+              ),
+            ),
+            Flexible(
+              flex: 4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  ValueListenableBuilder(
+                    valueListenable: showPrivilegeQrCode,
+                    builder: (BuildContext context, bool value, Widget? child) {
+                      return Visibility(
+                        visible: AppPreferences().entityId.isEmpty,
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            color: Colors.white,
+                          ),
+                          child: QrImage(
+                            data: appConfig != null
+                                ? appConfig.settings.prefixQRCode +
+                                    AppInstanceCreate().displayInstanceID
+                                : '',
+                            version: QrVersions.auto,
+                            size: 120.0,
+                            padding: const EdgeInsets.all(5),
+                            embeddedImage:
+                                const Svg('assets/images/ic_logo_my.svg'),
+                            embeddedImageStyle: QrEmbeddedImageStyle(
+                              // Cannot set too large, will scan failure!!
+                              size: const Size(30, 30),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: AutoSizeText(
                       S.of(context).main_privilege_message,
                       style: const TextStyle(
-                        fontSize: 25,
+                        fontSize: 20,
                         color: Colors.white,
                       ),
                       textAlign: TextAlign.center,
-                      maxLines: 4,
+                      maxLines: 2,
                     ),
                   ),
-                ),
+                ],
               ),
             ),
             Flexible(
