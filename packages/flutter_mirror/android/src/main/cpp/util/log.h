e62908fe61338e0a55ad1d2c4530a96cae61cd2b
Stephen Yang <stephen.tw.yang@viewsonic.com>
2023-03-30 16:31:44 +0800

    feat: disable verbose logging

--- packages/flutter_mirror/android/src/main/cpp/util/log.h
+++ packages/flutter_mirror/android/src/main/cpp/util/log.h
@@ -5,7 +5,7 @@
 
 #define LOG_TAG "MirrorPlugin"
 
-#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define ALOGV(...)  //__android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
