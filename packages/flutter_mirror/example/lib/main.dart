92646d7f494779730e903726aaa9c92e1c456679
Stephen Yang <stephen.tw.yang@viewsonic.com>
2023-04-27 14:36:22 +0800

    test: turn on airplay security

--- packages/flutter_mirror/example/lib/main.dart
+++ packages/flutter_mirror/example/lib/main.dart
@@ -56,7 +56,7 @@ class _MyAppState extends State<MyApp> implements FlutterMirrorListener {
       // start airplay
       await _plugin.startAirplay(const AirplayConfig(
         name: "display-1",
-        security: AirplaySecurity.none,
+        security: AirplaySecurity.onscreenCode,
       ));
 
       // load today's credentials
