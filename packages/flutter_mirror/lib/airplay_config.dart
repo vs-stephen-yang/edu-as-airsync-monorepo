6f956ce850ceebc442edcd5a6a8f27bccfa15255
Stephen Yang <stephen.tw.yang@viewsonic.com>
2023-04-27 14:35:52 +0800

    refactor: add commets for magic numbers

--- packages/flutter_mirror/lib/airplay_config.dart
+++ packages/flutter_mirror/lib/airplay_config.dart
@@ -1,6 +1,8 @@
+// the values must be same with the ones defined in android\src\main\cpp\flutter_mirror.cpp
 enum AirplaySecurity {
   none,
   onscreenCode,
+  //password
 }
 
 class AirplayConfig {
