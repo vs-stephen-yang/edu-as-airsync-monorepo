e0ed2529e4c6dd803c2b77d30fb55bea0290e249
Loren Chang <loren.chang@tw.viewsonic.com>
2023-04-28 17:17:25 +0800

    Integrate package_info_plus and show version on screen.

--- apps/sender/lib/screens/home.dart
+++ apps/sender/lib/screens/home.dart
@@ -1,4 +1,4 @@
-
+import 'package:display_cast_flutter/settings/app_config.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -11,11 +11,25 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    AppConfig? appConfig = AppConfig.of(context);
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[],
+          children: <Widget>[
+            Text(
+              appConfig?.appName ?? '',
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              appConfig?.appVersion ?? '',
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              appConfig?.appVersionCode.toString() ?? '',
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
         ),
       ),
     );
