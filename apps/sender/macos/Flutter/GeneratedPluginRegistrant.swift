4b22dd0f5231a52bca4a835cc5453fa25a311432
Loren Chang <loren.chang@tw.viewsonic.com>
2023-04-28 18:01:20 +0800

    Install flutter_webrtc package.

--- apps/sender/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/sender/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_webrtc
 import package_info_plus
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
