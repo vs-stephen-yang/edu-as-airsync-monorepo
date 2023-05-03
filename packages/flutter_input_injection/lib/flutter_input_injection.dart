20aaa4a6d16e09e0548a65ae3d15d8ff827a8837
Harvey Lin <harvey.lin@myviewboard.com>
2023-05-03 12:00:51 +0800

    refactor: Replace magic number with named constants.

--- packages/flutter_input_injection/lib/flutter_input_injection.dart
+++ packages/flutter_input_injection/lib/flutter_input_injection.dart
@@ -2,6 +2,12 @@
 import 'flutter_input_injection_platform_interface.dart';
 
 class FlutterInputInjection {
+  /* action type of touch event */
+  static const int TOUCH_POINT_START = 0;
+  static const int TOUCH_POINT_MOVE = 1;
+  static const int TOUCH_POINT_END = 2;
+  static const int TOUCH_POINT_CANCEL = 3;
+
   Future<String?> getPlatformVersion() {
     return FlutterInputInjectionPlatform.instance.getPlatformVersion();
   }
