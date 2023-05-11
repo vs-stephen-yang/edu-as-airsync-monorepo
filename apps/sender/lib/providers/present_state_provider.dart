4d85725ea54122f26c5354a792912154a32cd87e
Loren Chang <loren.chang@tw.viewsonic.com>
2023-05-11 17:30:05 +0800

    Implement webrtc business logic

--- apps/sender/lib/providers/present_state_provider.dart
+++ apps/sender/lib/providers/present_state_provider.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
+import 'package:display_cast_flutter/features/webrtc_helper.dart';
 import 'package:display_cast_flutter/settings/app_config.dart';
 import 'package:display_cast_flutter/utilities/debug_mode_print.dart';
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:uuid/uuid.dart';
 
 enum ViewState {
   idle,
@@ -13,13 +16,18 @@ enum ViewState {
 
 class PresentStateProvider extends ChangeNotifier {
   PresentStateProvider(BuildContext context) {
-    debugModePrint(
-        'appConfig apiGateway: ${AppConfig.of(context)!.settings.urlGateway}');
+    _urlGateway = AppConfig.of(context)!.settings.urlGateway;
+    _webRTCHelper = WebRTCHelper(AppConfig.of(context)!.settings.urlGetIce);
   }
 
   ViewState get state => _currentState;
   ViewState _currentState = ViewState.idle;
   Timer? _presentTimer;
+  final _userId = (const Uuid()).v4();
+  late final String _urlGateway;
+  late dynamic _msgDisplay;
+  late WebRTCHelper? _webRTCHelper;
+  late io.Socket? _socket;
 
   setViewState(ViewState newViewState) {
     _currentState = newViewState;
@@ -33,10 +41,66 @@ class PresentStateProvider extends ChangeNotifier {
   Future<void> presentTo(
       {required String displayCode, required String otp}) async {
     debugModePrint('presentTo: displayCode: $displayCode, otp: $otp');
-    setViewState(ViewState.waitReady);
     _presentTimer = Timer(const Duration(seconds: 30), () {
       presentStop();
     });
+    displayCode = displayCode.replaceAll('-', '');
+
+    _socket = io.io(
+        _urlGateway,
+        io.OptionBuilder()
+            .setTransports(['websocket'])
+            .enableForceNew()
+            .setQuery({
+              "userid": _userId,
+              "socketCustomEvent": displayCode,
+              "role": "presenter",
+            })
+            .build());
+
+    _socket?.onConnect((_) {
+      debugModePrint('connected to display');
+
+      _socket?.on("display-ready", (msg) async {
+        debugModePrint('display-ready: $msg');
+        _msgDisplay = msg;
+        presentSelectScreen();
+      });
+
+      _socket?.emit("create-presenter", {
+        "messageFor": displayCode,
+        "action": "join",
+        "status": "open",
+        "extra": {
+          "presenter": {
+            "id": _userId,
+            "name": null,
+            "remark": "",
+            "status": "",
+            "extra": {}
+          },
+          "display": {"code": displayCode, "token": ""}
+        }
+      });
+
+      _socket?.emit("join-display", {
+        "messageFor": displayCode,
+        "action": "join",
+        "status": "open",
+        "extra": {
+          "presenter": {
+            "id": _userId,
+            "name": null,
+            "remark": "",
+            "status": "",
+            "extra": {}
+          },
+          "moderator": null,
+          "display": {"code": displayCode, "setId": "5tovgl636ge"}
+        }
+      });
+    });
+    setViewState(ViewState.waitReady);
   }
 
   Future<void> presentSelectScreen() async {
@@ -48,10 +112,24 @@ class PresentStateProvider extends ChangeNotifier {
   }
 
   Future<void> presentStart() async {
+    await _webRTCHelper?.makeCall(
+      _msgDisplay['extra']['signal']['url'],
+      _msgDisplay['extra']['setClientId'],
+      _msgDisplay['extra']['setAllowedPeer'],
+    );
     setViewState(ViewState.presentStart);
   }
 
   Future<void> presentStop() async {
+    try {
+      await _webRTCHelper?.hangUp();
+
+      _socket?.disconnect();
+      _socket?.dispose();
+      _socket = null;
+    } catch (e) {
+      debugModePrint(e);
+    }
     setViewState(ViewState.idle);
   }
 }
