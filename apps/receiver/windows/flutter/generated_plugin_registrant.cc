dc05ac2bc8d5cb168a56e03c5e10441486ab43df
Loren Chang <loren.chang@tw.viewsonic.com>
2023-06-08 10:18:11 +0800

    Integrate flutter_mirror plug-in

--- apps/receiver/windows/flutter/generated_plugin_registrant.cc
+++ apps/receiver/windows/flutter/generated_plugin_registrant.cc
@@ -7,11 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <device_info_vs/device_info_vs_plugin.h>
+#include <flutter_mirror/flutter_mirror_plugin_c_api.h>
 #include <flutter_webrtc/flutter_web_r_t_c_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DeviceInfoVsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DeviceInfoVsPlugin"));
+  FlutterMirrorPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterMirrorPluginCApi"));
   FlutterWebRTCPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterWebRTCPlugin"));
 }
