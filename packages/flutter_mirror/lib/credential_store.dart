8bc339b21b2288159af0cb6d3bc03513cc383ab0
benjamin lee <benjamin.lee@viewsonic.com>
2023-06-09 15:25:59 +0800

    feat(googlecast): Switch to using encrypted credentials instead of plain text version.

--- packages/flutter_mirror/lib/credential_store.dart
+++ packages/flutter_mirror/lib/credential_store.dart
@@ -1,41 +1,26 @@
 import 'package:flutter_mirror/credentials.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:libcastauth/libcastauth.dart';
 import 'package:pem/pem.dart';
 import 'dart:convert';
 import 'dart:typed_data';
 
-Uint8List decodePem(String pem) {
-  const prefix = "-----BEGIN RSA PRIVATE KEY-----";
-  const postfix = "-----END RSA PRIVATE KEY-----";
-
-  if (pem.startsWith(prefix)) {
-    pem = pem.substring(prefix.length);
-  }
-
-  if (pem.endsWith(postfix)) {
-    pem = pem.substring(0, pem.length - postfix.length);
-  }
-
-  pem = pem.replaceAll('\n', '');
-  pem = pem.replaceAll('\r', '');
-
-  return Uint8List.fromList(base64.decode(pem));
-}
-
-// convert PEM to DER format
-Uint8List pem2der(String pem, PemLabel label) {
-  List<int> der = PemCodec(label).decode(pem);
-
-  return Uint8List.fromList(der);
-}
-
-String pad2digits(int v) {
-  return v.toString().padLeft(2, '0');
-}
-
 class CredentialsStore {
-  static const credentialsPath = "packages/flutter_mirror/assets/credentials";
+  static const _secureDataDir = "packages/flutter_mirror/assets/credentials";
+  static const _secureDataPath = "$_secureDataDir/data.json";
+  static const _aesKey = "BkRHQv/aOvqcP9wSIfUrSWuZfvehXiQ67mYsSwbbo6E=";
+  static const _aesIv = "rvFMI/HFqHMEBxsGdK4zDw==";
+
+  static late SecureData _secureData;
+  static late CastKeySetReader _castKeySetReader;
+
+  // initialize secureData and castKeySetReader
+  static Future init() async {
+    String json = await rootBundle.loadString(_secureDataPath);
+    _secureData = SecureData(_aesKey, _aesIv)..fromJson(json);
+    _castKeySetReader = CastKeySetReader.fromSecureData(_secureData);
+  }
 
   // load today's credentials
   static Future<Credentials> loadToday() async {
@@ -44,46 +29,20 @@ class CredentialsStore {
     return load(now.year, now.month, now.day);
   }
 
-  // load a credentials from file
-  static Future<Credentials> load(
-    int year,
-    int month,
-    int day,
-  ) async {
-    final m = pad2digits(month);
-    final d = pad2digits(day);
-
-    final path = "$credentialsPath/cooked_$year-$m-$d.json";
-
-    String json = await rootBundle.loadString(path);
-    return parse(json, year, month, day);
-  }
-
-  static Credentials parse(
-    String json,
-    int year,
-    int month,
-    int day,
-  ) {
-    Map<String, dynamic> cred = jsonDecode(json);
-
-    Uint8List deviceCert = pem2der(cred['cpu'], PemLabel.certificate);
-    Uint8List icaCert = pem2der(cred['ica'], PemLabel.certificate);
-
-    Uint8List tlsCert = pem2der(cred['pu'], PemLabel.certificate);
-    Uint8List tlsKey = decodePem(cred['pr']);
-
-    Uint8List signature = base64Decode(cred['sig']);
-
+  // load credentials from secureData
+  static Future<Credentials> load(int year, int month, int day) async {
+    var castKey = _castKeySetReader.getKeySet(year, month, day);
+    if (castKey == null) {
+      throw Exception("No key found for $year-$month-$day");
+    }
     return Credentials(
-      year,
-      month,
-      day,
-      deviceCert,
-      icaCert,
-      tlsCert,
-      tlsKey,
-      signature,
-    );
+        year,
+        month,
+        day,
+        castKey.deviceCertDer,
+        castKey.icaCertDer,
+        castKey.tlsCertDer,
+        castKey.tlsKeyDer,
+        castKey.signature);
   }
 }
