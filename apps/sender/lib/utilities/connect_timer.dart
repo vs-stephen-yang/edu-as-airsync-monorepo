140724a6b0134410ec675d16aad041daa73ca0c6
Zoey Chen <zoey.chen@myviewboard.com>
2023-07-26 17:15:30 +0800

    Implement webrtc_helper_v1 to backward compatibility with Display v1 version (part I)

---
+++ apps/sender/lib/utilities/connect_timer.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+typedef ConnectionTimerCallback = void Function();
+
+class ConnectionTimer {
+  Timer? mConnectionTimeoutTimer;
+
+  static final ConnectionTimer _instance = ConnectionTimer.internal();
+
+  static ConnectionTimer getInstance() {
+    return _instance;
+  }
+
+  ConnectionTimer.internal();
+
+  void startConnectionTimeoutTimer(ConnectionTimerCallback onFinish) {
+    if (mConnectionTimeoutTimer != null) stopConnectionTimeoutTimer();
+
+    mConnectionTimeoutTimer =
+        Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (timer.tick < 30) {
+        // onTick
+
+      } else if (timer.tick == 30) {
+        // onFinish
+        timer.cancel();
+        onFinish();
+      }
+    });
+  }
+
+  void stopConnectionTimeoutTimer() {
+    mConnectionTimeoutTimer?.cancel();
+  }
+}
