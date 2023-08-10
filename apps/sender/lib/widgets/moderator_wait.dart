52b6659b3092498fdb0042ac4b76f03d77026132
Zoey Chen <zoey.chen@myviewboard.com>
2023-08-10 16:56:28 +0800

    Implement New UI (part III):moderator

--- apps/sender/lib/widgets/moderator_wait.dart
+++ apps/sender/lib/widgets/moderator_wait.dart
@@ -2,7 +2,6 @@
 import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/present_state_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:provider/provider.dart';
 
 class ModeratorWait extends StatelessWidget {
@@ -11,44 +10,73 @@ class ModeratorWait extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     PresentStateProvider presentStateProvider = Provider.of<PresentStateProvider>(context);
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.topRight,
-          child:Container(
-            padding: const EdgeInsets.fromLTRB(0, 40, 30, 0),
-            child: ElevatedButton.icon(
+    return SizedBox(
+      width: 300,
+      height: 400,
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Expanded(
+                  child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Icon(
+                        Icons.groups,
+                        color: Colors.white,
+                      ))),
+              Padding(
+                padding: EdgeInsets.only(left: 6),
+                child: Text(
+                  'Moderator',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Spacer(flex: 1,),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Divider(color: Colors.white12,),
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: Text(
+              S.of(context).moderator_wait,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: ElevatedButton(
               onPressed: () {
                 presentStateProvider.presentEnd();
               },
               style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.black,
-                padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                side: const BorderSide(
-                    color: Colors.red,
-                    width: 1,
-                    style: BorderStyle.solid
+                foregroundColor: Colors.red,
+                backgroundColor: Colors.red,
+                fixedSize: const Size(300, 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
                 ),
               ),
-              icon: const Image(image: Svg('assets/images/ic_exit.svg')),
-              label: const Text(
+              // icon: const Image(image: Svg('assets/images/ic_exit.svg')),
+              child: const Text(
                 'EXIT',
-                style: TextStyle(color: Colors.red),
+                style: TextStyle(color: Colors.white),
               ),
             ),
-          ),
-        ),
-        Center(
-          child: Text(
-            S.of(context).moderator_wait,
-            style: const TextStyle(
-              color: Colors.grey,
-              fontSize: 20,
-            ),
-          ),
-        ),
-      ],
+          )
+        ],
+      ),
     );
   }
 }
