d17c814d43c9b8953e2767244ffa8c02d930af38
Stephen Yang <stephen.tw.yang@viewsonic.com>
2023-09-24 19:52:00 +0800

    feat: use non-blocking I/O for RTP

--- packages/flutter_mirror/android/src/main/java/com/viewsonic/miracast/rtp/RTPServer.java
+++ packages/flutter_mirror/android/src/main/java/com/viewsonic/miracast/rtp/RTPServer.java
@@ -1,6 +1,10 @@
 package com.viewsonic.miracast.rtp;
 
 import android.util.Log;
+
+import com.viewsonic.miracast.net.EventBase;
+import com.viewsonic.miracast.net.UdpSocket;
+import com.viewsonic.miracast.net.UdpSocketListener;
 import com.viewsonic.miracast.rtsp.OnReceiveRTSPListener;
 
 import java.io.IOException;
@@ -8,37 +12,82 @@ import java.net.SocketTimeoutException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 
 public class RTPServer {
   private static final String TAG = "MiraRTPServer";
   private static final int kRTPBufferSize = 1024 * 1024;
-  private UDPServer rtpSession_;
-  private UDPServer rtcpSession_;
+  private static final int MTU = 10 * 1024;
+
+  private UdpSocket rtpSession_;
+  private UdpSocket rtcpSession_;
+
+  private ByteBuffer rtpReadBuffer_ = ByteBuffer.allocate(MTU);
   private OnReceiveRTPListener receiveRTPListener_;
 
-  public RTPServer(OnReceiveRTPListener listener) {
+  private EventBase eventBase_;
+
+  public RTPServer(
+      EventBase eventBase,
+      OnReceiveRTPListener listener) {
     receiveRTPListener_ = listener;
+    eventBase_ = eventBase;
   }
 
-  public void start() {
-    rtpSession_ = new UDPServer(kRTPBufferSize, 0, receiveRTPListener_);
-    rtpSession_.start();
-    Log.i(TAG, "RTP session is running on port " + rtpSession_.port_);
+  public void start() throws IOException {
+    // RTP
+    rtpSession_ = new UdpSocket(eventBase_, socket -> {
+      onRtpPacket();
+    });
+    rtpSession_.setReceiveBufferSize(kRTPBufferSize);
+    rtpSession_.enableRead();
+    rtpSession_.bind(0);
+    Log.i(TAG, "RTP session is running on port " + rtpSession_.getLocalPort());
 
-    rtcpSession_ = new UDPServer(0, null);
-    rtcpSession_.start();
-    Log.i(TAG, "RTCP session is running on port " + rtcpSession_.port_);
+    // RTCP
+    rtcpSession_ = new UdpSocket(eventBase_, socket -> {
+      onRtcpPacket();
+    });
+    rtcpSession_.setReceiveBufferSize(kRTPBufferSize);
+    rtcpSession_.enableRead();
+    rtcpSession_.bind(0);
+    Log.i(TAG, "RTCP session is running on port " + rtcpSession_.getLocalPort());
+  }
+
+  private void onRtcpPacket() {
+  }
+
+  private void onRtpPacket() {
+    try {
+      rtpSession_.receive(rtpReadBuffer_);
+      rtpReadBuffer_.flip();
+      byte[] data = rtpReadBuffer_.array();
+
+      receiveRTPListener_.onRtpData(getMirrorSeqNum(data), data, rtpReadBuffer_.remaining());
+      rtpReadBuffer_.clear();
+    } catch (IOException e) {
+      // TODO
+      e.printStackTrace();
+    }
+  }
+
+  private static long getMirrorSeqNum(byte[] data) {
+    return ((data[2] & 0xFF) << 8) | (data[3] & 0xFF);
   }
 
   public void stop() {
-    rtpSession_.stop();
-    rtcpSession_.stop();
+    try {
+      rtpSession_.close();
+      rtcpSession_.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   public int getRtpPort() {
     if (null != rtpSession_) {
-      return rtpSession_.port_;
+      return rtpSession_.getLocalPort();
     }
 
     return 0;
@@ -46,110 +95,9 @@ public class RTPServer {
 
   public int getRtcpPort() {
     if (null != rtcpSession_) {
-      return rtcpSession_.port_;
+      return rtcpSession_.getLocalPort();
     }
 
     return 0;
-  }
-
-  public static int lastSeq = -1;
-
-  private static long getMirrorSeqNum(byte[] data) {
-    return ((data[2] & 0xFF) << 8) | (data[3] & 0xFF);
-  }
-
-  /**
-   * Represents the UDP server.
-   */
-  static class UDPServer {
-    private OnReceiveRTPListener receiveRTPListener_;
-    private static final int kSocketReceiveTimeoutMs_ = 100; //ms
-
-    class Worker extends Thread {
-      public static final int MTU = 10 * 1024;
-      private byte receiveBuffer_[] = new byte[MTU];
-
-      public Worker() {
-      }
-
-      public void run() {
-        DatagramPacket packet = new DatagramPacket(receiveBuffer_, MTU);
-        while(isRunning_.get() && !socket_.isClosed()) {
-          try {
-            socket_.receive(packet);
-            if (receiveRTPListener_ != null) {
-              receiveRTPListener_.onRtpData(getMirrorSeqNum(packet.getData()), packet.getData(),
-                  packet.getLength());
-            }
-          } catch (SocketTimeoutException e) {
-            continue;
-          } catch (Exception e) {
-            Log.e(TAG, "Exception: " + e.toString());
-            if (e.toString().contains("Socket closed")) {
-              break;
-            }
-          }
-        }
-
-        try {
-          socket_.close();
-        } catch (Exception e) {
-          Log.e(TAG, "Socket close - Exception: " + e.toString());
-        }
-      }
-    }
-
-    private int port_ = 0;
-    private Worker worker_;
-    private DatagramSocket socket_ = null;
-    private int socketBufferSize_ = 0;
-    private AtomicBoolean isRunning_ = new AtomicBoolean(false);
-
-    UDPServer(int bufferSize, int port, OnReceiveRTPListener listener) {
-      init(bufferSize, port, listener);
-    }
-
-    UDPServer(int port, OnReceiveRTPListener listener) {
-      init(0, port, listener);
-    }
-
-    private void init(int bufferSize, int port, OnReceiveRTPListener listener) {
-      socketBufferSize_ = bufferSize;
-      receiveRTPListener_ = listener;
-      try {
-        socket_ = new DatagramSocket(port);
-        if (socketBufferSize_ > 0) {
-          socket_.setReceiveBufferSize(socketBufferSize_);
-        }
-        if (socket_.isBound()) {
-          port_ = socket_.getLocalPort();
-        }
-        socket_.setSoTimeout(kSocketReceiveTimeoutMs_);
-        worker_ = new Worker();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-
-    public void start() {
-      if(socket_ != null && isRunning_.compareAndSet(false, true)) {
-        /* Ensure socket is created before starting worker thread. */
-        try {
-          worker_.start();
-        } catch (Exception e) {
-          Log.e(TAG, "udp server start Exception " + e.toString());
-        }
-      }
-    }
-
-    public void stop() {
-      if(isRunning_.compareAndSet(true, false)) {
-        try {
-          worker_.join();
-        } catch (Exception e) {
-          Log.e(TAG, "udp server stop Exception " + e.toString());
-        }
-      }
-    }
   }
 }
