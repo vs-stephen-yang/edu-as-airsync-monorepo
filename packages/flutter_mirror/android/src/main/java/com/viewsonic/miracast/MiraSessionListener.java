36dd49a33b48de879209d74214439df9a28d0f6d
Stephen Yang <stephen.tw.yang@viewsonic.com>
2023-10-02 15:12:42 +0800

    feat(miracast): Notify session start upon successful RTSP connection

--- packages/flutter_mirror/android/src/main/java/com/viewsonic/miracast/MiraSessionListener.java
+++ packages/flutter_mirror/android/src/main/java/com/viewsonic/miracast/MiraSessionListener.java
@@ -1,6 +1,8 @@
 package com.viewsonic.miracast;
 
 public interface MiraSessionListener {
+  void onRtspConnected(String mirrorId);
+
   void onMirrorData(String mirrorId, long seqNum, long lastSeqNum, byte[] data, int size);
 
   void onAudioFormatUpdate(String mirrorId, String codecName, int sampleRate, int channelCount);
