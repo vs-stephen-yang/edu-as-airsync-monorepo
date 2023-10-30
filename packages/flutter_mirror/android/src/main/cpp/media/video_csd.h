6173d7faad6999821e4b36b20959e22fa07e0336
Stephen Yang <stephen.tw.yang@viewsonic.com>
2023-10-30 15:53:05 +0800

    feat: Initialize video decoder with csd after first key frame

--- packages/flutter_mirror/android/src/main/cpp/media/video_csd.h
+++ packages/flutter_mirror/android/src/main/cpp/media/video_csd.h
@@ -7,13 +7,20 @@ class VideoCsd {
  public:
   VideoCsd(
       unsigned int width,
-      unsigned int height)
+      unsigned int height,
+      std::vector<uint8_t> csd0 = {},
+      std::vector<uint8_t> csd1 = {})
       : width(width),
-        height(height) {
+        height(height),
+        csd0(csd0),
+        csd1(csd1) {
   }
 
   unsigned int width = 0;
   unsigned int height = 0;
+
+  std::vector<uint8_t> csd0;
+  std::vector<uint8_t> csd1;
 };
 
 #endif  // FLUTTER_MIRROR_PLUGIN_VIDEO_CSD_H_
