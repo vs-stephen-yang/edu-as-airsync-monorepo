09191a37875c7caa21118984c08341b3eb26dc64
Zoey Chen <zoey.chen@myviewboard.com>
2023-11-10 11:45:37 +0800

    Implement new UI (part5) - modify PIN text field's behavior

--- apps/sender/lib/widgets/present_idle_pin_text.dart
+++ apps/sender/lib/widgets/present_idle_pin_text.dart
@@ -1,7 +1,6 @@
 
 import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/present_state_provider.dart';
-import 'package:display_cast_flutter/utilities/data_display_code.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_multi_formatter/formatters/masked_input_formatter.dart';
 import 'package:provider/provider.dart';
@@ -24,9 +23,7 @@ class PresentIdlePinTextState extends State<PresentIdlePinText> {
   final TextEditingController _codeController = TextEditingController();
   final FocusNode _codeFocusNode = FocusNode();
   final GlobalKey<CustomTextFormFieldState> codeKey = GlobalKey();
-  late OverlayEntry _overlayEntry;
   final LayerLink _layerLink = LayerLink();
-  bool _isOverlayVisible = false;
 
   @override
   void initState() {
@@ -44,65 +41,6 @@ class PresentIdlePinTextState extends State<PresentIdlePinText> {
     _codeController.dispose();
     _codeFocusNode.dispose();
     super.dispose();
-  }
-
-  /// the height of every item is 40
-  /// the scroll bar should show up when over 5 items
-  /// the max height of the overlay area is 200
-  OverlayEntry _createOverlayEntry() {
-    return OverlayEntry(builder: (builder) {
-      return Stack(
-        children: <Widget>[
-          // Check range outside listview
-          GestureDetector(
-            onTap: () {
-              _isOverlayVisible = false;
-              _overlayEntry.remove();
-            },
-            child: Container(
-              color: Colors.transparent, // 設置為透明色
-              width: double.infinity,
-              height: double.infinity,
-            ),
-          ),
-          Positioned(
-              width: 250,
-              height: DataDisplayCode.getInstance().displayCodeList!.length > 5
-                  ? 200
-                  : DataDisplayCode.getInstance().displayCodeList!.length * 40,
-              child: CompositedTransformFollower(
-                offset: const Offset(0, 50),
-                link: _layerLink,
-                child: Material(
-                  child: DataDisplayCode.getInstance().displayCodeList == null
-                      ? const SizedBox()
-                      : Scrollbar(
-                          child: ListView.builder(
-                          itemCount: DataDisplayCode.getInstance()
-                              .displayCodeList!
-                              .length,
-                          itemBuilder: (BuildContext context, int index) {
-                            List list =
-                                DataDisplayCode.getInstance().displayCodeList!;
-                            return InkWell(
-                              child: Container(
-                                  height: 40,
-                                  alignment: Alignment.centerLeft,
-                                  padding: const EdgeInsets.only(left: 15),
-                                  child: Text(list[index])),
-                              onTap: () {
-                                _codeController.text = list[index];
-                                _isOverlayVisible = false;
-                                _overlayEntry.remove();
-                              },
-                            );
-                          },
-                        )),
-                ),
-              ))
-        ],
-      );
-    });
   }
 
   @override
@@ -124,7 +62,6 @@ class PresentIdlePinTextState extends State<PresentIdlePinText> {
               key: codeKey,
               controller: _codeController,
               focusNode: _codeFocusNode,
-              // initialValue: displayCode,
               labelText: 'PIN code',
               errorText: S.of(context).main_display_code_description,
               inputFormatter: [
@@ -139,14 +76,6 @@ class PresentIdlePinTextState extends State<PresentIdlePinText> {
                   presentBtnEnable = true;
                 }
                 widget.onFieldChanged(FieldResult(enable: presentBtnEnable, displayCode: text));
-              },
-              onTap: () async {
-                await DataDisplayCode.getInstance().load();
-                if (!_isOverlayVisible && DataDisplayCode.getInstance().displayCodeList != null) {
-                  _isOverlayVisible = true;
-                  _overlayEntry = _createOverlayEntry();
-                  Overlay.of(context).insert(_overlayEntry);
-                }
               },
               onFieldSubmitted: (text) {
 
