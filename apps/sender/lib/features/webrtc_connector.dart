7d986957bc45101b2750beb95a42b6555b2cfa28
Zoey Chen <zoey.chen@myviewboard.com>
2023-11-15 13:52:29 +0800

    Integrate display_channel (part3) - implement PeerConnection

---
+++ apps/sender/lib/features/webrtc_connector.dart
@@ -0,0 +1,402 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
+import 'package:collection/collection.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:display_cast_flutter/features/protoc/event.pb.dart';
+import 'package:display_cast_flutter/features/protoc/internal.pb.dart';
+import 'package:display_cast_flutter/model/message.dart';
+import 'package:display_cast_flutter/utilities/debug_mode_print.dart';
+import 'package:display_cast_flutter/utilities/sdp_utility.dart';
+import 'package:display_channel/display_channel.dart';
+import 'package:flutter_input_injection/flutter_input_injection.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:window_size/window_size.dart';
+
+class WebRTCConnector {
+  WebRTCConnector(
+      {bool touchBack = false, bool systemAudio = false, required this.sendSignalMessage}) {
+    _touchBack = touchBack;
+    _systemAudio = systemAudio;
+  }
+
+  void Function(Map<String, dynamic>) sendSignalMessage;
+
+
+  final Map<String, dynamic> _configuration = {
+    'sdpSemantics': 'unified-plan',
+  };
+
+  dynamic _deviceId;
+
+  RTCPeerConnection? _pc;
+  RTCDataChannel? _dc;
+  MediaStream? _localStream;
+  // io.Socket? _socket;
+  double _screenWidth = 1920.0;
+  double _screenHeight = 1080.0;
+  static const int _maxTrackWidth = 1920;
+  static const int _maxTrackHeight = 1080;
+  int _trackWidth = _maxTrackWidth;
+  int _trackHeight = _maxTrackHeight;
+  bool _touchBack = false;
+  bool _isSourceTypeScreen = false;
+  bool _systemAudio = false;
+
+  int get trackHeight => _trackHeight;
+  final _flutterInputInjectionPlugin = FlutterInputInjection();
+
+  //region public methods
+
+  Future<void> makeCall(String peerId, dynamic source, List<RtcIceServer>? iceServerList) async {
+    dynamic deviceId;
+
+    if (Platform.isAndroid) {
+    } else if (Platform.isIOS) {
+      deviceId = 'broadcast';
+    } else {
+      deviceId = {'exact': source.id};
+      _isSourceTypeScreen = (source.type == SourceType.Screen);
+    }
+
+    _deviceId = deviceId;
+    await _peerConnectionConnect(iceServerList);
+  }
+
+  Future<void> hangUp() async {
+    await disposeStream();
+    await _peerConnectionDisconnect();
+  }
+
+  Future<void> disposeStream() async {
+    try {
+      if (_localStream != null) {
+        var stream = _localStream!;
+        _localStream = null;
+        await stream.dispose();
+      }
+    } catch (e) {
+      debugModePrint(e, type: runtimeType);
+    }
+  }
+
+  void streamStop() {
+    _pc?.getLocalStreams().forEach((element) {
+      element?.getTracks().first.enabled = false;
+      element?.getTracks().first.stop();
+    });
+  }
+
+  Future<void> streamPause() async {
+    var constraints = <String, dynamic>{
+      'audio': false,
+      'video': {
+        'deviceId': _deviceId,
+        'mandatory': {'frameRate': 0.0},
+      }
+    };
+    _localStream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    for (MediaStreamTrack track in _localStream!.getTracks()) {
+      await _pc?.getSenders().then((value) async {
+        for (var element in value) {
+          await element.replaceTrack(track);
+        }
+      });
+    }
+  }
+
+  Future<void> streamResume() async {
+    var constraints = <String, dynamic>{
+      'audio': _isAudioCaptureAllowed(),
+      'video': {
+        'deviceId': _deviceId,
+        'mandatory': {'frameRate': 30.0},
+      }
+    };
+    _localStream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    for (MediaStreamTrack track in _localStream!.getTracks()) {
+      await _pc?.getSenders().then((value) async {
+        for (var element in value) {
+          await element.replaceTrack(track);
+        }
+      });
+    }
+  }
+
+  Future<void> changeStreamFrameRate(Map<String, dynamic> json) async {
+    final msg = PresentChangeQualityMessage.fromJson(json);
+    if (msg.height < _maxTrackHeight) {
+      // make sure the width/height is not greater than the max width
+      _trackWidth = _maxTrackWidth ~/ (_maxTrackHeight / msg.height);
+      _trackHeight = msg.height;
+    }
+    final constraints = <String, dynamic>{
+      'audio': _isAudioCaptureAllowed(),
+      'video': !WebRTC.platformIsDesktop
+          ? true
+          : {
+        'deviceId': _deviceId,
+        'mandatory': {
+          'frameRate': msg.frameRate,
+        },
+        'width': _trackWidth.toString(),
+        'height': _trackHeight.toString(),
+      }
+    };
+
+    _localStream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    for (MediaStreamTrack track in _localStream!.getTracks()) {
+      _pc?.getSenders().then((value) async {
+        await value.first.replaceTrack(track);
+      });
+    }
+  }
+
+  //endregion
+
+  bool _isTouchBackAllowed() {
+    return _isSourceTypeScreen &&
+        _touchBack &&
+        _localStream!.getTracks().first.enabled;
+  }
+
+  bool _isAudioCaptureAllowed() {
+    if (WebRTC.platformIsWindows) {
+      return _isSourceTypeScreen && _systemAudio;
+    }
+    return true;
+  }
+
+  Future<void> _peerConnectionConnect(List<RtcIceServer>? iceServerList) async {
+    _configuration.putIfAbsent('iceServers', () => iceServerList);
+    _pc = await createPeerConnection(_configuration).whenComplete(() async {
+      await _publish();
+    });
+
+    _pc!.onAddTrack = _onAddTrack;
+    _pc!.onSignalingState = _onSignalingState;
+    _pc!.onIceGatheringState = _onIceGatheringState;
+    _pc!.onIceConnectionState = _onIceConnectionState;
+    _pc!.onConnectionState = _onPeerConnectionState;
+    _pc!.onIceCandidate = _onCandidate;
+  }
+
+  Future<void> _peerConnectionDisconnect() async {
+    try {
+      await _pc?.close();
+      await _pc?.dispose();
+      _pc = null;
+    } catch (e) {
+      debugModePrint(e, type: runtimeType);
+    }
+  }
+
+  Future<void> updateScreenSize() async {
+    Screen? screen = await getCurrentScreen();
+    if (screen != null) {
+      _screenWidth = screen.frame.width;
+      _screenHeight = screen.frame.height;
+    }
+  }
+
+  Future<void> _publish() async {
+    _dc = await _pc!.createDataChannel('pc-dc', RTCDataChannelInit()..id = 1);
+    _setDataChannelListeners(_dc!);
+
+    final constraints = <String, dynamic>{
+      'audio': _isAudioCaptureAllowed(),
+      'video': {
+        'deviceId': _deviceId,
+        'mandatory': {'frameRate': 30.0},
+        'width': _trackWidth = _maxTrackWidth,
+        'height': _trackHeight = _maxTrackHeight,
+      }
+    };
+
+    _localStream = await navigator.mediaDevices.getDisplayMedia(constraints);
+    for (MediaStreamTrack track in _localStream!.getTracks()) {
+      debugModePrint('track: ${track.kind}', type: runtimeType);
+      _pc!.addTrack(track, _localStream!);
+    }
+
+    final offerConstraints = <String, dynamic>{
+      'mandatory': {
+        'OfferToReceiveAudio': false,
+        'OfferToReceiveVideo': false,
+      },
+      'optional': [],
+    };
+
+    final offer = await _pc!.createOffer(offerConstraints);
+
+    offer.sdp = SdpUtil.removeCodec(offer.sdp!, "AV1");
+    RTCSessionDescription fixedOffer = SdpUtil.fixSdp(offer);
+
+    try {
+      await _pc!.setLocalDescription(fixedOffer);
+
+      Map<String, dynamic> json = {
+        'type': fixedOffer.type,
+        'sdp': fixedOffer.sdp,
+        'candidate': "",
+        'sdpMid': "",
+        'sdpMLineIndex': "",
+      };
+      sendSignalMessage(json);
+    } catch (e) {
+      debugModePrint(e, type: runtimeType);
+    }
+  }
+
+  /// handle signal message from Display, ex. offer, answer, candidates
+  Future<void> handleSignal(PresentSignalMessage msg) async {
+    final type = msg.signalType.toString(); //msg['type'];
+
+    if (type == 'offer') {
+      // handle offer from the peer
+      final offer = RTCSessionDescription(msg.sdp, type);
+      _pc!.setRemoteDescription(offer);
+
+      // create answer
+      final answer = await _pc!.createAnswer();
+      RTCSessionDescription fixedAnswer = SdpUtil.fixSdp(answer);
+      await _pc!.setLocalDescription(fixedAnswer);
+      // send answer to the peer
+      Map<String, dynamic> json = {
+        'type': fixedAnswer.type,
+        'sdp': fixedAnswer.sdp,
+        'candidate': '',
+        'sdpMid': '',
+        'sdpMLineIndex': '',
+      };
+      sendSignalMessage(json);
+    } else if (type == 'answer') {
+      // handle answer from the peer
+      final answer = RTCSessionDescription(msg.sdp, type);
+      _pc!.setRemoteDescription(answer);
+    } else if (type == 'candidates') {
+      final candidate = RTCIceCandidate(
+          msg.candidate, msg.sdpMid, msg.sdpMLineIndex);
+      // add candidates from the peer
+      _pc!.addCandidate(candidate);
+    }
+  }
+
+  void _setDataChannelListeners(RTCDataChannel dc) {
+    dc.onDataChannelState = _onDataChannelState;
+    dc.onMessage = _onMessage;
+  }
+
+  void _onDataChannelState(RTCDataChannelState state) {
+    debugModePrint('onDataChannelState: $state');
+  }
+
+  void _onMessage(RTCDataChannelMessage data) async {
+    if (data.isBinary) {
+      // touch event data
+      if (_isTouchBackAllowed()) {
+        EventMessage eventMessage = EventMessage.fromBuffer(data.binary);
+        if (eventMessage.hasTouchEvent()) {
+          int action = FlutterInputInjection.TOUCH_POINT_START;
+          if (eventMessage.touchEvent.eventType ==
+              TouchEvent_TouchEventType.TOUCH_POINT_START) {
+            action = FlutterInputInjection.TOUCH_POINT_START;
+          } else if (eventMessage.touchEvent.eventType ==
+              TouchEvent_TouchEventType.TOUCH_POINT_MOVE) {
+            action = FlutterInputInjection.TOUCH_POINT_MOVE;
+          } else if (eventMessage.touchEvent.eventType ==
+              TouchEvent_TouchEventType.TOUCH_POINT_END) {
+            action = FlutterInputInjection.TOUCH_POINT_END;
+          }
+          int id = eventMessage.touchEvent.touchPoints[0].id;
+          double remoteX = eventMessage.touchEvent.touchPoints[0].x;
+          double remoteY = eventMessage.touchEvent.touchPoints[0].y;
+          await updateScreenSize();
+          int injectX = (remoteX * _screenWidth).toInt();
+          if (injectX < 0) {
+            injectX = 0;
+          } else if (injectX > _screenWidth.toInt() - 1) {
+            injectX = _screenWidth.toInt() - 1;
+          }
+          int injectY = (remoteY * _screenHeight).toInt();
+          if (injectY < 0) {
+            injectY = 0;
+          } else if (injectY > _screenHeight.toInt() - 1) {
+            injectY = _screenHeight.toInt() - 1;
+          }
+          _flutterInputInjectionPlugin.sendTouch(action, id, injectX, injectY);
+        }
+      }
+    } else {
+      // text message
+      debugModePrint(data.text);
+    }
+  }
+
+  void _onAddTrack(MediaStream stream, MediaStreamTrack track) {
+    debugModePrint('onAddTrack: ${track.kind}', type: runtimeType);
+  }
+
+  void _onSignalingState(RTCSignalingState state) {
+    debugModePrint(state, type: runtimeType);
+  }
+
+  void _onIceGatheringState(RTCIceGatheringState state) {
+    debugModePrint(state, type: runtimeType);
+  }
+
+  void _onIceConnectionState(RTCIceConnectionState state) {
+    debugModePrint(state, type: runtimeType);
+  }
+
+  void _onPeerConnectionState(RTCPeerConnectionState state) async {
+    debugModePrint(state, type: runtimeType);
+    if (state == RTCPeerConnectionState.RTCPeerConnectionStateConnected) {
+      var senders = await _pc!.getSenders();
+      var sender =
+      senders.firstWhereOrNull((sender) => sender.track?.kind == 'video');
+      var params = sender!.parameters;
+      params.degradationPreference = RTCDegradationPreference.DISABLED;
+      await sender.setParameters(params);
+    }
+  }
+
+  void _onCandidate(RTCIceCandidate candidate) {
+    debugModePrint('onCandidate: ${candidate.candidate}', type: runtimeType);
+
+    // send candidates to the peer
+    Map<String, dynamic> json = {
+      'type': 'candidates',
+      'sdp': '',
+      'candidate': candidate.candidate,
+      'sdpMid': candidate.sdpMid,
+      'sdpMLineIndex': candidate.sdpMLineIndex,
+    };
+    sendSignalMessage(json);
+  }
+
+  Future<dynamic> getUserAgent() async {
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String? version, type;
+    if (WebRTC.platformIsMacOS) {
+      final macOsInfo = await deviceInfo.macOsInfo;
+      version =
+      '${macOsInfo.majorVersion}.${macOsInfo.minorVersion}.${macOsInfo.patchVersion}';
+      type = 'Mac OS';
+    } else if (WebRTC.platformIsWindows) {
+      final windowsInfo = await deviceInfo.windowsInfo;
+      version = '${windowsInfo.productName} (Build ${windowsInfo.buildNumber})';
+      type = 'Windows';
+    } else {
+      // other platform
+    }
+    return {
+      'sdk': {'version': version, 'type': type},
+      'capabilities': {
+        'continualIceGathering': true,
+        'unifiedPlan': true,
+        'streamRemovable': true
+      }
+    };
+  }
+}
