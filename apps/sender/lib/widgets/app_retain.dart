bdc6189c3112536367991543e529ac10d3f75eaf
Loren Chang <loren.chang@tw.viewsonic.com>
2023-12-05 14:23:42 +0800

    Implement goes to background mechanism while press Back key in Android platform

---
+++ apps/sender/lib/widgets/app_retain.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+///
+/// https://blog.csdn.net/weixin_46156477/article/details/116779831
+///
+class AppRetain extends StatelessWidget {
+  const AppRetain({super.key, required this.child});
+
+  static const _androidAppRetain =
+      MethodChannel('com.viewsonic.display.cast/android_app_retain');
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (Platform.isAndroid) {
+          if (Navigator.of(context).canPop()) {
+            return Future.value(true);
+          } else {
+            try {
+              _showSnackBarMessage(
+                context,
+                'AirSync App goes to the background.',
+              );
+              await Future.delayed(const Duration(seconds: 1));
+              _androidAppRetain.invokeMethod('sendToBackground');
+              return Future.value(false);
+            } catch (e) {
+              print(e);
+              return Future.value(true);
+            }
+          }
+        } else {
+          return Future.value(true);
+        }
+      },
+      child: child,
+    );
+  }
+
+  _showSnackBarMessage(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(content: Text(message)),
+      );
+  }
+}
