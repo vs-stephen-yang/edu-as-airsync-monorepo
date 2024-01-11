11a55036882efdb88e81fce9e0afb6e051b9935a
Harvey Lin <harvey.lin@myviewboard.com>
2024-01-12 05:11:37 +0800

    feat: Add foreground service to support android screen capture

--- apps/receiver/lib/model/remote_screen_server.dart
+++ apps/receiver/lib/model/remote_screen_server.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+import 'dart:developer';
 
 import 'package:flutter_ion_sfu/flutter_ion_sfu.dart';
 import 'package:flutter_ion_sfu/flutter_ion_sfu_configuration.dart';
 import 'package:ion_sdk_flutter/flutter_ion.dart';
 import 'package:uuid/uuid.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_background/flutter_background.dart';
 
 class RemoteScreenServer {
 
@@ -38,6 +42,39 @@ class RemoteScreenServer {
       constraints.simulcast = false;
       constraints.audio = false;
       constraints.resolution = "fhd";
+
+      if (!kIsWeb && Platform.isAndroid) {
+        // Android specific
+        Future<void> requestBackgroundPermission() async {
+          // Required for android screen share.
+          try {
+            var hasPermissions = await FlutterBackground.hasPermissions;
+            const androidConfig = FlutterBackgroundAndroidConfig(
+              notificationTitle: 'Screen Sharing',
+              notificationText: 'AirSync is sharing the screen.',
+              notificationImportance: AndroidNotificationImportance.Default,
+              notificationIcon: AndroidResource(
+                name: 'ic_launcher',
+                defType: 'mipmap',
+              ),
+              // Above Android 12 will has some issue if set below option true.
+              shouldRequestBatteryOptimizationsOff: false,
+            );
+
+            hasPermissions = await FlutterBackground.initialize(
+                androidConfig: androidConfig);
+
+            if (hasPermissions &&
+                !FlutterBackground.isBackgroundExecutionEnabled) {
+              await FlutterBackground.enableBackgroundExecution();
+            }
+          } catch (e) {
+            log(e.toString());
+          }
+        }
+
+        await requestBackgroundPermission();
+      }
 
       var localStream = await LocalStream.getDisplayMedia(
           constraints: constraints
