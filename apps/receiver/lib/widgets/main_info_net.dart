8e688a10f9274822754d38597facd38424d9b90b
Loren Chang <loren.chang@tw.viewsonic.com>
2024-01-30 18:21:59 +0800

    refactor: refactor to using FocusIconButton to replace Icon+InkWell to show focus for remote controller.

--- apps/receiver/lib/widgets/main_info_net.dart
+++ apps/receiver/lib/widgets/main_info_net.dart
@@ -3,8 +3,10 @@ import 'dart:math' as math;
 
 import 'package:display_flutter/app_analytics.dart';
 import 'package:display_flutter/app_colors.dart';
+import 'package:display_flutter/app_ui_constant.dart';
 import 'package:display_flutter/generated/l10n.dart';
 import 'package:display_flutter/providers/channel_provider.dart';
+import 'package:display_flutter/widgets/focus_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -41,8 +43,6 @@ class _MainInfoInternetState extends State<MainInfoInternet> {
 
   @override
   Widget build(BuildContext context) {
-    ChannelProvider channelProvider = Provider.of<ChannelProvider>(context);
-
     return Container(
       alignment: Alignment.center,
       width: 400,
@@ -51,122 +51,127 @@ class _MainInfoInternetState extends State<MainInfoInternet> {
         borderRadius: BorderRadius.all(Radius.circular(20)),
         color: AppColors.primary_grey,
       ),
-      child: channelProvider.connectNet
-          ? Wrap(
-              direction: Axis.vertical,
-              alignment: WrapAlignment.center,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              spacing: 16,
-              children: <Widget>[
-                Text(
-                  S.of(context).main_content_display_code,
-                  style: const TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w700,
+      child: Consumer<ChannelProvider>(
+        builder: (context, channelProvider, child) {
+          return (channelProvider.connectNet)
+              ? Wrap(
+                  direction: Axis.vertical,
+                  alignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 16,
+                  children: <Widget>[
+                    Text(
+                      S.of(context).main_content_display_code,
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      _getDisplayCode(channelProvider.displayCode),
+                      style: const TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      S.of(context).main_content_one_time_password,
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    if (channelProvider.displayCode.isNotEmpty)
+                      ValueListenableBuilder<bool>(
+                        valueListenable: _isEyeOpen,
+                        builder: (context, eyeOpen, child) {
+                          if (_mGetOTPTimer == null) _generateOTP();
+                          return Wrap(
+                            direction: Axis.horizontal,
+                            alignment: WrapAlignment.center,
+                            crossAxisAlignment: WrapCrossAlignment.center,
+                            spacing: 16,
+                            children: <Widget>[
+                              ValueListenableBuilder<int>(
+                                valueListenable: _otp,
+                                builder: (context, otp, child) {
+                                  if (!channelProvider.otpList
+                                      .contains(_otp.value.toString())) {
+                                    channelProvider
+                                        .setOtpList(_otp.value.toString());
+                                  }
+                                  return Text(
+                                    eyeOpen ? otp.toString() : 'XXXX',
+                                    style: const TextStyle(
+                                      fontSize: 35,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  );
+                                },
+                              ),
+                              ValueListenableBuilder<int>(
+                                valueListenable: _countDownProgress,
+                                builder: (context, progress, child) {
+                                  return Transform(
+                                    alignment: Alignment.center,
+                                    transform: Matrix4.rotationY(math.pi),
+                                    child: SizedBox(
+                                      width: 26,
+                                      height: 26,
+                                      child: CircularProgressIndicator(
+                                        value: progress / maxCountDown,
+                                        strokeWidth: 4,
+                                        backgroundColor: Colors.black,
+                                        valueColor:
+                                            const AlwaysStoppedAnimation<Color>(
+                                                Colors.white),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                              FocusIconButton(
+                                icons: eyeOpen
+                                    ? Icons.remove_red_eye_sharp
+                                    : Icons.remove_red_eye_outlined,
+                                splashRadius: 20,
+                                focusColor: Colors.grey,
+                                hasFocusSize: AppUIConstant.iconNotFocusSize,
+                                notFocusSize: AppUIConstant.iconNotFocusSize,
+                                onClick: () {
+                                  AppAnalytics().trackEventAppOTPMaskClick();
+                                  _isEyeOpen.value = !_isEyeOpen.value;
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                  ],
+                )
+              : Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.wifi_off,
+                        color: AppColors.iconDisableStandbyBackground,
+                        size: 120,
+                      ),
+                      Text(
+                        S.of(context).main_status_no_network,
+                        style: const TextStyle(
+                          color: AppColors.iconDisableStandbyBackground,
+                          fontSize: 28,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                Text(
-                  _getDisplayCode(channelProvider.displayCode),
-                  style: const TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                Text(
-                  S.of(context).main_content_one_time_password,
-                  style: const TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                if (channelProvider.displayCode.isNotEmpty)
-                  ValueListenableBuilder(
-                    valueListenable: _isEyeOpen,
-                    builder: (BuildContext context, bool value, Widget? child) {
-                      if (_mGetOTPTimer == null) _generateOTP();
-                      return Wrap(
-                        direction: Axis.horizontal,
-                        alignment: WrapAlignment.center,
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        spacing: 16,
-                        children: <Widget>[
-                          ValueListenableBuilder<int>(
-                            valueListenable: _otp,
-                            builder:
-                                (BuildContext context, int otp, Widget? child) {
-                              if (!channelProvider.otpList
-                                  .contains(_otp.value.toString())) {
-                                channelProvider
-                                    .setOtpList(_otp.value.toString());
-                              }
-                              return Text(
-                                value ? otp.toString() : 'XXXX',
-                                style: const TextStyle(
-                                  fontSize: 35,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              );
-                            },
-                          ),
-                          ValueListenableBuilder(
-                            valueListenable: _countDownProgress,
-                            builder: (BuildContext context, int value,
-                                Widget? child) {
-                              return Transform(
-                                alignment: Alignment.center,
-                                transform: Matrix4.rotationY(math.pi),
-                                child: SizedBox(
-                                  width: 26,
-                                  height: 26,
-                                  child: CircularProgressIndicator(
-                                    value: value / maxCountDown,
-                                    strokeWidth: 4,
-                                    backgroundColor: Colors.black,
-                                    valueColor:
-                                        const AlwaysStoppedAnimation<Color>(
-                                            Colors.white),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                          InkWell(
-                            child: Icon(
-                              value
-                                  ? Icons.remove_red_eye_sharp
-                                  : Icons.remove_red_eye_outlined,
-                              size: 40,
-                            ),
-                            onTap: () {
-                              AppAnalytics().trackEventAppOTPMaskClick();
-                              _isEyeOpen.value = !_isEyeOpen.value;
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-              ],
-            )
-          : Column(
-              children: [
-                const Spacer(),
-                const Icon(
-                  Icons.wifi_off,
-                  color: AppColors.iconDisableStandbyBackground,
-                  size: 120,
-                ),
-                Text(
-                  S.of(context).main_status_no_network,
-                  style: const TextStyle(
-                    color: AppColors.iconDisableStandbyBackground,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                const Spacer(),
-              ],
-            ),
+                );
+        },
+      ),
     );
   }
 
