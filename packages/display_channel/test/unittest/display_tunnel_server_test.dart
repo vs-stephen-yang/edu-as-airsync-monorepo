ff74079343f6496a712a2b002926d09e037baea4
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-02-02 18:32:43 +0800

    test: add test cases

--- packages/display_channel/test/unittest/display_tunnel_server_test.dart
+++ packages/display_channel/test/unittest/display_tunnel_server_test.dart
@@ -1,19 +1,71 @@
 import 'package:display_channel/display_channel.dart';
+import 'package:display_channel/src/client_connection.dart';
+import 'package:display_channel/src/server/tunnel/tunnel_message.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class FakeClientConnection extends ClientConnection {
+  FakeClientConnection();
+
+  @override
+  void open() {}
+
+  @override
+  Future<void> close() {
+    return Future<void>.value();
+  }
+
+  @override
+  void send(Map<String, dynamic> message) {}
+}
+
 void main() {
-  test('stop should work', () async {
-    // arrange
-    final server = DisplayTunnelServer(
-      (String url) => WebSocketClientConnection(url),
-      (Channel channel) {},
-      (ConnectionRequest connectionRequest) => ConnectRequestStatus.success,
+  late DisplayTunnelServer server;
+  late FakeClientConnection connection;
+  Channel? channel;
+
+  injectTunnelConnectedMessage(String connectionId, String token) {
+    final message = TunnelClientConnected(
+      connectionId,
+      '100',
+      token,
+      'DEF',
     );
 
-    // action
+    connection.onMessage?.call(message.toJson());
+  }
+
+  setUp(() {
+    connection = FakeClientConnection();
+
+    server = DisplayTunnelServer(
+      (String url) => connection,
+      (Channel c) {
+        channel = c;
+        channel!.onStateChange = (state) {};
+      },
+      (ConnectionRequest connectionRequest) => ConnectRequestStatus.success,
+    );
+  });
+
+  test('stop should work', () async {
+    // arrange
     server.start('1000', 'wss://example.com/dev');
+
+    // action
     server.stop();
 
     // assert
+  });
+
+  test('a new channel should be created when connected is received', () async {
+    // arrange
+    server.start('1000', 'wss://example.com/dev');
+
+    // action
+    injectTunnelConnectedMessage('1000', 'token');
+
+    // assert
+    expect(channel, isNotNull);
+    expect(channel!.state, ChannelState.connected);
   });
 }
