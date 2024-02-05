2898d218fe5976f91a354fa411acfae8610efda4
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-02-05 09:09:58 +0800

    refactor: move logic to display_tunnel_server

--- packages/display_channel/lib/src/display_tunnel_server.dart
+++ packages/display_channel/lib/src/display_tunnel_server.dart
@@ -10,9 +10,15 @@ class DisplayTunnelServer extends ChannelServer {
   void Function()? onTunnelConnecting;
 
   TunnelConnectionServer? _tunnelServer;
+  ClientConnection? _tunnelConnection;
 
   Duration reconnectionTimeoutDuration;
   Timer? _reconnectionTimer;
+
+  // Tunnel Heartbeat
+  // Avoid disconnection caused by AWS WebSocket Idle Connection Timeout.
+  Timer? _heartbeatTimer;
+  final Duration heartbeatInterval;
 
   final CreateWebsocketClientConnection _createTunnelConnection;
 
@@ -21,47 +27,89 @@ class DisplayTunnelServer extends ChannelServer {
     OnNewChannel onNewChannel,
     VerifyConnectRequest verifyConnectRequest, {
     this.reconnectionTimeoutDuration = const Duration(seconds: 2),
+    // AWS WebSocket Idle Connection Timeout 10 minutes
+    this.heartbeatInterval = const Duration(minutes: 9),
   }) : super(
           onNewChannel,
           verifyConnectRequest,
         );
 
+  _enableHeartbeat(bool enable) {
+    if (enable) {
+      // Avoid disconnection caused by AWS WebSocket Idle Connection Timeout.
+      // https://docs.aws.amazon.com/apigateway/latest/developerguide/limits.html
+
+      _heartbeatTimer?.cancel();
+      _heartbeatTimer = Timer.periodic(heartbeatInterval, (Timer timer) {
+        _tunnelServer?.onHearbeatTick();
+      });
+    } else {
+      _heartbeatTimer?.cancel();
+      _heartbeatTimer = null;
+    }
+  }
+
+  _enableReconnectionTimer(bool enable) {
+    if (enable) {
+      _reconnectionTimer ??= Timer(reconnectionTimeoutDuration, () {
+        // if the tunnel connection is not re-established within the timeout,
+        // close all of the channels
+        super.closeAllChannels();
+      });
+    } else {
+      _reconnectionTimer?.cancel();
+      _reconnectionTimer = null;
+    }
+  }
+
   void start(
     String instanceId,
     String tunnelServiceUrl,
   ) {
+    final uri = Uri.parse(tunnelServiceUrl);
+
+    final uriWithParameters = uri.replace(queryParameters: {
+      'role': 'server',
+      'instanceId': instanceId,
+    });
+
+    final connection = _createTunnelConnection(
+      uriWithParameters.toString(),
+    );
+
+    connection.onConnected = () {
+      _enableReconnectionTimer(false);
+
+      onTunnelConnected?.call();
+    };
+
+    connection.onDisconnected = () {
+      _enableReconnectionTimer(true);
+    };
+
+    connection.onConnecting = () {
+      onTunnelConnecting?.call();
+    };
+
     _tunnelServer = TunnelConnectionServer(
-      instanceId,
-      tunnelServiceUrl,
-      (String url) => _createTunnelConnection(url),
+      connection,
       (String clientId, connection) =>
           handleNewConnection(clientId, connection),
       (ConnectionRequest connectionRequest) =>
           verifyConnectionRequest(connectionRequest),
     );
 
-    _tunnelServer!.onTunnelConnected = () {
-      _reconnectionTimer?.cancel();
-      _reconnectionTimer = null;
+    connection.open();
 
-      onTunnelConnected?.call();
-    };
-
-    _tunnelServer!.onTunnelConnecting = () {
-      onTunnelConnecting?.call();
-
-      _reconnectionTimer ??= Timer(reconnectionTimeoutDuration, () {
-        // the tunnel connection is not re-established within the timeout
-        // close all of the channels
-        super.closeAllChannels();
-      });
-    };
-
-    _tunnelServer!.start();
+    _tunnelConnection = connection;
   }
 
   @override
   void stop() {
-    _tunnelServer?.stop();
+    _enableReconnectionTimer(false);
+
+    _enableHeartbeat(false);
+
+    _tunnelConnection?.close();
   }
 }
