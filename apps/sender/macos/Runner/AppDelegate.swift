665b70e95d5466db34022d76881d7accd7be9b52
Zoey Chen <zoey.chen@myviewboard.com>
2024-02-05 10:57:17 +0800

    feat: remove AudioSwitch

--- apps/sender/macos/Runner/AppDelegate.swift
+++ apps/sender/macos/Runner/AppDelegate.swift
@@ -3,11 +3,6 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
-  
-    override func applicationDidFinishLaunching(_ notification: Notification) {
-        let controller: FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
-        AudioSwitch.register(with: controller.registrar(forPlugin: "audio_switch"))
-    }
     
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
