1fe748257401558dfba5de03332c0ce699d68221
Loren Chang <loren.chang@tw.viewsonic.com>
2024-03-25 15:32:23 +0800

    refactor: move version number information into "Settings" menu

--- apps/sender/lib/widgets/title_bar.dart
+++ apps/sender/lib/widgets/title_bar.dart
@@ -1,4 +1,3 @@
-import 'package:display_cast_flutter/settings/app_config.dart';
 import 'package:flutter/material.dart';
 
 class TitleBar extends StatelessWidget {
@@ -6,13 +5,15 @@ class TitleBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    AppConfig? appConfig = AppConfig.of(context);
     return Container(
       padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
       width: 300,
       child: Row(
         children: [
-          Image.asset('assets/images/ic_launcher.png', height: 46,),
+          Image.asset(
+            'assets/images/ic_launcher.png',
+            height: 46,
+          ),
           const Padding(
             padding: EdgeInsets.only(left: 10),
             child: Text(
@@ -25,13 +26,6 @@ class TitleBar extends StatelessWidget {
             ),
           ),
           const Padding(padding: EdgeInsets.all(5.0)),
-          Text(
-            'Ver ${appConfig?.appVersion}',
-            style: const TextStyle(
-              color: Colors.white38,
-              fontSize: 12,
-            ),
-          ),
         ],
       ),
     );
