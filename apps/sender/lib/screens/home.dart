80f824f3af597c3571f2d6de91c171d89e7de02f
Loren Chang <loren.chang@tw.viewsonic.com>
2024-03-25 13:48:47 +0800

    fix: add SafeArea widget to prevent App name block by "Dynamic Island".

--- apps/sender/lib/screens/home.dart
+++ apps/sender/lib/screens/home.dart
@@ -40,99 +40,101 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return AppRetain(
-      child: Scaffold(
-        body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Container(
-            decoration: const BoxDecoration(
-                gradient: RadialGradient(
-                    center: Alignment(0, -0.7),
-                    radius: 1,
-                    colors: [
-                  AppColors.homeBackground,
-                  Colors.black,
-                ])),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                const Positioned(
-                  left: 0,
-                  right: 0,
-                  top: 0,
-                  child: TitleBar(),
-                ),
-                const Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: BottomBar(),
-                ),
-                Consumer2<ChannelProvider, DemoProvider>(builder: (context, channel, demo, child) {
-                  if (!demo.isDemoMode) {
-                    debugModePrint('PresentState: ${channel.state}');
-                    if (!kIsWeb) {
-                      FlutterWindowClose.setWindowShouldCloseHandler(() async {
-                        await channel.presentStop();
-                        await channel.presentEnd(goIdleState: false);
-                        return true;
-                      });
-                    }
-                    switch (channel.state) {
-                      case ViewState.idle:
-                        return PresentIdle();
-                      case ViewState.selectRole:
-                        if (kIsWeb) {
-                          WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-                            channel.currentRole = JoinIntentType.present;
-                            if (channel.moderatorStatus) {
-                              channel.presentModeratorNamePage();
-                            } else {
-                              channel.beginBasicMode();
-                            }
-                          });
+      child: SafeArea(
+        child: Scaffold(
+          body: ConstrainedBox(
+            constraints: const BoxConstraints.expand(),
+            child: Container(
+              decoration: const BoxDecoration(
+                  gradient: RadialGradient(
+                      center: Alignment(0, -0.7),
+                      radius: 1,
+                      colors: [
+                        AppColors.homeBackground,
+                        Colors.black,
+                      ])),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  const Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    child: TitleBar(),
+                  ),
+                  const Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: BottomBar(),
+                  ),
+                  Consumer2<ChannelProvider, DemoProvider>(builder: (context, channel, demo, child) {
+                    if (!demo.isDemoMode) {
+                      debugModePrint('PresentState: ${channel.state}');
+                      if (!kIsWeb) {
+                        FlutterWindowClose.setWindowShouldCloseHandler(() async {
+                          await channel.presentStop();
+                          await channel.presentEnd(goIdleState: false);
+                          return true;
+                        });
+                      }
+                      switch (channel.state) {
+                        case ViewState.idle:
+                          return PresentIdle();
+                        case ViewState.selectRole:
+                          if (kIsWeb) {
+                            WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+                              channel.currentRole = JoinIntentType.present;
+                              if (channel.moderatorStatus) {
+                                channel.presentModeratorNamePage();
+                              } else {
+                                channel.beginBasicMode();
+                              }
+                            });
+                            return const SizedBox();
+                          } else {
+                            return const PresentSelectRole();
+                          }
+                        case ViewState.moderatorName:
+                          return const ModeratorIdle();
+                        case ViewState.moderatorWait:
+                          return const ModeratorWait();
+                        case ViewState.waitReady:
+                          return const PresentWaitReady();
+                        case ViewState.selectScreen:
+                          return const PresentSelectScreen();
+                        case ViewState.presentStart:
+                          final widget = PresentPresentStart(countStartTime: WebOnHiddenHelper.getInstance().getOnHiddenTimestamp());
+                          WebOnHiddenHelper.getInstance().resetHiddenTime();
+                          return widget;
+                        case ViewState.moderatorStart:
+                          final widget = ModeratorPresentStart(countStartTime: WebOnHiddenHelper.getInstance().getOnHiddenTimestamp());
+                          WebOnHiddenHelper.getInstance().resetHiddenTime();
+                          return widget;
+                        case ViewState.remoteScreen:
+                          return const RemoteScreenWidget();
+                        case ViewState.settings:
+                          return const Settings();
+                        case ViewState.language:
+                          return const Language();
+                        default:
                           return const SizedBox();
-                        } else {
-                          return const PresentSelectRole();
-                        }
-                      case ViewState.moderatorName:
-                        return const ModeratorIdle();
-                      case ViewState.moderatorWait:
-                        return const ModeratorWait();
-                      case ViewState.waitReady:
-                        return const PresentWaitReady();
-                      case ViewState.selectScreen:
-                        return const PresentSelectScreen();
-                      case ViewState.presentStart:
-                        final widget = PresentPresentStart(countStartTime: WebOnHiddenHelper.getInstance().getOnHiddenTimestamp());
-                        WebOnHiddenHelper.getInstance().resetHiddenTime();
-                        return widget;
-                      case ViewState.moderatorStart:
-                        final widget = ModeratorPresentStart(countStartTime: WebOnHiddenHelper.getInstance().getOnHiddenTimestamp());
-                        WebOnHiddenHelper.getInstance().resetHiddenTime();
-                        return widget;
-                      case ViewState.remoteScreen:
-                        return const RemoteScreenWidget();
-                      case ViewState.settings:
-                        return const Settings();
-                      case ViewState.language:
-                        return const Language();
-                      default:
-                        return const SizedBox();
+                      }
+                    } else {
+                      switch (demo.state) {
+                        case DemoViewState.off:
+                          return const SizedBox();
+                        case DemoViewState.selectRole:
+                          return const PresentSelectRoleDemo();
+                        case DemoViewState.presentStart:
+                          return PresentPresentStartDemo();
+                        case DemoViewState.remoteScreen:
+                          return const RemoteScreenDemo();
+                      }
                     }
-                  } else {
-                    switch (demo.state) {
-                      case DemoViewState.off:
-                        return const SizedBox();
-                      case DemoViewState.selectRole:
-                        return const PresentSelectRoleDemo();
-                      case DemoViewState.presentStart:
-                        return PresentPresentStartDemo();
-                      case DemoViewState.remoteScreen:
-                        return const RemoteScreenDemo();
-                    }
-                  }
-                }),
-              ],
+                  }),
+                ],
+              ),
             ),
           ),
         ),
