1ff736ce60c74ae527a47a2f64f7be73ac7144a6
Loren Chang <loren.chang@tw.viewsonic.com>
2024-03-28 17:47:27 +0800

    refactor: refactor webrtc_view to using inner method and remove unused code.

--- apps/receiver/lib/widgets/webrtc_view_new.dart
+++ apps/receiver/lib/widgets/webrtc_view_new.dart
@@ -1,4 +1,3 @@
-
 import 'dart:typed_data';
 import 'dart:ui';
 
@@ -29,32 +28,18 @@ class WebRTCView extends StatefulWidget {
 }
 
 class WebRTCViewState extends State<WebRTCView> {
-  bool _showConnectionInfo = false;
   final GlobalKey _widgetKey = GlobalKey();
   bool _textureSizeChanged = true;
   Size _textureSize = const Size(0, 0);
   Offset _textureOffset = const Offset(0, 0);
-  RTCConnector? _rtcConnector;
   GlobalKey repaintBoundaryKey = GlobalKey();
   GlobalKey<PauseScreenImageState> pauseScreenImageKey = GlobalKey();
-  late ChannelProvider channelProvider;
   Widget? pauseScreenImage;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   void deactivate() {
     pauseScreenImageKey.currentState?.clearImage();
-    _rtcConnector = null;
     super.deactivate();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 
   void _getTextureInfo() {
@@ -75,7 +60,7 @@ class WebRTCViewState extends State<WebRTCView> {
       return;
     } else {
       final RenderBox renderBox =
-      textureElement!.findRenderObject() as RenderBox;
+          textureElement!.findRenderObject() as RenderBox;
       _textureSize = renderBox.size;
       _textureOffset = renderBox.localToGlobal(Offset.zero);
       printInDebug(
@@ -84,185 +69,181 @@ class WebRTCViewState extends State<WebRTCView> {
     }
   }
 
-  void _onTouchStart(PointerEvent event) {
-    _onTouchEvent(TouchEvent_TouchEventType.TOUCH_POINT_START, event);
-  }
-
-  void _onTouchMove(PointerEvent event) {
-    _onTouchEvent(TouchEvent_TouchEventType.TOUCH_POINT_MOVE, event);
-  }
-
-  void _onTouchEnd(PointerEvent event) {
-    _onTouchEvent(TouchEvent_TouchEventType.TOUCH_POINT_END, event);
-  }
-
-  void _onTouchEvent(TouchEvent_TouchEventType eventType, PointerEvent event) {
-    if (_textureSizeChanged) {
-      _getTextureInfo();
-    }
-
-    final curTouchEventPoint = TouchEventPoint();
-    curTouchEventPoint.x =
-        (event.position.dx - _textureOffset.dx) / _textureSize.width;
-    /* make curTouchEventPoint.x between 0.0 ~ 1.0 */
-    if (curTouchEventPoint.x < 0.0) {
-      curTouchEventPoint.x = 0.0;
-    } else if (curTouchEventPoint.x > 1.0) {
-      curTouchEventPoint.x = 1.0;
-    }
-    curTouchEventPoint.y =
-        (event.position.dy - _textureOffset.dy) / _textureSize.height;
-    /* make curTouchEventPoint.y between 0.0 ~ 1.0 */
-    if (curTouchEventPoint.y < 0.0) {
-      curTouchEventPoint.y = 0.0;
-    } else if (curTouchEventPoint.y > 1.0) {
-      curTouchEventPoint.y = 1.0;
-    }
-
-    curTouchEventPoint.id = event.pointer;
-
-    final curTouchEvent = TouchEvent();
-    curTouchEvent.eventType = eventType;
-    curTouchEvent.touchPoints.add(curTouchEventPoint);
-
-    final curEventMessage = EventMessage();
-    curEventMessage.touchEvent = curTouchEvent;
-
-    _rtcConnector?.sendRTCData(curEventMessage.writeToBuffer());
-  }
-
   @override
   Widget build(BuildContext context) {
-    channelProvider = Provider.of<ChannelProvider>(context);
-    _rtcConnector =
+    // When create widget in Home has check connection is RTCConnector.
+    RTCConnector rtcConnector =
         HybridConnectionList().getConnection<RTCConnector>(widget.index);
-    if (_rtcConnector?.presentationState == PresentationState.pauseStreaming && pauseScreenImage == null) {
-      pauseVideo();
-    } else if (_rtcConnector?.presentationState == PresentationState.resumeStreaming) {
-      resumeVideo();
-      _rtcConnector?.presentationState = PresentationState.streaming;
+
+    onTouchEvent(TouchEvent_TouchEventType eventType, PointerEvent event) {
+      if (_textureSizeChanged) {
+        _getTextureInfo();
+      }
+
+      final curTouchEventPoint = TouchEventPoint();
+      curTouchEventPoint.x =
+          (event.position.dx - _textureOffset.dx) / _textureSize.width;
+      /* make curTouchEventPoint.x between 0.0 ~ 1.0 */
+      if (curTouchEventPoint.x < 0.0) {
+        curTouchEventPoint.x = 0.0;
+      } else if (curTouchEventPoint.x > 1.0) {
+        curTouchEventPoint.x = 1.0;
+      }
+      curTouchEventPoint.y =
+          (event.position.dy - _textureOffset.dy) / _textureSize.height;
+      /* make curTouchEventPoint.y between 0.0 ~ 1.0 */
+      if (curTouchEventPoint.y < 0.0) {
+        curTouchEventPoint.y = 0.0;
+      } else if (curTouchEventPoint.y > 1.0) {
+        curTouchEventPoint.y = 1.0;
+      }
+
+      curTouchEventPoint.id = event.pointer;
+
+      final curTouchEvent = TouchEvent();
+      curTouchEvent.eventType = eventType;
+      curTouchEvent.touchPoints.add(curTouchEventPoint);
+
+      final curEventMessage = EventMessage();
+      curEventMessage.touchEvent = curTouchEvent;
+
+      rtcConnector.sendRTCData(curEventMessage.writeToBuffer());
     }
 
-    String presenterName = _rtcConnector?.senderName ?? '';
-    if (presenterName.length > 10) {
-      presenterName = '${presenterName.substring(0, 10)}..';
-    }
-    return Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        RepaintBoundary(
-          key: repaintBoundaryKey,
-          child: Focus(
-            descendantsAreFocusable: false,
-            canRequestFocus: false,
-            child: NotificationListener<SizeChangedLayoutNotification>(
-              onNotification: (notification) {
-                printInDebug('onVideoWidgetResize');
-                _textureSizeChanged = true;
-                return false;
-              },
-              child: Listener(
-                onPointerDown: _onTouchStart,
-                onPointerMove: _onTouchMove,
-                onPointerUp: _onTouchEnd,
-                child: _rtcConnector != null? RTCVideoView(_rtcConnector!.remoteRenderer!, key: _widgetKey) : const SizedBox(),
-              ),
-            ),
-          ),
-        ),
-        if (_rtcConnector != null && _rtcConnector?.presentationState == PresentationState.pauseStreaming)
-          pauseScreenImage = pauseScreenImage ?? PauseScreenImage(key: pauseScreenImageKey),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Visibility(
-            visible: _rtcConnector?.presentationState == PresentationState.streaming &&
-                presenterName.isNotEmpty,
-            child: ConstrainedBox(
-              constraints: const BoxConstraints.tightFor(height: 30,width: 160),
-              child: Container(
-                padding: const EdgeInsets.all(5),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: AppColors.primaryBlackA50,
-                ),
-                child: AutoSizeText(
-                  presenterName,
-                  style: const TextStyle(fontSize: 20),
-                  maxLines: 1,
+    return Consumer<ChannelProvider>(builder: (_, channelProvider, __) {
+      // Currently rtcConnector will call channelProvider's notifyListeners
+      // to update PresentationState
+      if (rtcConnector.presentationState == PresentationState.pauseStreaming &&
+          pauseScreenImage == null) {
+        pauseVideo();
+      } else if (rtcConnector.presentationState ==
+          PresentationState.resumeStreaming) {
+        resumeVideo();
+        rtcConnector.presentationState = PresentationState.streaming;
+      }
+
+      String presenterName = rtcConnector.senderName ?? '';
+      if (presenterName.length > 10) {
+        presenterName = '${presenterName.substring(0, 10)}..';
+      }
+      return Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          RepaintBoundary(
+            key: repaintBoundaryKey,
+            child: Focus(
+              descendantsAreFocusable: false,
+              canRequestFocus: false,
+              child: NotificationListener<SizeChangedLayoutNotification>(
+                onNotification: (notification) {
+                  printInDebug('onVideoWidgetResize');
+                  _textureSizeChanged = true;
+                  return false;
+                },
+                child: Listener(
+                  onPointerDown: (PointerEvent event) {
+                    onTouchEvent(
+                        TouchEvent_TouchEventType.TOUCH_POINT_START, event);
+                  },
+                  onPointerMove: (PointerEvent event) {
+                    onTouchEvent(
+                        TouchEvent_TouchEventType.TOUCH_POINT_MOVE, event);
+                  },
+                  onPointerUp: (PointerEvent event) {
+                    onTouchEvent(
+                        TouchEvent_TouchEventType.TOUCH_POINT_END, event);
+                  },
+                  child: RTCVideoView(rtcConnector.remoteRenderer!,
+                      key: _widgetKey),
                 ),
               ),
             ),
           ),
-        ),
-        if (_rtcConnector != null && _rtcConnector?.presentationState == PresentationState.waitForStream)
-          Transform.scale(
-            scale: SplitScreen.mapSplitScreen.value[keySplitScreenCount] > 1
-                ? 0.5
-                : 1.0,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width / 2,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Visibility(
-                    visible: channelProvider.isModeratorMode,
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          S.of(context).main_wait_up_next,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 25,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          presenterName,
-                          style: const TextStyle(
-                            color: AppColors.primary_blue,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 30,
-                          ),
-                        ),
-                      ],
-                    ),
+          if (rtcConnector.presentationState ==
+              PresentationState.pauseStreaming)
+            pauseScreenImage =
+                pauseScreenImage ?? PauseScreenImage(key: pauseScreenImageKey),
+          if (rtcConnector.presentationState == PresentationState.streaming &&
+              presenterName.isNotEmpty)
+            Align(
+              alignment: Alignment.topCenter,
+              child: ConstrainedBox(
+                constraints:
+                    const BoxConstraints.tightFor(height: 30, width: 160),
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.primaryBlackA50,
                   ),
-                  const SizedBox(
-                    height: 20,
+                  child: AutoSizeText(
+                    presenterName,
+                    style: const TextStyle(fontSize: 20),
+                    maxLines: 1,
                   ),
-                  const SizedBox(
-                    width: 32,
-                    height: 32,
-                    child: LoadingIcon(),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    S.of(context).main_wait_title,
-                    style: const TextStyle(
-                      color: AppColors.primary_blue,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 25,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-      ],
-    );
-  }
-
-  bool get showConnectionInfo => _showConnectionInfo;
-
-  set showConnectionInfo(bool value) {
-    setState(() {
-      _showConnectionInfo = value;
+          if (rtcConnector.presentationState == PresentationState.waitForStream)
+            Transform.scale(
+              scale: SplitScreen.mapSplitScreen.value[keySplitScreenCount] > 1
+                  ? 0.5
+                  : 1.0,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    if (channelProvider.isModeratorMode)
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            S.of(context).main_wait_up_next,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 25,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            presenterName,
+                            style: const TextStyle(
+                              color: AppColors.primary_blue,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const SizedBox(
+                      width: 32,
+                      height: 32,
+                      child: LoadingIcon(),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      S.of(context).main_wait_title,
+                      style: const TextStyle(
+                        color: AppColors.primary_blue,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      );
     });
   }
 
@@ -323,6 +304,8 @@ class PauseScreenImageState extends State<PauseScreenImage> {
 
   @override
   Widget build(BuildContext context) {
-    return _capturedImage != null? Image.memory(_capturedImage!, fit:BoxFit.fill): const SizedBox();
+    return _capturedImage != null
+        ? Image.memory(_capturedImage!, fit: BoxFit.fill)
+        : const SizedBox();
   }
 }
