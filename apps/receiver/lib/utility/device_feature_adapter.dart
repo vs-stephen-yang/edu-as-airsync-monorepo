01685ef117e1a5805f736b8e6ea776c7f1304af8
BenjaminLee <benjamin.lee@myviewboard.com>
2024-04-18 11:51:35 +0800

    feat: Add software decode toggle button in debug switch

--- apps/receiver/lib/utility/device_feature_adapter.dart
+++ apps/receiver/lib/utility/device_feature_adapter.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DeviceFeatureAdapter {
 
@@ -13,6 +14,8 @@ class DeviceFeatureAdapter {
 
   // passes the instantiation to the _instance object
   factory DeviceFeatureAdapter() => _instance;
+
+  static bool UseSoftwareDecode = false;
 
   static Map<String, dynamic> deviceOptions = {
     '52-1C': {
@@ -27,12 +30,32 @@ class DeviceFeatureAdapter {
     },
   };
 
+  static dynamic softwareDecodeOptions = {
+    "maxHardwareDecodeSession": 0
+  };
+
+  static save() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("UseSoftwareDecode", UseSoftwareDecode);
+  }
+
+  static load() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    UseSoftwareDecode = prefs.getBool("UseSoftwareDecode") ?? false;
+  }
+
   static ensureInitialized() async {
-    String model = await _instance._loadModel() ?? '';
-    for (var entry in deviceOptions.entries) {
-      if (model.contains(entry.key)) {
-        await WebRTC.initialize(options: entry.value);
-        break;
+    await load();
+
+    if (UseSoftwareDecode) {
+      await WebRTC.initialize(options: softwareDecodeOptions);
+    } else {
+      String model = await _instance._loadModel() ?? '';
+      for (var entry in deviceOptions.entries) {
+        if (model.contains(entry.key)) {
+          await WebRTC.initialize(options: entry.value);
+          break;
+        }
       }
     }
   }
