233932d089aaa5daf3ef2116212dfc4e167c641c
Zoey Chen <zoey.chen@myviewboard.com>
2024-04-25 15:44:40 +0800

    feat: Add device list (p9): add version limit and get ip from display code

--- apps/sender/lib/providers/device_list_provider.dart
+++ apps/sender/lib/providers/device_list_provider.dart
@@ -1,33 +1,42 @@
 
+import 'dart:async';
+
 import 'package:bonsoir/bonsoir.dart';
 import 'package:display_cast_flutter/model/airsync_bonsoir_service.dart';
 import 'package:display_cast_flutter/model/discover_services.dart';
+import 'package:display_channel/display_channel.dart';
 import 'package:flutter/material.dart';
 
 class DeviceListProvider with ChangeNotifier {
 
   DiscoverServices discoverServices = DiscoverServices();
 
-  Future<void> startDiscovery() async {
-    await discoverServices.startDiscovery((BonsoirDiscoveryEvent event) async {
+   void startDiscovery(String versionPostfix) {
+
+    discoverServices.startDiscovery((BonsoirDiscoveryEvent event) {
       if (event.type == BonsoirDiscoveryEventType.discoveryServiceFound) {
-        print('Service found : ${event.service?.toJson()}');
-        await discoverServices.resolveService(event.service!);
-      } else if (event.type == BonsoirDiscoveryEventType.discoveryServiceResolved) {
-        print('Service resolved : ${event.service?.toJson()}');
-        if (event.service is ResolvedBonsoirService) {
-          String? ip = await discoverServices.lookupIpAddress((event.service as ResolvedBonsoirService).host!);
-          addDevice(AirSyncBonsoirService(
-              uuid: event.service!.attributes['uuid'] ?? '',
-              name: event.service!.name,
-              type: event.service!.type,
-              host: (event.service as ResolvedBonsoirService).host!,
-              ip: ip ?? '',
-              port: event.service!.port));
+        if (event.service!.attributes['ver'] != null) {
+          String serviceVersionPostfix = getVersionSuffix(event.service!.attributes['ver']!);
+          if (versionPostfix == serviceVersionPostfix) {
+            if (event.service!.attributes['displayCode'] != null && event.service!.attributes['displayCode']!.isNotEmpty) {
+              String dc = event.service!.attributes['displayCode']!;
+              DisplayCode displayCode = decodeDisplayCode(dc);
+              addDevice(AirSyncBonsoirService(
+                  uuid: event.service!.attributes['uuid'] ?? '',
+                  name: event.service!.attributes['fn'] ?? 'AirSync',
+                  type: event.service!.type,
+                  host: dc,
+                  ip: displayCode.ipAddress ?? '',
+                  port: 5100));
+            }
+          }
         }
+      } else if (event.type == BonsoirDiscoveryEventType.discoveryServiceResolved) {
+
       } else if (event.type == BonsoirDiscoveryEventType.discoveryServiceLost) {
-        print('Service lost : ${event.service?.toJson()}');
-        removeDevice(event.service!.attributes['uuid']);
+        if (event.service!.attributes.containsValue('AirSync')) {
+          removeDevice(event.service!.attributes['uuid']);
+        }
       }
     });
   }
@@ -40,6 +49,7 @@ class DeviceListProvider with ChangeNotifier {
   List<AirSyncBonsoirService> get devices => _devices;
 
   void addDevice(AirSyncBonsoirService device) {
+    _devices.removeWhere((element) => element.ip == device.ip);
     _devices.add(device);
     notifyListeners();
   }
@@ -51,6 +61,15 @@ class DeviceListProvider with ChangeNotifier {
 
   void clearDevices() {
     _devices.clear();
+  }
+
+  String getVersionSuffix(String version) {
+    int dashIndex = version.lastIndexOf("-");
+    if (dashIndex != -1) {
+      return version.substring(dashIndex);
+    } else {
+      return "";
+    }
   }
 
 }
