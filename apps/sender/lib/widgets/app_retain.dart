82c2ed2b335c25bbbbabe0177c324dcddbdc71a5
Loren Chang <loren.chang@tw.viewsonic.com>
2024-05-09 17:18:21 +0800

    lint: remove lint warnings (WillPopScope->PopScope)

--- apps/sender/lib/widgets/app_retain.dart
+++ apps/sender/lib/widgets/app_retain.dart
@@ -18,27 +18,22 @@ class AppRetain extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (!kIsWeb && Platform.isAndroid) {
-          if (Navigator.of(context).canPop()) {
-            return Future.value(true);
-          } else {
-            try {
-              _showSnackBarMessage(
-                context,
-                'AirSync App goes to the background.',
-              );
-              await Future.delayed(const Duration(seconds: 1));
-              _androidAppRetain.invokeMethod('sendToBackground');
-              return Future.value(false);
-            } catch (e) {
-              debugModePrint(e);
-              return Future.value(true);
-            }
-          }
-        } else {
-          return Future.value(true);
+    return PopScope(
+      canPop: (!kIsWeb && Platform.isAndroid) ? false : true,
+      onPopInvoked: (didPop) async {
+        debugModePrint('PopScope didPop: $didPop', type: runtimeType);
+        if (didPop) {
+          return;
+        }
+        try {
+          _showSnackBarMessage(
+            context,
+            'AirSync App goes to the background.',
+          );
+          await Future.delayed(const Duration(seconds: 1));
+          _androidAppRetain.invokeMethod('sendToBackground');
+        } catch (e) {
+          debugModePrint(e);
         }
       },
       child: child,
