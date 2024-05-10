e481fad0da5c23faa03b6bfc7921ff17eb18e442
Loren Chang <loren.chang@tw.viewsonic.com>
2024-05-10 17:07:44 +0800

    refactor: present select role widget to using same code (role widget).

--- apps/sender/lib/widgets/present_select_role.dart
+++ apps/sender/lib/widgets/present_select_role.dart
@@ -1,94 +1,91 @@
 import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/channel_provider.dart';
 import 'package:display_cast_flutter/utilities/app_analytics.dart';
+import 'package:display_cast_flutter/utilities/app_constants.dart';
 import 'package:display_channel/display_channel.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:display_cast_flutter/utilities/app_constants.dart';
 
 class PresentSelectRole extends StatelessWidget {
   const PresentSelectRole({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ChannelProvider>(
-        builder: (context, channelProvider, _) => Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (!kIsWeb)
-                  InkWell(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                            padding: const EdgeInsets.all(36),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white),
-                              borderRadius: BorderRadius.circular(8),
-                              // color: Colors.transparent,
-                            ),
-                            child: const Image(
-                              image: Svg('assets/images/ic_receiver.svg'),
-                            )),
-                        const Padding(padding: EdgeInsets.all(5)),
-                        Text(
-                          S.of(context).present_role_receive,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: AppConstants.fontSizeTitle,
-                          ),
-                        )
-                      ],
-                    ),
-                    onTap: () {
-                      AppAnalytics.instance
-                          .trackEvent('remote_screen_selected');
+    ChannelProvider channelProvider =
+        Provider.of<ChannelProvider>(context, listen: false);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RoleItem(
+          name: S.of(context).present_role_receive,
+          iconWidget: const Image(image: Svg('assets/images/ic_receiver.svg')),
+          onTap: () {
+            AppAnalytics.instance.trackEvent('remote_screen_selected');
 
-                      channelProvider.currentRole = JoinIntentType.remoteScreen;
-                      channelProvider.presentModeratorNamePage();
-                    },
-                  ),
-                const Padding(padding: EdgeInsets.all(10)),
-                InkWell(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                          padding: const EdgeInsets.all(36),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white),
-                            borderRadius: BorderRadius.circular(8),
-                            // color: Colors.transparent,
-                          ),
-                          child: const Image(
-                            image: Svg('assets/images/ic_cast_screen.svg'),
-                          )),
-                      const Padding(padding: EdgeInsets.all(5)),
-                      Text(
-                        S.of(context).present_role_cast_screen,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: AppConstants.fontSizeTitle,
-                        ),
-                      )
-                    ],
-                  ),
-                  onTap: () async {
-                    AppAnalytics.instance.trackEvent('present_selected');
+            channelProvider.currentRole = JoinIntentType.remoteScreen;
+            channelProvider.presentModeratorNamePage();
+          },
+        ),
+        const SizedBox(width: 10),
+        RoleItem(
+          name: S.of(context).present_role_cast_screen,
+          iconWidget:
+              const Image(image: Svg('assets/images/ic_cast_screen.svg')),
+          onTap: () async {
+            AppAnalytics.instance.trackEvent('present_selected');
 
-                    channelProvider.currentRole = JoinIntentType.present;
-                    if (channelProvider.moderatorStatus) {
-                      channelProvider.presentModeratorNamePage();
-                    } else {
-                      await channelProvider.beginBasicMode();
-                    }
-                  },
-                ),
-              ],
-            ));
+            channelProvider.currentRole = JoinIntentType.present;
+            if (channelProvider.moderatorStatus) {
+              channelProvider.presentModeratorNamePage();
+            } else {
+              await channelProvider.beginBasicMode();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class RoleItem extends StatelessWidget {
+  const RoleItem(
+      {super.key,
+      required this.name,
+      required this.iconWidget,
+      required this.onTap});
+
+  final String name;
+  final Widget iconWidget;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(36),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(8),
+              // color: Colors.transparent,
+            ),
+            child: iconWidget,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: AppConstants.fontSizeTitle,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
