20a7f17f74fc4fbd8858f69e2b9422647c80a4b2
Loren Chang <loren.chang@tw.viewsonic.com>
2024-05-21 12:06:33 +0800

    refactor: fine tune ui position for mobile device's landscape mode. (prevent "Pause" and "Stop" overlap on "build by" logo)

--- apps/sender/lib/widgets/moderator_present_start.dart
+++ apps/sender/lib/widgets/moderator_present_start.dart
@@ -13,93 +13,86 @@ class ModeratorPresentStart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ChannelProvider channelProvider = Provider.of<ChannelProvider>(context);
-    var textStyle30 = const TextStyle(color: Colors.white, fontSize: 28);
-    return Column(
+    ChannelProvider channelProvider =
+        Provider.of<ChannelProvider>(context, listen: false);
+    var textStyle28 = const TextStyle(color: Colors.white, fontSize: 28);
+    var textStyle14 = const TextStyle(
+        color: Colors.white, fontSize: AppConstants.fontSizeNormal);
+    return Stack(
       children: [
-        const Expanded(flex: 2, child: SizedBox()),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
-          child: Text(
-            S.of(context).present_time,
-            style: textStyle30,
-          ),
-        ),
-        const PresentTimer(),
-        Expanded(
-          flex: 3,
+        ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 38.0),
-                  child: ValueListenableBuilder(
-                    valueListenable: presentingState,
-                    builder: (BuildContext context, value, Widget? child) {
-                      return InkWell(
-                        onTap: () {
-                          presentingState.value = !presentingState.value;
-                          if (presentingState.value) {
-                            channelProvider.presentResume();
-                          } else {
-                            channelProvider.presentPause();
-                          }
-                        },
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              value
-                                  ? Icons.pause_presentation
-                                  : Icons.smart_display_outlined,
-                              color: Colors.white,
-                            ),
-                            const Padding(padding: EdgeInsets.only(left: 8)),
-                            Text(
-                              value
-                                  ? S.of(context).present_state_pause
-                                  : S.of(context).present_state_resume,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: AppConstants.fontSizeNormal,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
+              Text(
+                S.of(context).present_time,
+                style: textStyle28,
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 38.0, top: 20.0),
-                  child: InkWell(
+              const SizedBox(height: 30),
+              const PresentTimer(),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 30,
+          bottom: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ValueListenableBuilder(
+                valueListenable: presentingState,
+                builder: (BuildContext context, value, Widget? child) {
+                  return InkWell(
                     onTap: () {
-                      channelProvider.presentStop();
-                      channelProvider.presentModeratorWaitPage();
+                      presentingState.value = !presentingState.value;
+                      if (presentingState.value) {
+                        channelProvider.presentResume();
+                      } else {
+                        channelProvider.presentPause();
+                      }
                     },
                     child: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        const Icon(
-                          Icons.cancel_presentation,
+                        Icon(
+                          value
+                              ? Icons.pause_presentation
+                              : Icons.smart_display_outlined,
                           color: Colors.white,
                         ),
-                        const Padding(padding: EdgeInsets.only(left: 8)),
+                        const SizedBox(width: 8),
                         Text(
-                          S.of(context).present_state_stop,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: AppConstants.fontSizeNormal,
-                          ),
+                          value
+                              ? S.of(context).present_state_pause
+                              : S.of(context).present_state_resume,
+                          style: textStyle14,
                         ),
                       ],
                     ),
-                  ),
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+              InkWell(
+                onTap: () {
+                  channelProvider.presentStop();
+                  channelProvider.presentModeratorWaitPage();
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.cancel_presentation,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      S.of(context).present_state_stop,
+                      style: textStyle14,
+                    ),
+                  ],
                 ),
               ),
               if (channelProvider.showTouchBack())
