71839ed823b13e4f4682418ba25680247b810233
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-05-23 17:21:58 +0800

    feat: share logs to another app

--- apps/sender/lib/utilities/log.dart
+++ apps/sender/lib/utilities/log.dart
@@ -1,14 +1,47 @@
+import 'dart:io';
+
+import 'package:display_cast_flutter/utilities/log_storage.dart';
 import 'package:logging/logging.dart';
 import 'package:flutter/foundation.dart';
 
 Logger log = Logger('airsync');
 
+const _maxLogSize = 1000;
+
 void initLogger() {
   Logger.root.level = Level.INFO; // defaults to Level.INFO
 
   Logger.root.onRecord.listen((record) {
+    final msg = '${record.level.name} ${record.message}';
+
     if (kDebugMode) {
-      print('${record.level.name} ${record.message}');
+      print(msg);
     }
+
+    _logStorage?.addLog(msg);
   });
+}
+
+LogStorage? _logStorage;
+
+void enableLogToMemory(bool enable) {
+  if (enable) {
+    _logStorage = LogStorage(_maxLogSize);
+  } else {
+    _logStorage?.clearLogs();
+    _logStorage = null;
+  }
+}
+
+Future<void> writeLogToFile(File file) async {
+  if (_logStorage == null) {
+    return;
+  }
+
+  final logContent = _logStorage!.getLogs().join(Platform.lineTerminator);
+
+  await file.writeAsString(
+    logContent,
+    flush: true,
+  );
 }
