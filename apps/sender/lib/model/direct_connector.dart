ab84e83be2d9df384c73a13aa315e970d62fb0cd
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-06-04 11:46:43 +0800

    style: code formating

--- apps/sender/lib/model/direct_connector.dart
+++ apps/sender/lib/model/direct_connector.dart
@@ -1,10 +1,8 @@
-
 import 'package:display_cast_flutter/utilities/log.dart';
 import 'package:display_cast_flutter/model/airsync_bonsoir_service.dart';
 import 'package:display_channel/display_channel.dart';
 
 class DirectConnector {
-
   final String _clientId;
 
   final String? _displayCode;
@@ -31,9 +29,8 @@ class DirectConnector {
         _onOpenError = onOpenError;
 
   open({
-    required AirSyncBonsoirService service
+    required AirSyncBonsoirService service,
   }) {
-
     // open direct channel
     final uri = Uri(
       scheme: 'wss',
@@ -44,7 +41,7 @@ class DirectConnector {
     _directClient = DisplayChannelClient(
         _clientId,
         uri,
-            (url) => WebSocketClientConnection(url,
+        (url) => WebSocketClientConnection(url,
             maxRetryDelay: const Duration(seconds: 3),
             maxRetryAttempts: 3,
             logger: (url, message) =>
@@ -52,12 +49,10 @@ class DirectConnector {
             allowSelfSignedCertificates: true));
     _directClient?.openDirectChannel(token: _otp, displayCode: _displayCode);
     _directClient?.onStateChange = (ChannelState state) {
-
       if (state == ChannelState.connected) {
         _onOpened(_directClient!);
 
         drainPendingMessages(_directClient!, _directPendingMessages);
-
       } else if (state == ChannelState.closed) {
         final error = mapCloseCodeToChannelConnectorError(
           _directClient?.closeReason?.code,
@@ -68,9 +63,9 @@ class DirectConnector {
   }
 
   drainPendingMessages(
-      DisplayChannelClient client,
-      List<ChannelMessage> messages,
-      ) {
+    DisplayChannelClient client,
+    List<ChannelMessage> messages,
+  ) {
     // Note: messages may arrive early before the state of the channel switches to connected
     for (var message in messages) {
       client.onChannelMessage?.call(message);
