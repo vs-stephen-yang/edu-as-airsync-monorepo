147245be3aa7b9990b432161768499f7c7a191da
Zoey Chen <zoey.chen@myviewboard.com>
2024-06-18 18:37:50 +0800

    feat: [UI_UX 67647] Notification message when recovering (part1) - add toast and ReconnectState

--- apps/receiver/lib/utility/channel_util.dart
+++ apps/receiver/lib/utility/channel_util.dart
@@ -3,6 +3,13 @@ import 'dart:io';
 import 'package:display_channel/display_channel.dart';
 import 'package:display_flutter/utility/assets_util.dart';
 
+enum ReconnectState {
+  idle,
+  reconnecting,
+  success,
+  fail
+}
+
 const _certPemPath = 'assets/channel/certificate.pem';
 const _keyPemPath = 'assets/channel/private_key.pem';
 
