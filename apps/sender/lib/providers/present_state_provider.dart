e34514bcf538047a43fd4fea12a82f3fbc60504f
Zoey Chen <zoey.chen@myviewboard.com>
2024-07-05 15:07:08 +0800

    refactor: add present_state_provider.dart

--- apps/sender/lib/providers/present_state_provider.dart
+++ apps/sender/lib/providers/present_state_provider.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:flutter/cupertino.dart';
+
 enum ViewState {
   idle,
   waitReady,
@@ -16,4 +18,65 @@ enum ViewState {
   settings,
   language,
   deviceList
+}
+
+class PresentStateProvider extends ChangeNotifier {
+
+  ViewState _currentState = ViewState.idle;
+
+  ViewState get currentState => _currentState;
+
+  set currentState(ViewState value) {
+    _currentState = value;
+  }
+
+  setViewState(ViewState newViewState) {
+    _currentState = newViewState;
+    notifyListeners();
+  }
+
+  Future<void> presentMainPage() async {
+    setViewState(ViewState.idle);
+  }
+
+  Future<void> presentSelectRolePage() async {
+    setViewState(ViewState.selectRole);
+  }
+
+  Future<void> presentSelectScreenPage() async {
+    setViewState(ViewState.selectScreen);
+  }
+
+  Future<void> presentBasicStartPage() async {
+    setViewState(ViewState.presentStart);
+  }
+
+  Future<void> presentRemoteScreenPage() async {
+    setViewState(ViewState.remoteScreen);
+  }
+
+  Future<void> presentModeratorNamePage() async {
+    setViewState(ViewState.moderatorName);
+  }
+
+  Future<void> presentModeratorWaitPage() async {
+    setViewState(ViewState.moderatorWait);
+  }
+
+  Future<void> presentModeratorStartPage() async {
+    setViewState(ViewState.moderatorStart);
+  }
+
+  Future<void> presentSettingPage() async {
+    setViewState(ViewState.settings);
+  }
+
+  Future<void> presentLanguagePage() async {
+    setViewState(ViewState.language);
+  }
+
+  Future<void> presentDeviceListPage() async {
+    setViewState(ViewState.deviceList);
+  }
+
 }
