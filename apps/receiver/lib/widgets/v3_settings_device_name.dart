8eddd51fb7dd85452c27d613386fdc1d05952d6e
Zoey Chen <zoey.chen@myviewboard.com>
2024-08-19 09:46:09 +0800

    feat: [USER STORY 68869] Device setting revamp (part3)

---
+++ apps/receiver/lib/widgets/v3_settings_device_name.dart
@@ -0,0 +1,167 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:display_flutter/app_preferences.dart';
+import 'package:display_flutter/assets/tokens/tokens.g.dart';
+import 'package:display_flutter/generated/l10n.dart';
+import 'package:display_flutter/providers/settings_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:provider/provider.dart';
+
+class V3SettingsDeviceName extends StatefulWidget {
+  const V3SettingsDeviceName({super.key});
+
+  @override
+  State<V3SettingsDeviceName> createState() => _V3SettingsDeviceNameState();
+}
+
+class _V3SettingsDeviceNameState extends State<V3SettingsDeviceName> {
+  final TextEditingController _controller =
+      TextEditingController(text: AppPreferences().instanceName);
+  final FocusNode _focusNode = FocusNode();
+  bool _isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FocusScope.of(context).requestFocus(_focusNode);
+    });
+    // 监听焦点变化
+    _focusNode.addListener(() {
+      setState(() {
+        _isEditing = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SettingsProvider settingsProvider =
+        Provider.of<SettingsProvider>(context, listen: false);
+
+    return Stack(
+      children: [
+        Positioned(
+            left: 0,
+            top: 0,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Image(
+                    image: Svg('assets/images/ic_arrow_left.svg'),
+                    width: 21,
+                    height: 21,
+                  ),
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  onPressed: () {
+                    settingsProvider.setPage(SettingPageState.deviceSetting);
+                  },
+                ),
+                Text(
+                  S.of(context).v3_settings_device_name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            )),
+        Positioned(
+            left: 13,
+            top: 53,
+            width: 352,
+            child: Row(
+              children: [
+                Text(
+                  S.of(context).v3_settings_device_name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: 100,
+                  child: TextField(
+                    textAlign: TextAlign.right,
+                    // 使文字靠右顯示
+                    controller: _controller,
+                    focusNode: _focusNode,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none, // 去掉底線
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: _isEditing,
+                  maintainSize: true,
+                  maintainAnimation: true,
+                  maintainState: true,
+                  child: IconButton(
+                    icon: const Image(
+                      image: Svg('assets/images/ic_close_white.svg'),
+                      width: 21,
+                      height: 21,
+                    ),
+                    padding: const EdgeInsets.only(left: 0, right: 13),
+                    onPressed: () {
+                      print('zz ${_controller.text}');
+                      _isEditing = false;
+                      _focusNode.unfocus();
+                      // FocusScope.of(context).requestFocus(FocusNode());
+                    },
+                  ),
+                )
+              ],
+            )),
+        Positioned(
+          right: 13,
+          bottom: 13,
+          child: _saveButton(
+              context, S.of(context).v3_settings_device_name_save, onClick: () {
+            AppPreferences().set(instanceName: _controller.text);
+            settingsProvider.setPage(SettingPageState.deviceSetting);
+          }),
+        )
+      ],
+    );
+  }
+
+  _saveButton(BuildContext context, String text,
+      {required VoidCallback onClick}) {
+    return InkWell(
+      onTap: () {
+        onClick();
+      },
+      child: Container(
+        width: 48,
+        height: 26,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: context.tokens.color.vsdslColorSecondary,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: AutoSizeText(
+          text,
+          style: const TextStyle(
+            fontSize: 12,
+            color: Colors.white,
+          ),
+          maxLines: 1,
+        ),
+      ),
+    );
+  }
+}
