d84981291f3d8e6ae72999d4182e9f9bb918e98f
BenjaminLee <benjamin.lee@myviewboard.com>
2024-08-21 10:09:29 +0800

    fix: remove obsolete tests

--- packages/flutter_virtual_display/example/integration_test/plugin_integration_test.dart
+++ packages/flutter_virtual_display/example/integration_test/plugin_integration_test.dart
@@ -14,12 +14,4 @@ import 'package:flutter_virtual_display/flutter_virtual_display.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final FlutterVirtualDisplay plugin = FlutterVirtualDisplay();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
-  });
 }
