d425ebcc57b401e8891941c0c07f6a28b9ed4240
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-09-18 18:53:02 +0800

    feat: expand OTP range from 1000 to 9999 to include all 4-digit combinations

--- apps/receiver/lib/utility/misc_util.dart
+++ apps/receiver/lib/utility/misc_util.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
 String generateOTP(Random random) {
-  final randomNumber = random.nextInt(9000) + 1000;
-  return randomNumber.toString();
+  final randomNumber = random.nextInt(10000);
+  return randomNumber.toString().padLeft(4, '0');
 }
