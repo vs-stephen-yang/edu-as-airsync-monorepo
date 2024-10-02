ab4b40e2ed595a3618e6ea8415d5c24baa576c2e
Zoey Chen <zoey.chen@myviewboard.com>
2024-10-02 15:53:10 +0800

    feat: [USER STORY 70540] Native sender app revamp (一般連線) (p4)

--- apps/sender/lib/widgets/v3_present_idle_text_field.dart
+++ apps/sender/lib/widgets/v3_present_idle_text_field.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+import 'package:display_cast_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/channel_provider.dart';
+import 'package:display_cast_flutter/utilities/app_analytics.dart';
 import 'package:display_cast_flutter/utilities/channel_util.dart';
+import 'package:display_cast_flutter/utilities/data_display_code.dart';
 import 'package:display_cast_flutter/widgets/v3_custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -36,9 +39,9 @@ class V3PresentIdleTextFieldState extends State<V3PresentIdleTextField> {
   final GlobalKey<V3CustomTextFormFieldState> codeKey = GlobalKey();
   final GlobalKey<V3CustomTextFormFieldState> otpKey = GlobalKey();
 
-  OverlayEntry? _overlayEntry;
-  final LayerLink _layerLink = LayerLink();
-  bool _isOverlayVisible = false;
+  OverlayEntry? _dropDownMenuEntry;
+  final LayerLink _dropDownLayerLink = LayerLink();
+  bool _isDropDownMenuVisible = false;
 
   bool _isCodeSelectedFromHistory = false;
 
@@ -56,13 +59,12 @@ class V3PresentIdleTextFieldState extends State<V3PresentIdleTextField> {
       if (_otpFocusNode.hasFocus) {
         _otpController.selection = TextSelection(
             baseOffset: 0, extentOffset: _otpController.text.length);
-        //TODO:
         // User use "TAB" key to move to OTP input text field,
         // remove display code overlay
-        // if (_isOverlayVisible) {
-        //   _isOverlayVisible = false;
-        //   _overlayEntry?.remove();
-        // }
+        if (_isDropDownMenuVisible) {
+          _isDropDownMenuVisible = false;
+          _dropDownMenuEntry?.remove();
+        }
       }
     });
   }
@@ -89,103 +91,187 @@ class V3PresentIdleTextFieldState extends State<V3PresentIdleTextField> {
       width: widget.widthTextField,
       child: Column(
         children: [
-          V3CustomTextFormField(
-            key: codeKey,
-            controller: _codeController,
-            focusNode: _codeFocusNode,
-            hintText: S.of(context).main_display_code,
-            maxTextLength: displayCodeMaxLength,
-            inputFormatter: [
-              if (!Platform.isWindows) UpperCaseTextFormatter(),
-              if (!Platform.isWindows)
-                MaskedInputFormatter(
-                  '###########',
-                  allowedCharMatcher: RegExp('[A-Za-z0-9]'),
-                ),
-            ],
-            onFieldChanged: (text) {
-              if (Platform.isWindows) {
-                if (text.contains(RegExp(r'[^a-zA-Z0-9]'))) {
-                  _setErrorMsg(codeKey, S.of(context).main_display_code_error);
-                  // TODO:
-                  // _isOverlayVisible = false;
-                  // if (_overlayEntry != null && _overlayEntry!.mounted) _overlayEntry?.remove();
-                  // return;
-                }
-                _codeController.value = _codeController.value.copyWith(
-                  text: text.toUpperCase(),
-                  selection: TextSelection.collapsed(offset: text.length),
-                  composing: TextRange.empty,
-                );
-              }
-
-              _isCodeSelectedFromHistory = false;
-              bool presentBtnEnable = false;
-              if (text.length >= displayCodeMinLength &&
-                  _otpController.text.length == otpLength) {
-                presentBtnEnable = true;
-              }
-              widget.onFieldChanged(V3FieldResult(
-                  enable: presentBtnEnable,
-                  isDisplayCodeSelectedFromHistory: _isCodeSelectedFromHistory,
-                  displayCode: text,
-                  password: _otpController.text));
-            },
-            onTap: () async {
-              // TODO:
-              // List? displayList = await DataDisplayCode.getInstance().load();
-              // if (!_isOverlayVisible && displayList != null) {
-              //   _isOverlayVisible = true;
-              //   _createOverlayEntry(displayList.reversed.toList());
-              // }
-            },
-            onFieldSubmitted: (text) {
-              _otpFocusNode.requestFocus();
-            },
+          CompositedTransformTarget(
+            link: _dropDownLayerLink,
+            child: _displayCodeTextFormField(context),
           ),
           const SizedBox(height: 20),
-          V3CustomTextFormField(
-            key: otpKey,
-            controller: _otpController,
-            focusNode: _otpFocusNode,
-            hintText: S.of(context).main_password,
-            maxTextLength: otpLength,
-            inputFormatter: [
-              if (!Platform.isWindows)
-                MaskedInputFormatter(
-                  '0000',
-                  allowedCharMatcher: RegExp('[0-9]'),
-                ),
-            ],
-            onFieldChanged: (text) {
-              if (Platform.isWindows) {
-                if (text.contains(RegExp(r'[^0-9]'))) {
-                  _setErrorMsg(otpKey, S.of(context).main_otp_error);
-                  return;
-                }
-              }
-              bool presentBtnEnable = false;
-              if (_codeController.text.length >= displayCodeMinLength &&
-                  text.length == otpLength) {
-                presentBtnEnable = true;
-              }
-              widget.onFieldChanged(V3FieldResult(
-                  enable: presentBtnEnable,
-                  isDisplayCodeSelectedFromHistory: _isCodeSelectedFromHistory,
-                  displayCode: _codeController.text,
-                  password: text));
-            },
-            onFieldSubmitted: (text) {
-              widget.onPasswordEnterEvent(text);
-            },
-          ),
+          _otpTextFormField(context),
           const SizedBox(height: 20),
         ],
       ),
     );
   }
 
-  _setErrorMsg(
+  V3CustomTextFormField _displayCodeTextFormField(BuildContext context) {
+    return V3CustomTextFormField(
+      key: codeKey,
+      controller: _codeController,
+      focusNode: _codeFocusNode,
+      hintText: S.of(context).main_display_code,
+      maxTextLength: displayCodeMaxLength,
+      inputFormatter: [
+        if (!Platform.isWindows) UpperCaseTextFormatter(),
+        if (!Platform.isWindows)
+          MaskedInputFormatter(
+            '###########',
+            allowedCharMatcher: RegExp('[A-Za-z0-9]'),
+          ),
+      ],
+      onFieldChanged: (text) {
+        if (Platform.isWindows) {
+          if (text.contains(RegExp(r'[^a-zA-Z0-9]'))) {
+            _setTextFormFieldErrorMsg(
+                codeKey, S.of(context).main_display_code_error);
+            _isDropDownMenuVisible = false;
+            if (_dropDownMenuEntry != null && _dropDownMenuEntry!.mounted)
+              _dropDownMenuEntry?.remove();
+            return;
+          }
+          _codeController.value = _codeController.value.copyWith(
+            text: text.toUpperCase(),
+            selection: TextSelection.collapsed(offset: text.length),
+            composing: TextRange.empty,
+          );
+        }
+
+        _isCodeSelectedFromHistory = false;
+        bool presentBtnEnable = false;
+        if (text.length >= displayCodeMinLength &&
+            _otpController.text.length == otpLength) {
+          presentBtnEnable = true;
+        }
+        widget.onFieldChanged(V3FieldResult(
+            enable: presentBtnEnable,
+            isDisplayCodeSelectedFromHistory: _isCodeSelectedFromHistory,
+            displayCode: text,
+            password: _otpController.text));
+      },
+      onTap: () async {
+        List? displayList = await DataDisplayCode.getInstance().load();
+        if (!_isDropDownMenuVisible && displayList != null) {
+          _isDropDownMenuVisible = true;
+          _dropDownMenu(displayList.reversed.toList());
+        }
+      },
+      onFieldSubmitted: (text) {
+        _otpFocusNode.requestFocus();
+      },
+    );
+  }
+
+  V3CustomTextFormField _otpTextFormField(BuildContext context) {
+    return V3CustomTextFormField(
+      key: otpKey,
+      controller: _otpController,
+      focusNode: _otpFocusNode,
+      hintText: S.of(context).main_password,
+      maxTextLength: otpLength,
+      inputFormatter: [
+        if (!Platform.isWindows)
+          MaskedInputFormatter(
+            '0000',
+            allowedCharMatcher: RegExp('[0-9]'),
+          ),
+      ],
+      onFieldChanged: (text) {
+        if (Platform.isWindows) {
+          if (text.contains(RegExp(r'[^0-9]'))) {
+            _setTextFormFieldErrorMsg(otpKey, S.of(context).main_otp_error);
+            return;
+          }
+        }
+        bool presentBtnEnable = false;
+        if (_codeController.text.length >= displayCodeMinLength &&
+            text.length == otpLength) {
+          presentBtnEnable = true;
+        }
+        widget.onFieldChanged(V3FieldResult(
+            enable: presentBtnEnable,
+            isDisplayCodeSelectedFromHistory: _isCodeSelectedFromHistory,
+            displayCode: _codeController.text,
+            password: text));
+      },
+      onFieldSubmitted: (text) {
+        widget.onPasswordEnterEvent(text);
+      },
+    );
+  }
+
+  /// the height of every item is 48
+  /// the scroll bar should show up when over 5 items
+  /// the max height of the overlay area is 200
+  _dropDownMenu(List displayList) {
+    _dropDownMenuEntry = OverlayEntry(builder: (builder) {
+      return Stack(
+        children: <Widget>[
+          // Check range outside listview
+          GestureDetector(
+            onTap: () {
+              _isDropDownMenuVisible = false;
+              _dropDownMenuEntry?.remove();
+            },
+            child: Container(
+              color: Colors.transparent, // 設置為透明色
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          Positioned(
+            width: 300,
+            height: displayList.length > 4 ? 208 : displayList.length * 48,
+            child: CompositedTransformFollower(
+              offset: const Offset(0, 55),
+              link: _dropDownLayerLink,
+              child: Material(
+                color: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                child: Container(
+                  color: Colors.transparent,
+                  margin: const EdgeInsets.all(8),
+                  child: ListView.builder(
+                    itemCount: displayList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                        title: Text(displayList[index]),
+                        hoverColor: context.tokens.color.vsdslColorTertiary,
+                        trailing: IconButton(
+                          icon: const Icon(Icons.highlight_remove),
+                          onPressed: () {
+                            DataDisplayCode.getInstance()
+                                .remove(displayList[index]);
+                            displayList.removeAt(index);
+                            _isDropDownMenuVisible = false;
+                            _dropDownMenuEntry?.remove();
+                          },
+                        ),
+                        onTap: () {
+                          AppAnalytics.instance
+                              .trackEvent('select_display_code');
+                          _isCodeSelectedFromHistory = true;
+                          _codeController.text = displayList[index];
+                          _isDropDownMenuVisible = false;
+                          _dropDownMenuEntry?.remove();
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    });
+    Overlay.of(context).insert(_dropDownMenuEntry!);
+  }
+
+  _setTextFormFieldErrorMsg(
     GlobalKey<V3CustomTextFormFieldState> key,
     String text,
   ) {
@@ -195,21 +281,21 @@ class V3PresentIdleTextFieldState extends State<V3PresentIdleTextField> {
   _showConnectErrorMessage(ChannelConnectError error) {
     switch (error) {
       case ChannelConnectError.instanceNotFound:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           codeKey,
           S.of(context).main_instance_not_found_or_offline,
         );
         break;
 
       case ChannelConnectError.invalidDisplayCode:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           codeKey,
           S.of(context).main_display_code_invalid,
         );
         break;
 
       case ChannelConnectError.networkError:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           codeKey,
           S.of(context).main_connect_network_error,
         );
@@ -217,28 +303,28 @@ class V3PresentIdleTextFieldState extends State<V3PresentIdleTextField> {
 
       case ChannelConnectError.invalidOtp:
       case ChannelConnectError.authenticationRequired:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           otpKey,
           S.of(context).main_password_invalid,
         );
         break;
 
       case ChannelConnectError.rateLimitExceeded:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           codeKey,
           S.of(context).main_connect_rate_limited,
         );
         break;
 
       case ChannelConnectError.connectionModeUnsupported:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           codeKey,
           S.of(context).main_connection_mode_unsupported,
         );
         break;
 
       case ChannelConnectError.unknownError:
-        _setErrorMsg(
+        _setTextFormFieldErrorMsg(
           otpKey,
           S.of(context).main_connect_unknown_error,
         );
