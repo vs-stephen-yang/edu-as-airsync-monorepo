6260b751584538b785e7a60080c3aa69cfbf97cf
Zoey Chen <zoey.chen@myviewboard.com>
2024-10-09 11:53:23 +0800

    feat: [USER STORY 70053] Show 3rd-party library license information (p5)

--- apps/receiver/lib/widgets/v3_settings_legal_policy.dart
+++ apps/receiver/lib/widgets/v3_settings_legal_policy.dart
@@ -1,37 +1,13 @@
-import 'dart:convert';
-
 import 'package:display_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_flutter/generated/l10n.dart';
+import 'package:display_flutter/oss_licenses.dart';
 import 'package:display_flutter/providers/settings_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:provider/provider.dart';
 
-class V3SettingsLegalPolicy extends StatefulWidget {
+class V3SettingsLegalPolicy extends StatelessWidget {
   const V3SettingsLegalPolicy({super.key});
-
-  @override
-  State<V3SettingsLegalPolicy> createState() => _V3SettingsLegalPolicyState();
-}
-
-class _V3SettingsLegalPolicyState extends State<V3SettingsLegalPolicy> {
-  // late List<LicenseEntry> _licenses;
-
-  @override
-  void initState() {
-    super.initState();
-    // _loadLicenses();
-  }
-
-  Future<List<License>> _loadLicenses() async {
-    final bundle = DefaultAssetBundle.of(context);
-    final licenses = await bundle.loadString('assets/3rd_licenses.json');
-    return json
-        .decode(licenses)
-        .map<License>((licenses) => License.fromJson(licenses))
-        .toList();
-    // return _licenses = await LicenseRegistry.licenses.toList();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,49 +62,39 @@ class _V3SettingsLegalPolicyState extends State<V3SettingsLegalPolicy> {
             ),
           ),
           Expanded(
-            child: FutureBuilder<List<License>>(
-              future: _loadLicenses(),
-              builder: (context, snapshot) =>
-                  snapshot.connectionState == ConnectionState.waiting
-                      ? const Center(child: CircularProgressIndicator())
-                      : snapshot.hasError
-                          ? const Center(child: Text('Error loading licenses'))
-                          : ListView.builder(
-                              itemCount: snapshot.data!.length,
-                              itemBuilder: (context, index) {
-                                final license = snapshot.data![index];
-                                return SizedBox(
-                                  height: 26,
-                                  child: Row(
-                                    children: [
-                                      Text(
-                                        license.name!,
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                      const Spacer(),
-                                      IconButton(
-                                        icon: const Image(
-                                          image: Svg(
-                                              'assets/images/ic_arrow_right.svg'),
-                                          width: 21,
-                                          height: 21,
-                                        ),
-                                        padding: EdgeInsets.zero,
-                                        constraints: const BoxConstraints(),
-                                        onPressed: () {
-                                          settingsProvider.setPage(
-                                              SettingPageState.licenses,
-                                              license: license);
-                                        },
-                                      )
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
+            child: ListView.builder(
+              itemCount: dependencies.length,
+              itemBuilder: (context, index) {
+                final license = dependencies[index];
+                return SizedBox(
+                  height: 26,
+                  child: Row(
+                    children: [
+                      Text(
+                        license.name,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        icon: const Image(
+                          image: Svg('assets/images/ic_arrow_right.svg'),
+                          width: 21,
+                          height: 21,
+                        ),
+                        padding: EdgeInsets.zero,
+                        constraints: const BoxConstraints(),
+                        onPressed: () {
+                          settingsProvider.setPage(SettingPageState.licenses,
+                              license: license);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ],
