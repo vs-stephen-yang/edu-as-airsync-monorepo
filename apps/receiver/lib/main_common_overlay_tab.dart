05aa5c32208c5038e504976c6fe4bfdf566cba83
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-10-23 18:16:50 +0800

    feat: enable sentry for overlay

--- apps/receiver/lib/main_common_overlay_tab.dart
+++ apps/receiver/lib/main_common_overlay_tab.dart
@@ -5,17 +5,23 @@ import 'package:display_flutter/generated/l10n.dart';
 import 'package:display_flutter/providers/pref_language_provider.dart';
 import 'package:display_flutter/screens/overlay_tab.dart';
 import 'package:display_flutter/screens/v3_overlay_tab.dart';
+import 'package:display_flutter/settings/app_config.dart';
 import 'package:display_flutter/settings/theme_config.dart';
 import 'package:display_flutter/utility/device_feature_adapter.dart';
 import 'package:display_flutter/utility/log.dart';
+import 'package:display_flutter/utility/sentry_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-commonOverlayTabEntry() {
+commonOverlayTabEntry(ConfigSettings settings) {
   runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+
+    if (settings.sentry != null) {
+      initSentry(settings.sentry!);
+    }
 
     await DeviceFeatureAdapter.ensureInitialized();
 
