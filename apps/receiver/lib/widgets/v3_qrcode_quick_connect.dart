6444ac52eb03644546b4d497c2f62b448b60235f
Loren Chang <loren.chang@tw.viewsonic.com>
2024-10-28 18:16:52 +0800

    refactor: extract the QR code image generator into an independent widget

--- apps/receiver/lib/widgets/v3_qrcode_quick_connect.dart
+++ apps/receiver/lib/widgets/v3_qrcode_quick_connect.dart
@@ -1,13 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:display_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_flutter/generated/l10n.dart';
-import 'package:display_flutter/providers/channel_provider.dart';
-import 'package:display_flutter/providers/instance_info_provider.dart';
+import 'package:display_flutter/widgets/v3_qrcode_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-import 'package:provider/provider.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
 class V3QrcodeQuickConnect extends StatelessWidget {
   const V3QrcodeQuickConnect(
@@ -32,66 +27,16 @@ class V3QrcodeQuickConnect extends StatelessWidget {
             ? context.tokens.spacing.vsdslSpacing3xl.top - 1
             : context.tokens.spacing.vsdslSpacingXl.top - 1);
 
-    PackageInfo? pkgInfo;
-    Widget qrCode = StatefulBuilder(builder: (context, setState) {
-      if (pkgInfo == null) {
-        PackageInfo.fromPlatform().then((packageInfo) {
-          setState(() {
-            pkgInfo = packageInfo;
-          });
-        });
-      }
-      return Stack(
-        alignment: Alignment.center,
-        children: [
-          Image(
-            width: size,
-            height: size,
-            image: Svg(isStringOnTop
-                ? 'assets/images/ic_qrcode_background2.svg'
-                : 'assets/images/ic_qrcode_background1.svg'),
-          ),
-          Consumer2<InstanceInfoProvider, ChannelProvider>(
-            builder: (_, instanceProvider, channelProvider, __) {
-              return ValueListenableBuilder<String>(
-                valueListenable: channelProvider.otp,
-                builder: (_, otp, __) {
-                  // todo: design deep link to implement quick connect
-                  final ver = (pkgInfo?.version ?? '').replaceAll('-', '_');
-                  final dc = instanceProvider.displayCode;
-                  final otp = channelProvider.otp.value;
-                  String quickConnectData =
-                      'https://airsync/Quick_Connect?display_code=$dc&otp=$otp&ver=$ver';
-                  return QrImageView(
-                    data: quickConnectData,
-                    version: QrVersions.auto,
-                    padding: EdgeInsets.zero,
-                    size: isStringOnTop ? size - 35 : size - 32,
-                  );
-                },
-              );
-            },
-          ),
-        ],
-      );
-    });
-
     List<Widget> children = [];
-    if (isStringOnTop) {
-      children.add(title);
-      children.add(space);
-      children.add(qrCode);
-    } else {
-      children.add(qrCode);
-      children.add(space);
-      children.add(title);
-    }
+    children.add(title);
+    children.add(space);
+    children.add(const V3QRcodeImage(isShowBackground: true));
 
     return Container(
       padding: EdgeInsets.symmetric(
           vertical: context.tokens.spacing.vsdslSpacing4xl.top - 1),
       child: Column(
-        children: children,
+        children: isStringOnTop ? children : children.reversed.toList(),
       ),
     );
   }
