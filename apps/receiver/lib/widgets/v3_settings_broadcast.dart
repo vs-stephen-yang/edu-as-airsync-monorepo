74d08a30fdf8f5265b56014e64da139578ac30e8
Loren Chang <loren.chang@viewsonic.com>
2024-10-31 19:21:27 +0800

    refactor: implement new broadcast ui (Devices and Boards)

--- apps/receiver/lib/widgets/v3_settings_broadcast.dart
+++ apps/receiver/lib/widgets/v3_settings_broadcast.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:display_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_flutter/generated/l10n.dart';
 import 'package:display_flutter/providers/channel_provider.dart';
@@ -11,120 +12,201 @@ class V3SettingsBroadcast extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SettingsProvider settingsProvider =
-        Provider.of<SettingsProvider>(context, listen: false);
     return Stack(
       children: [
         Positioned(
-            left: 13,
-            top: 57,
-            right: 13,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 26,
-                  child: _buildCastDevices(context),
+          left: context.tokens.spacing.vsdslSpacingXl.left,
+          top: 57,
+          right: context.tokens.spacing.vsdslSpacingXl.right,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AutoSizeText(
+                S.of(context).v3_settings_broadcast_cast_to,
+                style: TextStyle(
+                  color: context.tokens.color.vsdslColorOnSurfaceInverse,
+                  fontSize: 12,
                 ),
-                Padding(
-                    padding: EdgeInsets.only(
-                        bottom: context.tokens.spacing.vsdslSpacingSm.bottom)),
-                _buildTextDesc(
-                    context, S.of(context).v3_shortcuts_cast_device_desc),
-                Container(
-                  height: 1,
-                  margin: EdgeInsets.only(
-                      top: context.tokens.spacing.vsdslSpacingSm.top,
-                      bottom: context.tokens.spacing.vsdslSpacingSm.bottom),
-                  color: context.tokens.color.vsdslColorOutlineVariant,
-                ),
-                SizedBox(
-                  height: 26,
-                  child: _buildCastBoards(context, settingsProvider),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(
-                        bottom: context.tokens.spacing.vsdslSpacingSm.bottom)),
-                _buildTextDesc(context,
-                    S.of(context).v3_settings_broadcast_cast_boards_desc),
-              ],
-            ))
+              ),
+              SizedBox(height: context.tokens.spacing.vsdslSpacingXl.top),
+              const CastToDevices(),
+              SizedBox(height: context.tokens.spacing.vsdslSpacingMd.top),
+              const CastToBoards(),
+            ],
+          ),
+        ),
       ],
     );
   }
+}
 
-  Text _buildTextDesc(BuildContext context, String text) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 9,
-        fontWeight: FontWeight.w400,
-        color: context.tokens.color.vsdslColorOnSurfaceVariant,
+class CastToDevices extends StatelessWidget {
+  const CastToDevices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 325,
+      height: 88,
+      decoration: BoxDecoration(
+        borderRadius: context.tokens.radii.vsdslRadiusLg,
+        color: context.tokens.color.vsdslColorSurface900,
+      ),
+      padding: context.tokens.spacing.vsdslSpacingXl,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 43,
+            height: 43,
+            child: Image(
+              image: Svg('assets/images/ic_cast_to_devices.svg'),
+            ),
+          ),
+          SizedBox(
+            width: context.tokens.spacing.vsdslSpacingXl.left,
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 27,
+                  child: Row(
+                    children: [
+                      AutoSizeText(
+                        S.of(context).v3_settings_broadcast_devices,
+                        style: TextStyle(
+                          color:
+                              context.tokens.color.vsdslColorOnSurfaceInverse,
+                          fontSize: 12,
+                        ),
+                      ),
+                      const Spacer(),
+                      Consumer<ChannelProvider>(
+                        builder: (_, channelProvider, __) {
+                          return SizedBox(
+                            width: 36,
+                            height: 21,
+                            child: IconButton(
+                              icon: Image(
+                                image: Svg(channelProvider.isSenderMode
+                                    ? 'assets/images/ic_switch_on.svg'
+                                    : 'assets/images/ic_switch_off.svg'),
+                              ),
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              onPressed: () async {
+                                if (channelProvider.isSenderMode) {
+                                  await channelProvider.removeSender(
+                                      fromSender: true);
+                                } else {
+                                  await channelProvider.startRemoteScreen(
+                                      fromSender: true);
+                                }
+                              },
+                            ),
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: context.tokens.spacing.vsdslSpacingSm.top),
+                AutoSizeText(
+                  S.of(context).v3_shortcuts_cast_device_desc,
+                  minFontSize: 8,
+                  style: TextStyle(
+                    fontSize: 9,
+                    fontWeight: FontWeight.w400,
+                    color: context.tokens.color.vsdslColorOnSurfaceVariant,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+}
 
-  Row _buildCastBoards(
-      BuildContext context, SettingsProvider settingsProvider) {
-    return Row(
-      children: [
-        Text(
-          S.of(context).v3_settings_broadcast_cast_boards,
-          style: TextStyle(
-            color: context.tokens.color.vsdslColorOnSurfaceInverse,
-            fontSize: 12,
-          ),
-        ),
-        const Spacer(),
-        IconButton(
-          icon: const Image(
-            image: Svg('assets/images/ic_arrow_right.svg'),
-            width: 21,
-            height: 21,
-          ),
-          padding: EdgeInsets.zero,
-          constraints: const BoxConstraints(),
-          onPressed: () {
-            settingsProvider.setPage(SettingPageState.broadcastBoards);
-          },
-        ),
-      ],
-    );
-  }
+class CastToBoards extends StatelessWidget {
+  const CastToBoards({super.key});
 
-  Row _buildCastDevices(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          S.of(context).v3_shortcuts_cast_device,
-          style: TextStyle(
-            color: context.tokens.color.vsdslColorOnSurfaceInverse,
-            fontSize: 12,
-          ),
-        ),
-        const Spacer(),
-        Consumer<ChannelProvider>(builder: (_, channelProvider, __) {
-          return SizedBox(
-            height: 21,
-            child: IconButton(
-              icon: Image(
-                image: Svg(channelProvider.isSenderMode
-                    ? 'assets/images/ic_switch_on.svg'
-                    : 'assets/images/ic_switch_off.svg'),
-              ),
-              padding: EdgeInsets.zero,
-              constraints: const BoxConstraints(),
-              onPressed: () async {
-                if (channelProvider.isSenderMode) {
-                  await channelProvider.removeSender(fromSender: true);
-                } else {
-                  await channelProvider.startRemoteScreen(fromSender: true);
-                }
-              },
+  @override
+  Widget build(BuildContext context) {
+    SettingsProvider settingsProvider =
+        Provider.of<SettingsProvider>(context, listen: false);
+    return Container(
+      width: 325,
+      height: 88,
+      decoration: BoxDecoration(
+        borderRadius: context.tokens.radii.vsdslRadiusLg,
+        color: context.tokens.color.vsdslColorSurface900,
+      ),
+      padding: context.tokens.spacing.vsdslSpacingXl,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 43,
+            height: 43,
+            child: Image(
+              image: Svg('assets/images/ic_cast_to_boards.svg'),
             ),
-          );
-        }),
-      ],
+          ),
+          SizedBox(
+            width: context.tokens.spacing.vsdslSpacingXl.left,
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 27,
+                  child: Row(
+                    children: [
+                      AutoSizeText(
+                        S.of(context).v3_settings_broadcast_boards,
+                        style: TextStyle(
+                          color:
+                              context.tokens.color.vsdslColorOnSurfaceInverse,
+                          fontSize: 12,
+                        ),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: 21,
+                        height: 21,
+                        child: IconButton(
+                          icon: const Image(
+                            image: Svg('assets/images/ic_arrow_right.svg'),
+                          ),
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          onPressed: () {
+                            settingsProvider
+                                .setPage(SettingPageState.broadcastBoards);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: context.tokens.spacing.vsdslSpacingSm.top),
+                AutoSizeText(
+                  S.of(context).v3_settings_broadcast_cast_boards_desc,
+                  minFontSize: 8,
+                  style: TextStyle(
+                    fontSize: 9,
+                    fontWeight: FontWeight.w400,
+                    color: context.tokens.color.vsdslColorOnSurfaceVariant,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
