99511c2003d280151bafaeb37494c43903cc5f25
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-11-01 13:54:20 +0800

    refactor: extract utility function

--- apps/receiver/lib/utility/misc_util.dart
+++ apps/receiver/lib/utility/misc_util.dart
@@ -1,6 +1,23 @@
+import 'dart:io';
 import 'dart:math';
+
+import 'package:flutter/foundation.dart';
 
 String generateOTP(Random random) {
   final randomNumber = random.nextInt(10000);
   return randomNumber.toString().padLeft(4, '0');
+}
+
+String getPlatformName() {
+  if (kIsWeb) {
+    return 'Web';
+  }
+
+  if (Platform.isIOS) {
+    return 'iOS';
+  } else if (Platform.isAndroid) {
+    return 'Android';
+  } else {
+    return '';
+  }
 }
