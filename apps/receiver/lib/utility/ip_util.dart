8f4459b57e6f441dd9194f0804894b2a557f33c4
Stephen Yang <stephen.tw.yang@viewsonic.com>
2024-11-04 14:35:16 +0800

    refactor: extract function

--- apps/receiver/lib/utility/ip_util.dart
+++ apps/receiver/lib/utility/ip_util.dart
@@ -17,3 +17,65 @@ bool isPrivateIp(String ipAddress) {
     return false;
   }
 }
+
+Future<String?> getPreferredNetworkIpAddress() async {
+  List<NetworkInterface> interfaces =
+      await NetworkInterface.list(type: InternetAddressType.IPv4);
+
+  List<NetworkInterface> ethernetInterfaces = [];
+  List<NetworkInterface> wifiInterfaces = [];
+  List<NetworkInterface> mobileInterfaces = [];
+  for (NetworkInterface interface in interfaces) {
+    if (interface.name.toLowerCase().startsWith("eth")) {
+      ethernetInterfaces.add(interface);
+    } else if (interface.name.toLowerCase().startsWith("wi") ||
+        interface.name.toLowerCase().startsWith("wlan")) {
+      wifiInterfaces.add(interface);
+    } else if (interface.name.toLowerCase().startsWith("rmnet") ||
+        interface.name.toLowerCase().startsWith("wwan")) {
+      mobileInterfaces.add(interface);
+    }
+  }
+
+  // by order
+  ethernetInterfaces.sort((a, b) => a.name.compareTo(b.name));
+  wifiInterfaces.sort((a, b) => a.name.compareTo(b.name));
+  mobileInterfaces.sort((a, b) => a.name.compareTo(b.name));
+
+  if (ethernetInterfaces.isNotEmpty) {
+    for (NetworkInterface interface in ethernetInterfaces) {
+      String? ethernetIp = interface.addresses.isNotEmpty
+          ? interface.addresses[0].address
+          : null;
+      if (ethernetIp != null) {
+        return ethernetIp;
+      }
+      break;
+    }
+  }
+
+  if (wifiInterfaces.isNotEmpty) {
+    for (NetworkInterface interface in wifiInterfaces) {
+      String? wifiIp = interface.addresses.isNotEmpty
+          ? interface.addresses[0].address
+          : null;
+      if (wifiIp != null) {
+        return wifiIp;
+      }
+      break;
+    }
+  }
+
+  if (mobileInterfaces.isNotEmpty) {
+    for (NetworkInterface interface in mobileInterfaces) {
+      String? mobileIp = interface.addresses.isNotEmpty
+          ? interface.addresses[0].address
+          : null;
+      if (mobileIp != null) {
+        return mobileIp;
+      }
+      break;
+    }
+  }
+  return null;
+}
