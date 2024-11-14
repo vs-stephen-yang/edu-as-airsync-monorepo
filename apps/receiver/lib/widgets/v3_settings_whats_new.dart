6dfde959f424d093bc527ad21738cd3bd3aebe98
Ken Chung <ken.mh.chung@tw.viewsonic.com>
2024-11-14 10:54:05 +0800

    fix: [BUG #74296][Receiver][IFP52-2] What's new欄位內部版號未正確顯示，目前為%s

--- apps/receiver/lib/widgets/v3_settings_whats_new.dart
+++ apps/receiver/lib/widgets/v3_settings_whats_new.dart
@@ -1,7 +1,10 @@
 import 'package:display_flutter/generated/l10n.dart';
+import 'package:display_flutter/settings/app_config.dart';
 import 'package:display_flutter/widgets/v3_focus_single_child_scroll_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:gap/gap.dart';
+import 'package:sprintf/sprintf.dart';
 
 class V3SettingsWhatsNew extends StatelessWidget {
   const V3SettingsWhatsNew({super.key});
@@ -9,7 +12,7 @@ class V3SettingsWhatsNew extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 57, left: 13, right: 13),
+      padding: const EdgeInsets.only(top: 57, left: 13, right: 13, bottom: 13),
       child: Column(
         children: [
           Expanded(
@@ -20,9 +23,20 @@ class V3SettingsWhatsNew extends StatelessWidget {
                   height: 53,
                   image: Svg('assets/images/ic_logo_airsync_icon.svg'),
                 ),
-                const SizedBox(height: 27),
+                const Gap(12),
+                Center(
+                  child: Text(
+                    'v${AppConfig.of(context)?.appVersion ?? ''}',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+                const Gap(16),
                 Text(
-                  S.of(context).v3_settings_whats_new_content,
+                  sprintf(S.current.v3_settings_whats_new_content,
+                      ['v${AppConfig.of(context)?.appVersion ?? ''}']),
                   textAlign: TextAlign.start,
                   style: const TextStyle(
                     fontWeight: FontWeight.w400,
