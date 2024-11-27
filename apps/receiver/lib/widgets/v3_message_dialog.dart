411dad34fabdf33c142aa38a7bda2188dbecf1e5
Loren Chang <loren.chang@tw.viewsonic.com>
2024-11-27 14:45:46 +0800

    feat: [USER STORY #74231] Refine CDE remote controller action
    fix V3MessageDialog focus scope issue

--- apps/receiver/lib/widgets/v3_message_dialog.dart
+++ apps/receiver/lib/widgets/v3_message_dialog.dart
@@ -14,137 +14,141 @@ class V3MessageDialog extends ConsumerWidget {
     if (!dialogState.isVisible) {
       return const SizedBox.shrink();
     }
-    return Container(
-      alignment: Alignment.center,
-      width: double.infinity,
-      height: double.infinity,
-      color: context.tokens.color.vsdslColorOpacityNeutralXs,
+    return FocusScope(
+      autofocus: true,
+      node: FocusScopeNode(),
       child: Container(
-        width: dialogState.width ?? 400,
-        // 設置固定寬度
-        height: dialogState.height ?? 265,
-        // 設置固定高度
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: context.tokens.color.vsdslColorSurface100,
-          borderRadius: BorderRadius.circular(
-              context.tokens.radii.vsdslRadiusXl.topLeft.x),
-          border: Border.all(
-              color: context.tokens.color.vsdslColorSurface100, width: 1.0),
-          boxShadow: context.tokens.shadow.vsdslShadowNeutralXl,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (dialogState.showIcon ?? false) ...[
-              const Image(
-                image: Svg('assets/images/ic_logo_airsync_icon.svg'),
-                width: 66,
-                height: 66,
-              ),
-              const SizedBox(height: 12),
-            ],
-            if (dialogState.title != null)
-              Text(
-                dialogState.title!,
-                style: TextStyle(
-                  color: context.tokens.color.vsdslColorNeutral,
-                  fontSize: 16,
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: double.infinity,
+        color: context.tokens.color.vsdslColorOpacityNeutralXs,
+        child: Container(
+          width: dialogState.width ?? 400,
+          // 設置固定寬度
+          height: dialogState.height ?? 265,
+          // 設置固定高度
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: context.tokens.color.vsdslColorSurface100,
+            borderRadius: BorderRadius.circular(
+                context.tokens.radii.vsdslRadiusXl.topLeft.x),
+            border: Border.all(
+                color: context.tokens.color.vsdslColorSurface100, width: 1.0),
+            boxShadow: context.tokens.shadow.vsdslShadowNeutralXl,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (dialogState.showIcon ?? false) ...[
+                const Image(
+                  image: Svg('assets/images/ic_logo_airsync_icon.svg'),
+                  width: 66,
+                  height: 66,
                 ),
-              ),
-            if (dialogState.title != null && dialogState.content != null)
-              const SizedBox(height: 12),
-            if (dialogState.content != null)
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Text(
-                    dialogState.content!,
-                    style: TextStyle(
-                      color: context.tokens.color.vsdslColorNeutral,
-                      fontSize: 12,
-                    ),
-                  ),
-                ),
-              ),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (dialogState.cancelText != null)
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      foregroundColor: WidgetStateProperty.resolveWith<Color>(
-                        (Set<WidgetState> states) {
-                          return context.tokens.color
-                              .vsdslColorPrimaryVariant; // 默认前景颜色
-                        },
-                      ),
-                      backgroundColor: WidgetStateProperty.resolveWith<Color>(
-                        (Set<WidgetState> states) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return context
-                                .tokens.color.vsdslColorSurface200; // 按下状态的背景颜色
-                          }
-                          return context.tokens.color
-                              .vsdslColorOnSurfaceInverse; // 默认背景颜色
-                        },
-                      ),
-                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(9999),
-                          side: BorderSide(
-                              color: context
-                                  .tokens.color.vsdslColorPrimaryVariant,
-                              width: 1.0),
-                        ),
-                      ),
-                      elevation: WidgetStateProperty.all(4.0),
-                    ),
-                    onPressed: () {
-                      ref.read(dialogProvider.notifier).hideDialog();
-                      dialogState.onCancel?.call();
-                    },
-                    child: Text(dialogState.cancelText!),
-                  ),
-                const SizedBox(width: 8),
-                if (dialogState.confirmText != null)
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      foregroundColor: WidgetStateProperty.resolveWith<Color>(
-                        (Set<WidgetState> states) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return context.tokens.color.vsdslColorSurface300;
-                          }
-                          return context
-                              .tokens.color.vsdslColorOnSurfaceInverse;
-                        },
-                      ),
-                      backgroundColor: WidgetStateProperty.resolveWith<Color>(
-                        (Set<WidgetState> states) {
-                          if (states.contains(WidgetState.pressed)) {
-                            return context
-                                .tokens.color.vsdslColorPrimaryVariant;
-                          }
-                          return context.tokens.color.vsdslColorPrimary;
-                        },
-                      ),
-                      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(9999),
-                        ),
-                      ),
-                      elevation: WidgetStateProperty.all(4.0),
-                    ),
-                    onPressed: () {
-                      ref.read(dialogProvider.notifier).hideDialog();
-                      dialogState.onConfirm?.call();
-                    },
-                    child: Text(dialogState.confirmText!),
-                  ),
+                const SizedBox(height: 12),
               ],
-            ),
-          ],
+              if (dialogState.title != null)
+                Text(
+                  dialogState.title!,
+                  style: TextStyle(
+                    color: context.tokens.color.vsdslColorNeutral,
+                    fontSize: 16,
+                  ),
+                ),
+              if (dialogState.title != null && dialogState.content != null)
+                const SizedBox(height: 12),
+              if (dialogState.content != null)
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      dialogState.content!,
+                      style: TextStyle(
+                        color: context.tokens.color.vsdslColorNeutral,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (dialogState.cancelText != null)
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        foregroundColor: WidgetStateProperty.resolveWith<Color>(
+                          (Set<WidgetState> states) {
+                            return context.tokens.color
+                                .vsdslColorPrimaryVariant; // 默认前景颜色
+                          },
+                        ),
+                        backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                          (Set<WidgetState> states) {
+                            if (states.contains(WidgetState.pressed)) {
+                              return context.tokens.color
+                                  .vsdslColorSurface200; // 按下状态的背景颜色
+                            }
+                            return context.tokens.color
+                                .vsdslColorOnSurfaceInverse; // 默认背景颜色
+                          },
+                        ),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(9999),
+                            side: BorderSide(
+                                color: context
+                                    .tokens.color.vsdslColorPrimaryVariant,
+                                width: 1.0),
+                          ),
+                        ),
+                        elevation: WidgetStateProperty.all(4.0),
+                      ),
+                      onPressed: () {
+                        ref.read(dialogProvider.notifier).hideDialog();
+                        dialogState.onCancel?.call();
+                      },
+                      child: Text(dialogState.cancelText!),
+                    ),
+                  const SizedBox(width: 8),
+                  if (dialogState.confirmText != null)
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        foregroundColor: WidgetStateProperty.resolveWith<Color>(
+                          (Set<WidgetState> states) {
+                            if (states.contains(WidgetState.pressed)) {
+                              return context.tokens.color.vsdslColorSurface300;
+                            }
+                            return context
+                                .tokens.color.vsdslColorOnSurfaceInverse;
+                          },
+                        ),
+                        backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                          (Set<WidgetState> states) {
+                            if (states.contains(WidgetState.pressed)) {
+                              return context
+                                  .tokens.color.vsdslColorPrimaryVariant;
+                            }
+                            return context.tokens.color.vsdslColorPrimary;
+                          },
+                        ),
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(9999),
+                          ),
+                        ),
+                        elevation: WidgetStateProperty.all(4.0),
+                      ),
+                      onPressed: () {
+                        ref.read(dialogProvider.notifier).hideDialog();
+                        dialogState.onConfirm?.call();
+                      },
+                      child: Text(dialogState.confirmText!),
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
