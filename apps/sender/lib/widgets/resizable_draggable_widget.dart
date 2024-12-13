89893c90579158cc5869d49ab66c735e22a145f7
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2024-12-13 18:38:28 +0800

    feat: [USER STORY #75108] 調整cast to device and cast to board接收端畫面的tab UI設計

---
+++ apps/sender/lib/widgets/resizable_draggable_widget.dart
@@ -0,0 +1,354 @@
+import 'dart:math' as math;
+
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:display_cast_flutter/assets/tokens/tokens.g.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class ResizableDraggableWidget extends StatefulWidget {
+  final double halfScreen;
+  final String text;
+  final VoidCallback onStop;
+  final double height;
+
+  const ResizableDraggableWidget({
+    super.key,
+    required this.halfScreen,
+    required this.text,
+    required this.onStop,
+    this.height = 50,
+  });
+
+  @override
+  State<ResizableDraggableWidget> createState() =>
+      _ResizableDraggableWidgetState();
+}
+
+class _ResizableDraggableWidgetState extends State<ResizableDraggableWidget> {
+  double _currentX = 0;
+  double _width = 0;
+  double _collapsedWidth = 0;
+  bool _isExpanded = true;
+
+  final GlobalKey _textKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final renderBox =
+          _textKey.currentContext?.findRenderObject() as RenderBox?;
+      if (renderBox == null) return;
+
+      setState(() {
+        final textWidth = renderBox.size.width;
+
+        final gap = context.tokens.spacing.vsdswSpacingSm.right;
+        final textPadding = context.tokens.spacing.vsdswSpacingXs.right;
+        _width = textWidth + (26 * 3) + (gap * 3) + (textPadding * 2) + 8;
+        // 1 icon + 2 gaps
+        _collapsedWidth = 26 + (gap * 2);
+
+        // initial position
+        _currentX = widget.halfScreen - (_width / 2);
+      });
+    });
+  }
+
+  double? _previousScreenWidth;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final screenWidth = constraints.maxWidth;
+        double maxAllowedWidth = _isExpanded ? _width : _collapsedWidth;
+
+        if (_previousScreenWidth != null &&
+            _previousScreenWidth != screenWidth) {
+          double oldAvailableSpace = _previousScreenWidth! - maxAllowedWidth;
+          double newAvailableSpace = screenWidth - maxAllowedWidth;
+
+          if (oldAvailableSpace > 0) {
+            double positionRatio = _currentX / oldAvailableSpace;
+            _currentX = positionRatio * newAvailableSpace;
+          } else {
+            _currentX =
+                math.min(math.max(0, _currentX), screenWidth - maxAllowedWidth);
+          }
+        }
+
+        _previousScreenWidth = screenWidth;
+
+        _currentX =
+            math.min(math.max(0, _currentX), screenWidth - maxAllowedWidth);
+
+        return Stack(
+          children: [
+            Positioned(
+              left: _currentX,
+              bottom: 0,
+              child: _isExpanded
+                  ? _buildExpandedWidget(screenWidth, onStop: widget.onStop)
+                  : _buildCollapsedWidget(screenWidth),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _updatePositionForExpandOrCollapse(
+      bool isExpanding, double collapsedWidth, double screenWidth) {
+    double widgetCurrentWidth = _isExpanded ? _width : collapsedWidth;
+    double widgetCenter = _currentX + (widgetCurrentWidth / 2);
+    double screenCenter = screenWidth / 2;
+
+    setState(() {
+      if (widgetCenter > screenCenter) {
+        if (isExpanding) {
+          _currentX -= (_width - collapsedWidth);
+        } else {
+          _currentX += (_width - collapsedWidth);
+        }
+      } else if (widgetCenter == screenCenter) {
+        _currentX = isExpanding
+            ? (screenCenter - _width / 2)
+            : (screenCenter - collapsedWidth / 2);
+      }
+
+      if (isExpanding) {
+        _currentX = _currentX.clamp(0.0, screenWidth - _width);
+      } else {
+        final newX = _currentX.clamp(0.0, screenWidth - collapsedWidth);
+        _currentX = (newX + collapsedWidth > screenWidth)
+            ? (screenWidth - collapsedWidth)
+            : newX;
+      }
+    });
+  }
+
+  Widget _buildExpandedWidget(double screenWidth,
+      {required VoidCallback onStop}) {
+    return Draggable(
+      axis: Axis.horizontal,
+      feedback: const SizedBox.shrink(),
+      childWhenDragging: ExpandedContentWidget(
+        width: _width,
+        height: widget.height,
+        text: widget.text,
+        textKey: _textKey,
+        onMinimize: () {},
+        onStop: () {},
+      ),
+      onDragUpdate: (details) {
+        setState(() {
+          _currentX =
+              (_currentX + details.delta.dx).clamp(0.0, screenWidth - _width);
+        });
+      },
+      onDragEnd: (_) {
+        setState(() {
+          _currentX = _currentX.clamp(0.0, screenWidth - _width);
+        });
+      },
+      child: ExpandedContentWidget(
+        width: _width,
+        height: widget.height,
+        text: widget.text,
+        textKey: _textKey,
+        onMinimize: () {
+          _updatePositionForExpandOrCollapse(
+              false, _collapsedWidth, screenWidth);
+          setState(() => _isExpanded = false);
+        },
+        onStop: onStop,
+      ),
+    );
+  }
+
+  Widget _buildCollapsedWidget(double screenWidth) {
+    return Draggable(
+      axis: Axis.horizontal,
+      feedback: const SizedBox.shrink(),
+      childWhenDragging: CollapsedContentWidget(
+        width: _collapsedWidth,
+        height: widget.height,
+        isDragging: true,
+        parentContext: context,
+      ),
+      onDragUpdate: (details) {
+        setState(() {
+          _currentX = (_currentX + details.delta.dx)
+              .clamp(0.0, screenWidth - _collapsedWidth);
+        });
+      },
+      onDragEnd: (_) {
+        setState(() {
+          _currentX = _currentX.clamp(0.0, screenWidth - _collapsedWidth);
+        });
+      },
+      child: GestureDetector(
+        onTap: () {
+          _updatePositionForExpandOrCollapse(
+              true, _collapsedWidth, screenWidth);
+          setState(() => _isExpanded = true);
+        },
+        child: CollapsedContentWidget(
+          width: _collapsedWidth,
+          height: widget.height,
+          isDragging: false,
+          parentContext: context,
+        ),
+      ),
+    );
+  }
+}
+
+class ExpandedContentWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final String text;
+  final GlobalKey textKey;
+  final VoidCallback onMinimize;
+  final VoidCallback onStop;
+
+  const ExpandedContentWidget({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.text,
+    required this.textKey,
+    required this.onMinimize,
+    required this.onStop,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final borderSide = BorderSide(
+      width: 2,
+      color: context.tokens.color.vsdswColorOnSurface,
+    );
+    final gap = SizedBox(width: context.tokens.spacing.vsdswSpacingSm.right);
+    final textPadding =
+        SizedBox(width: context.tokens.spacing.vsdswSpacingXs.right);
+
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        border: Border(left: borderSide, top: borderSide, right: borderSide),
+        borderRadius: BorderRadius.only(
+          topLeft: context.tokens.radii.vsdswRadiusXl.topLeft,
+          topRight: context.tokens.radii.vsdswRadiusXl.topRight,
+        ),
+        color: context.tokens.color.vsdswColorSuccess,
+      ),
+      child: Row(
+        children: [
+          gap,
+          _buildIcon('assets/images/ic_drag.svg'),
+          textPadding,
+          _buildText(context),
+          textPadding,
+          _buildStopButton(context),
+          gap,
+          _buildMinimizeButton(),
+          gap,
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIcon(String assetPath) {
+    return Container(
+      width: 26,
+      height: 26,
+      alignment: Alignment.center,
+      child: SvgPicture.asset(assetPath),
+    );
+  }
+
+  Widget _buildText(BuildContext context) {
+    return AutoSizeText(
+      key: textKey,
+      text,
+      style: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w700,
+        color: context.tokens.color.vsdswColorOnSuccess,
+      ),
+      maxLines: 1,
+    );
+  }
+
+  Widget _buildStopButton(BuildContext context) {
+    return GestureDetector(
+      onTap: onStop,
+      child: SizedBox(
+        width: 26,
+        height: 26,
+        child: CircleAvatar(
+          radius: 13,
+          backgroundColor: context.tokens.color.vsdswColorError,
+          child: Container(
+            width: 8,
+            height: 8,
+            decoration: BoxDecoration(
+              color: context.tokens.color.vsdswColorSurface100,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(2),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMinimizeButton() {
+    return GestureDetector(
+      onTap: onMinimize,
+      child: _buildIcon('assets/images/ic_minimize.svg'),
+    );
+  }
+}
+
+class CollapsedContentWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final bool isDragging;
+  final BuildContext parentContext;
+
+  const CollapsedContentWidget({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.isDragging,
+    required this.parentContext,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final borderSide = BorderSide(
+      width: 2,
+      color: parentContext.tokens.color.vsdswColorOnSurface,
+    );
+    final gap = parentContext.tokens.spacing.vsdswSpacingSm.right;
+
+    return Container(
+      padding: EdgeInsets.only(right: gap, left: gap, bottom: 4),
+      width: width,
+      height: height * 0.7,
+      decoration: BoxDecoration(
+        border: Border(left: borderSide, top: borderSide, right: borderSide),
+        borderRadius: BorderRadius.only(
+          topLeft: parentContext.tokens.radii.vsdswRadiusXl.topLeft,
+          topRight: parentContext.tokens.radii.vsdswRadiusXl.topRight,
+        ),
+        color: parentContext.tokens.color.vsdswColorSuccess
+            .withOpacity(isDragging ? 1.0 : 0.3),
+      ),
+      alignment: Alignment.center,
+      child: SvgPicture.asset('assets/images/ic_expend.svg'),
+    );
+  }
+}
--- apps/sender/lib/widgets/v3_remote_screen.dart
+++ apps/sender/lib/widgets/v3_remote_screen.dart
@@ -1,8 +1,8 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:display_cast_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/channel_provider.dart';
 import 'package:display_cast_flutter/utilities/app_analytics.dart';
+import 'package:display_cast_flutter/widgets/resizable_draggable_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
@@ -60,84 +60,19 @@ class _V3RemoteScreenState extends State<V3RemoteScreen> {
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      height: 50,
-                      padding: EdgeInsets.symmetric(
-                        vertical: context.tokens.spacing.vsdswSpacingXs.top,
-                        horizontal: context.tokens.spacing.vsdswSpacingMd.left,
-                      ),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          left: BorderSide(
-                            width: 2,
-                            color: context.tokens.color.vsdswColorOnSurface,
-                          ),
-                          top: BorderSide(
-                            width: 2,
-                            color: context.tokens.color.vsdswColorOnSurface,
-                          ),
-                          right: BorderSide(
-                            width: 2,
-                            color: context.tokens.color.vsdswColorOnSurface,
-                          ),
-                        ),
-                        borderRadius: BorderRadius.only(
-                            topLeft: context.tokens.radii.vsdswRadiusXl.topLeft,
-                            topRight:
-                                context.tokens.radii.vsdswRadiusXl.topRight),
-                        color: context.tokens.color.vsdswColorSuccess,
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          AutoSizeText(
-                            S.current.v3_main_receive_app_receive_from
-                                .replaceAll(
-                                    '%s', channelProvider.deviceName ?? ''),
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                              color: context.tokens.color.vsdswColorOnSuccess,
-                            ),
-                            maxLines: 1,
-                          ),
-                          SizedBox(
-                              width:
-                                  context.tokens.spacing.vsdswSpacingMd.right),
-                          ElevatedButton.icon(
-                            onPressed: () {
-                              trackEvent('click_exit', EventCategory.session);
+              ResizableDraggableWidget(
+                halfScreen: MediaQuery.of(context).size.width / 2,
+                text: S.current.v3_main_receive_app_receive_from
+                    .replaceAll('%s', channelProvider.deviceName ?? ''),
+                onStop: () {
+                  trackEvent('click_exit', EventCategory.session);
 
-                              if (widget.isModeratorShare) {
-                                channelProvider.removeShareRemoteScreenClient();
-                              } else {
-                                channelProvider.removeRemoteScreenClient();
-                              }
-                            },
-                            label: Text(
-                              S.of(context).v3_main_receive_app_stop,
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: context.tokens.color.vsdswColorOnSurface,
-                              ),
-                            ),
-                            icon: Icon(
-                              Icons.stop,
-                              size: 16,
-                              color: context.tokens.color.vsdswColorError,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  if (widget.isModeratorShare) {
+                    channelProvider.removeShareRemoteScreenClient();
+                  } else {
+                    channelProvider.removeRemoteScreenClient();
+                  }
+                },
               ),
               IgnorePointer(
                 child: Stack(
