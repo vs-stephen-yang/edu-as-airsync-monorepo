1fe4573b22e6a4c94061e04996545d058f161224
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2024-12-17 18:10:02 +0800

    feat: [USER STORY #75108] 調整cast to device and cast to board接收端畫面的tab UI設計 for demo

--- apps/sender/lib/demo/v3_remote_screen_widget_demo.dart
+++ apps/sender/lib/demo/v3_remote_screen_widget_demo.dart
@@ -1,7 +1,7 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:display_cast_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/v3_demo_provider.dart';
+import 'package:display_cast_flutter/widgets/resizable_draggable_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -47,8 +47,6 @@ class _V3RemoteScreenDemoState extends State<V3RemoteScreenDemo> {
           return Stack(
             alignment: Alignment.center,
             children: [
-              // const RemoteVideoView(),
-              // Positioned.fill(
               Center(
                 child: Image.asset(
                   'assets/images/demo_remote.png',
@@ -65,77 +63,13 @@ class _V3RemoteScreenDemoState extends State<V3RemoteScreenDemo> {
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      height: 50,
-                      padding: EdgeInsets.symmetric(
-                        vertical: context.tokens.spacing.vsdswSpacingXs.top,
-                        horizontal: context.tokens.spacing.vsdswSpacingMd.left,
-                      ),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          left: BorderSide(
-                            width: 2,
-                            color: context.tokens.color.vsdswColorOnSurface,
-                          ),
-                          top: BorderSide(
-                            width: 2,
-                            color: context.tokens.color.vsdswColorOnSurface,
-                          ),
-                          right: BorderSide(
-                            width: 2,
-                            color: context.tokens.color.vsdswColorOnSurface,
-                          ),
-                        ),
-                        borderRadius: BorderRadius.only(
-                            topLeft: context.tokens.radii.vsdswRadiusXl.topLeft,
-                            topRight:
-                                context.tokens.radii.vsdswRadiusXl.topRight),
-                        color: context.tokens.color.vsdswColorSuccess,
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          AutoSizeText(
-                            S.current.v3_main_receive_app_receive_from
-                                .replaceAll('%s', 'ViewSonic Service'),
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                              color: context.tokens.color.vsdswColorOnSuccess,
-                            ),
-                            maxLines: 1,
-                          ),
-                          SizedBox(
-                              width:
-                                  context.tokens.spacing.vsdswSpacingMd.right),
-                          ElevatedButton.icon(
-                            onPressed: () {
-                              channelProvider.presentDemoOff();
-                            },
-                            label: Text(
-                              S.of(context).v3_main_receive_app_stop,
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: context.tokens.color.vsdswColorOnSurface,
-                              ),
-                            ),
-                            icon: Icon(
-                              Icons.stop,
-                              size: 16,
-                              color: context.tokens.color.vsdswColorError,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+              ResizableDraggableWidget(
+                halfScreen: MediaQuery.of(context).size.width / 2,
+                text: S.current.v3_main_receive_app_receive_from
+                    .replaceAll('%s', 'ViewSonic Service'),
+                onStop: () {
+                  channelProvider.presentDemoOff();
+                },
               ),
               IgnorePointer(
                 child: Stack(
