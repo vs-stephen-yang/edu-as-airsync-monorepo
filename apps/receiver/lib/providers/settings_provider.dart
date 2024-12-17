1da7a1c21ea4cbdea2536543ffd6e583d384b98a
Loren Chang <loren.chang@tw.viewsonic.com>
2024-12-17 18:38:54 +0800

    feat: Add locked preference option for menu functionality.

--- apps/receiver/lib/providers/settings_provider.dart
+++ apps/receiver/lib/providers/settings_provider.dart
@@ -1,5 +1,6 @@
 import 'package:display_flutter/oss_licenses.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 enum SettingPageState {
   deviceSetting,
@@ -15,7 +16,9 @@ enum SettingPageState {
 }
 
 class SettingsProvider with ChangeNotifier {
-  SettingsProvider();
+  SettingsProvider() {
+    _load();
+  }
 
   SettingPageState _currentPage = SettingPageState.deviceSetting;
 
@@ -28,6 +31,63 @@ class SettingsProvider with ChangeNotifier {
   Package? _license;
 
   Package? get license => _license;
+
+  static const defaultDeviceSettingLock = false;
+  static const defaultBroadcastLock = false;
+  static const defaultMirroringLock = false;
+  static const defaultConnectivityLock = false;
+  bool _isDeviceSettingLock = defaultDeviceSettingLock;
+  bool _isBroadcastLock = defaultBroadcastLock;
+  bool _isMirroringLock = defaultMirroringLock;
+  bool _isConnectivityLock = defaultConnectivityLock;
+
+  bool get isDeviceSettingLock => _isDeviceSettingLock;
+
+  set isDeviceSettingLock(bool value) {
+    _isDeviceSettingLock = value;
+    _save();
+  }
+
+  bool get isBroadcastLock => _isBroadcastLock;
+
+  set isBroadcastLock(bool value) {
+    _isBroadcastLock = value;
+    _save();
+  }
+
+  bool get isMirroringLock => _isMirroringLock;
+
+  set isMirroringLock(bool value) {
+    isMirroringLock = value;
+    _save();
+  }
+
+  bool get isConnectivityLock => _isConnectivityLock;
+
+  set isConnectivityLock(bool value) {
+    isConnectivityLock = value;
+    _save();
+  }
+
+  _save() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('app_isDeviceSettingLock', _isDeviceSettingLock);
+    await prefs.setBool('app_isBroadcastLock', _isBroadcastLock);
+    await prefs.setBool('app_isMirroringLock', _isMirroringLock);
+    await prefs.setBool('app_isConnectivityLock', _isConnectivityLock);
+  }
+
+  _load() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isDeviceSettingLock =
+        prefs.getBool('app_isDeviceSettingLock') ?? defaultDeviceSettingLock;
+    _isBroadcastLock =
+        prefs.getBool('app_isBroadcastLock') ?? defaultBroadcastLock;
+    _isMirroringLock =
+        prefs.getBool('app_isMirroringLock') ?? defaultMirroringLock;
+    _isConnectivityLock =
+        prefs.getBool('app_isConnectivityLock') ?? defaultConnectivityLock;
+  }
 
   setPage(SettingPageState state, {Package? license}) {
     switch (state) {
