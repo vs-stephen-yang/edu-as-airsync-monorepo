6f3e7891297642c13b1c8b36a273f42ae4c8bcf4
Loren Chang <loren.chang@tw.viewsonic.com>
2024-12-30 13:05:08 +0800

    feat: [USER STORY #74861] AirSync Manager integration: UIUX for lock (p3)
    Add a SingleChildScrollView to the 2nd layer to enable scrolling when the child content is too long.

--- apps/receiver/lib/widgets/v3_setting_2ndLayer.dart
+++ apps/receiver/lib/widgets/v3_setting_2ndLayer.dart
@@ -10,10 +10,12 @@ class V3Setting2ndLayer extends StatelessWidget {
     super.key,
     required this.child,
     this.isDisable = false,
+    this.isDisableFromModerator = false,
   });
 
   final Widget child;
   final bool isDisable;
+  final bool isDisableFromModerator;
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +25,51 @@ class V3Setting2ndLayer extends StatelessWidget {
           left: context.tokens.spacing.vsdslSpacingXl.left,
           top: 57,
           right: context.tokens.spacing.vsdslSpacingXl.right,
-          bottom: context.tokens.spacing.vsdslSpacingXl.bottom,
-          child: child,
+          bottom: context.tokens.spacing.vsdslSpacingXl.bottom +
+              // 67 = 51 (Size of message height) + 16 (Figma space: 48)
+              (isDisable ? 67 : 0),
+          child: SingleChildScrollView(
+            child: child,
+          ),
         ),
+        if (isDisableFromModerator)
+          Positioned(
+            left: context.tokens.spacing.vsdslSpacingXl.left,
+            right: context.tokens.spacing.vsdslSpacingXl.right,
+            bottom: context.tokens.spacing.vsdslSpacingXl.bottom,
+            child: Container(
+              width: 325,
+              height: 51,
+              decoration: BoxDecoration(
+                color: context.tokens.color.vsdslColorSurface900,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: context.tokens.spacing.vsdslSpacingXl,
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/ic_toast_alert.svg',
+                    width: 16,
+                    height: 16,
+                  ),
+                  Gap(context.tokens.spacing.vsdslSpacingLg.right),
+                  SizedBox(
+                    width: 270,
+                    child: AutoSizeText(
+                      S.of(context).v3_settings_mirroring_blocked,
+                      style: TextStyle(
+                        fontSize: 9,
+                        fontWeight: FontWeight.w400,
+                        color: context.tokens.color.vsdslColorWarning,
+                      ),
+                      minFontSize: 8,
+                      maxLines: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         if (isDisable)
           Positioned(
             left: context.tokens.spacing.vsdslSpacingXl.left,
