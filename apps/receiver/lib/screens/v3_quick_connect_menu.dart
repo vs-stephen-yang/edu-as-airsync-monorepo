795d0e71939403d0b1ad071aa7940317ad348ccc
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2025-01-02 16:03:22 +0800

    feat: [USER STORY #71083] Support remote controller on casting view P2, with tabbar with focus

--- apps/receiver/lib/screens/v3_quick_connect_menu.dart
+++ apps/receiver/lib/screens/v3_quick_connect_menu.dart
@@ -4,11 +4,36 @@ import 'package:display_flutter/widgets/v3_focus.dart';
 import 'package:display_flutter/widgets/v3_instruction.dart';
 import 'package:display_flutter/widgets/v3_qrcode_quick_connect.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:no_context_navigation/no_context_navigation.dart';
 
-class V3QuickConnectMenu extends StatelessWidget {
+class V3QuickConnectMenu extends StatefulWidget {
   const V3QuickConnectMenu({super.key});
+
+  @override
+  _V3QuickConnectMenuState createState() => _V3QuickConnectMenuState();
+}
+
+class _V3QuickConnectMenuState extends State<V3QuickConnectMenu> {
+  final List<FocusNode> _focusNodes = List.generate(2, (_) => FocusNode());
+  int selected = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: selected);
+  }
+
+  @override
+  void dispose() {
+    for (var node in _focusNodes) {
+      node.dispose();
+    }
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,38 +66,104 @@ class V3QuickConnectMenu extends StatelessWidget {
                           borderRadius: context.tokens.radii.vsdslRadiusLg,
                           color: context.tokens.color.vsdslColorSurface200,
                         ),
-                        child: TabBar(
-                          indicator: BoxDecoration(
-                            color: context.tokens.color.vsdslColorPrimary,
-                            borderRadius: context.tokens.radii.vsdslRadiusLg,
-                          ),
-                          indicatorSize: TabBarIndicatorSize.tab,
-                          labelColor:
-                              context.tokens.color.vsdslColorOnSurfaceInverse,
-                          unselectedLabelColor:
-                              context.tokens.color.vsdslColorPrimary,
-                          dividerHeight: 0,
-                          tabs: [
-                            // TODO: add focus
-                            Tab(
-                              text: S
-                                  .of(context)
-                                  .v3_quick_connect_menu_display_code,
-                            ),
-                            // TODO: add focus
-                            Tab(
-                              text: S.of(context).v3_quick_connect_menu_qrcode,
-                            ),
-                          ],
+                        child: Row(
+                          children: List.generate(_focusNodes.length, (index) {
+                            return Expanded(
+                              child: Focus(
+                                focusNode: _focusNodes[index],
+                                onFocusChange: (hasFocus) {
+                                  if (hasFocus) {
+                                    _focusNodes[index].requestFocus();
+                                    setState(() {});
+                                  } else {
+                                    _focusNodes[index].unfocus();
+                                    setState(() {});
+                                  }
+                                },
+                                onKey: (node, event) {
+                                  if (event.logicalKey ==
+                                      LogicalKeyboardKey.enter) {
+                                    setState(() {
+                                      selected = index;
+                                    });
+                                    _pageController.animateToPage(
+                                      index,
+                                      duration:
+                                          const Duration(milliseconds: 300),
+                                      curve: Curves.ease,
+                                    );
+                                    return KeyEventResult.handled;
+                                  }
+                                  return KeyEventResult.ignored;
+                                },
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      selected = index;
+                                    });
+                                    _pageController.animateToPage(
+                                      index,
+                                      duration:
+                                          const Duration(milliseconds: 300),
+                                      curve: Curves.ease,
+                                    );
+                                  },
+                                  child: AnimatedContainer(
+                                    duration: const Duration(milliseconds: 200),
+                                    decoration: BoxDecoration(
+                                      borderRadius:
+                                          context.tokens.radii.vsdslRadiusLg,
+                                      border: Border.all(
+                                        color: _focusNodes[index].hasFocus
+                                            ? context
+                                                .tokens.color.vsdslColorPrimary
+                                            : Colors.transparent,
+                                        width: 2.0,
+                                      ),
+                                      color: selected == index
+                                          ? context
+                                              .tokens.color.vsdslColorPrimary
+                                          : Colors.transparent,
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      index == 0
+                                          ? S
+                                              .of(context)
+                                              .v3_quick_connect_menu_display_code
+                                          : S
+                                              .of(context)
+                                              .v3_quick_connect_menu_qrcode,
+                                      style: TextStyle(
+                                        color: selected == index
+                                            ? context.tokens.color
+                                                .vsdslColorOnSurfaceInverse
+                                            : context
+                                                .tokens.color.vsdslColorPrimary,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
                         ),
                       ),
                     ),
-                    const Positioned(
+                    Positioned(
                       top: 117,
                       height: 340,
                       width: 485,
-                      child: TabBarView(
-                        children: [
+                      child: PageView(
+                        controller: _pageController,
+                        onPageChanged: (index) {
+                          setState(() {
+                            selected = index;
+                            FocusScope.of(context)
+                                .requestFocus(_focusNodes[index]);
+                          });
+                        },
+                        children: const [
                           V3Instruction(isQuickConnect: true),
                           V3QrCodeQuickConnect(isStringOnTop: true, size: 195),
                         ],
