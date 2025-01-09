ffae261fa898021e07067c0f4f294e957061694b
Ken Chung <ken.mh.chung@tw.viewsonic.com>
2025-01-09 16:48:14 +0800

    fix: [BUG #78295] 遙控器往上方移動到最上面後Focus會不見 (Device list/ Participants list)

--- apps/receiver/lib/screens/v3_participants_menu.dart
+++ apps/receiver/lib/screens/v3_participants_menu.dart
@@ -28,44 +28,47 @@ class V3ParticipantsMenu extends StatelessWidget {
             insetPadding: EdgeInsets.zero,
             elevation: 16.0,
             shadowColor: context.tokens.color.vsdslColorOpacityNeutralSm,
-            child: SizedBox(
-              width: 384,
-              height: 442,
-              child: Stack(
-                children: [
-                  // This is for accessibility
-                  const Focus(autofocus: true, child: SizedBox()),
-                  const Positioned(
-                    left: 13,
-                    top: 27,
-                    right: 13,
-                    bottom: 66,
-                    child: V3ParticipantList(isForMenuUse: true),
-                  ),
-                  Positioned(
-                    right: 5,
-                    bottom: 5,
-                    child: V3Focus(
-                      child: SizedBox(
-                        width: 33,
-                        height: 33,
-                        child: IconButton(
-                          focusNode: primaryFocusNode,
-                          icon: const Image(
-                            image: Svg('assets/images/ic_menu_close_gray.svg'),
+            child: FocusScope(
+              autofocus: true,
+              node: FocusScopeNode(),
+              child: SizedBox(
+                width: 384,
+                height: 442,
+                child: Stack(
+                  children: [
+                    const Positioned(
+                      left: 13,
+                      top: 27,
+                      right: 13,
+                      bottom: 66,
+                      child: V3ParticipantList(isForMenuUse: true),
+                    ),
+                    Positioned(
+                      right: 5,
+                      bottom: 5,
+                      child: V3Focus(
+                        child: SizedBox(
+                          width: 33,
+                          height: 33,
+                          child: IconButton(
+                            focusNode: primaryFocusNode,
+                            icon: const Image(
+                              image:
+                                  Svg('assets/images/ic_menu_close_gray.svg'),
+                            ),
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            onPressed: () {
+                              if (navService.canPop()) {
+                                navService.goBack();
+                              }
+                            },
                           ),
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          onPressed: () {
-                            if (navService.canPop()) {
-                              navService.goBack();
-                            }
-                          },
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
