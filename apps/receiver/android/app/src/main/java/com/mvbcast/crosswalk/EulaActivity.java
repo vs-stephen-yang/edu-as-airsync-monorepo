ac11f1b0b1385aec9ae856305300c01241ab547a
Loren Chang <loren.chang@tw.viewsonic.com>
2025-01-10 18:09:56 +0800

    feat: [USER STORY #78411] Use full screen space for AirSync on EDLA model

--- apps/receiver/android/app/src/main/java/com/mvbcast/crosswalk/EulaActivity.java
+++ apps/receiver/android/app/src/main/java/com/mvbcast/crosswalk/EulaActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
@@ -13,18 +14,24 @@ import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
 import com.mvbcast.crosswalk.vbsota.SystemImageOTAHelper;
 import com.mvbcast.crosswalk.vsapi.VSApiHandler;
+
+import java.util.Calendar;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-
-import java.util.Calendar;
 
 public class EulaActivity extends FlutterActivity {
     private static final String TAG = EulaActivity.class.getSimpleName();
@@ -143,6 +150,12 @@ public class EulaActivity extends FlutterActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        enableFullscreenMode(this);
+    }
+
+    @Override
     protected void onDestroy() {
         if (vsApiHandler != null) {
             vsApiHandler.dispose();
@@ -206,4 +219,34 @@ public class EulaActivity extends FlutterActivity {
     }
     // endregion
 
+    private void enableFullscreenMode(FlutterActivity activity) {
+        if (activity.getWindow() != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                // For Android 11 (API 30) and above
+                WindowInsetsController windowInsetsController =
+                        activity.getWindow().getInsetsController();
+                if (windowInsetsController != null) {
+                    windowInsetsController.hide(WindowInsets.Type.statusBars() |
+                            WindowInsets.Type.navigationBars());
+                    windowInsetsController.
+                            setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                }
+                activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                // For Android 4.4 (API 19) to Android 10 (API 29)
+                activity.getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            } else {
+                // For Android versions below 4.4 (API 19)
+                activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        }
+    }
 }
