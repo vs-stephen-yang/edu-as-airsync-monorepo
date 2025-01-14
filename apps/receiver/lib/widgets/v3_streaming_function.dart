863fc48768a78239f45f5438ab05822c5232a00c
Loren Chang <loren.chang@tw.viewsonic.com>
2025-01-14 17:28:34 +0800

    fix: [BUG #78549] [遙控器]在Streaming中打開下方功能開關後，再點擊收合開關時，需持續focus在收合後的icon

--- apps/receiver/lib/widgets/v3_streaming_function.dart
+++ apps/receiver/lib/widgets/v3_streaming_function.dart
@@ -12,7 +12,7 @@ import 'package:display_flutter/utility/v3_toast.dart';
 import 'package:display_flutter/widgets/split_screen_function.dart';
 import 'package:display_flutter/widgets/v3_focus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class V3StreamingFunction extends StatefulWidget {
@@ -42,126 +42,53 @@ class _V3StreamingFunctionState extends State<V3StreamingFunction> {
 
   @override
   Widget build(BuildContext context) {
-    return isCollapsed
-        ? _CollapsedView(onExpand: () {
-            _toggleCollapse();
-            _startAutoCollapseTimer();
-          })
-        : _ExpandedView(
-            index: widget.index,
-            onUserInteraction: _startAutoCollapseTimer,
-            onCollapse: () {
-              _toggleCollapse();
-              autoCollapseTimer?.cancel();
-            },
-          );
-  }
-
-  void _startAutoCollapseTimer() {
-    autoCollapseTimer?.cancel();
-    autoCollapseTimer = Timer(const Duration(seconds: 10), () {
-      setState(() {
-        isCollapsed = true;
-      });
-    });
-  }
-
-  void _toggleCollapse() {
-    setState(() {
-      isCollapsed = !isCollapsed;
-    });
-  }
-}
-
-class _CollapsedView extends StatelessWidget {
-  final VoidCallback onExpand;
-
-  const _CollapsedView({required this.onExpand});
-
-  @override
-  Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints.tightFor(width: 37, height: 22),
-      child: Container(
-        padding: const EdgeInsets.only(top: 4),
-        decoration: BoxDecoration(
-          color:
-              context.tokens.color.vsdslColorOpacityNeutralXl.withOpacity(0.48),
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(50),
-            topRight: Radius.circular(50),
-          ),
-        ),
-        child: V3Focus(
-          child: IconButton(
-            icon: const Image(
-              image: Svg('assets/images/ic_expend.svg'),
-              semanticLabel: 'Expand',
-            ),
-            padding: EdgeInsets.zero,
-            constraints: const BoxConstraints(),
-            onPressed: onExpand,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _ExpandedView extends StatefulWidget {
-  final int index;
-  final VoidCallback onCollapse;
-  final VoidCallback onUserInteraction;
-
-  const _ExpandedView({
-    required this.index,
-    required this.onCollapse,
-    required this.onUserInteraction,
-  });
-
-  @override
-  State<StatefulWidget> createState() => _ExpandedViewState();
-}
-
-class _ExpandedViewState extends State<_ExpandedView> {
-  @override
-  Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
+      padding: isCollapsed ? EdgeInsets.zero : const EdgeInsets.only(bottom: 8),
       child: ConstrainedBox(
         constraints: BoxConstraints.tightFor(
-          width: HybridConnectionList().getConnectionCount() > 1 ? 140 : 106,
-          height: 43,
+          width: isCollapsed
+              ? 37
+              : HybridConnectionList.hybridSplitScreenCount.value > 1
+                  ? 140
+                  : 106,
+          height: isCollapsed ? 22 : 43,
         ),
         child: Container(
+          padding: isCollapsed ? const EdgeInsets.only(top: 4) : null,
           decoration: BoxDecoration(
             color: context.tokens.color.vsdslColorOpacityNeutralXl
                 .withOpacity(0.48),
-            borderRadius: context.tokens.radii.vsdslRadiusFull,
+            borderRadius: isCollapsed
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50),
+                  )
+                : context.tokens.radii.vsdslRadiusFull,
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              if (HybridConnectionList.hybridSplitScreenCount.value > 1)
-                V3Focus(
+              // Use Visibility Widget to Maintain Focus on the Correct Icon During Collapse/Expand.
+              Visibility(
+                visible: !isCollapsed &&
+                    HybridConnectionList.hybridSplitScreenCount.value > 1,
+                child: V3Focus(
                   child: SizedBox(
                     width: 27,
                     height: 27,
                     child: IconButton(
-                      icon: Image(
-                        image:
-                            HybridConnectionList().enlargedScreenIndex.value ==
-                                    widget.index
-                                ? const Svg(
-                                    'assets/images/ic_streaming_collapse.svg')
-                                : const Svg(
-                                    'assets/images/ic_streaming_expand.svg'),
+                      icon: SvgPicture.asset(
+                        HybridConnectionList().enlargedScreenIndex.value ==
+                                widget.index
+                            ? 'assets/images/ic_streaming_collapse.svg'
+                            : 'assets/images/ic_streaming_expand.svg',
                       ),
+                      focusColor: Colors.transparent,
                       padding: EdgeInsets.zero,
                       constraints: const BoxConstraints(),
                       onPressed: () {
-                        widget.onUserInteraction();
+                        _startAutoCollapseTimer();
                         if (HybridConnectionList()
                             .isMirrorRequest(widget.index)) {
                           var connection = HybridConnectionList()
@@ -198,105 +125,140 @@ class _ExpandedViewState extends State<_ExpandedView> {
                     ),
                   ),
                 ),
-              Consumer<MirrorStateProvider>(
-                builder: (_, mirrorStateProvider, __) {
-                  var isMute = HybridConnectionList()
-                      .getAudioDisableStateByIndex(widget.index);
-                  return V3Focus(
-                    child: SizedBox(
-                      width: 27,
-                      height: 27,
-                      child: IconButton(
-                        icon: Image(
-                          image: isMute
-                              ? const Svg(
-                                  'assets/images/ic_streaming_unmute.svg')
-                              : const Svg(
-                                  'assets/images/ic_streaming_mute.svg'),
-                        ),
-                        padding: EdgeInsets.zero,
-                        constraints: const BoxConstraints(),
-                        onPressed: () {
-                          widget.onUserInteraction();
+              ),
+              Visibility(
+                visible: !isCollapsed,
+                child: Consumer<MirrorStateProvider>(
+                  builder: (_, mirrorStateProvider, __) {
+                    var isMute = HybridConnectionList()
+                        .getAudioDisableStateByIndex(widget.index);
+                    return V3Focus(
+                      child: SizedBox(
+                        width: 27,
+                        height: 27,
+                        child: IconButton(
+                          icon: SvgPicture.asset(
+                            isMute
+                                ? 'assets/images/ic_streaming_unmute.svg'
+                                : 'assets/images/ic_streaming_mute.svg',
+                          ),
+                          focusColor: Colors.transparent,
+                          padding: EdgeInsets.zero,
+                          constraints: const BoxConstraints(),
+                          onPressed: () {
+                            _startAutoCollapseTimer();
 
-                          setState(() {
-                            HybridConnectionList()
-                                .updateAudioEnableStateByIndex(
-                                    widget.index, isMute, true);
-                          });
-                        },
+                            setState(() {
+                              HybridConnectionList()
+                                  .updateAudioEnableStateByIndex(
+                                      widget.index, isMute, true);
+                            });
+                          },
+                        ),
                       ),
+                    );
+                  },
+                ),
+              ),
+              Visibility(
+                visible: !isCollapsed,
+                child: V3Focus(
+                  child: SizedBox(
+                    width: 27,
+                    height: 27,
+                    child: IconButton(
+                      icon: SvgPicture.asset(
+                        'assets/images/ic_streaming_stop.svg',
+                      ),
+                      focusColor: Colors.transparent,
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(),
+                      onPressed: () {
+                        if (HybridConnectionList()
+                            .isMirrorRequest(widget.index)) {
+                          var connection = HybridConnectionList()
+                              .getConnection<MirrorRequest>(widget.index);
+                          if (connection.mirrorState == MirrorState.mirroring) {
+                            HybridConnectionList()
+                                .stopPresenterBy(widget.index);
+                            return;
+                          }
+                        }
+                        RTCConnector webrtcConnector = HybridConnectionList()
+                            .getConnection<RTCConnector>(widget.index);
+                        if (webrtcConnector.isChannelReconnect()) {
+                          webrtcConnector.clickButtonWhenReconnect = true;
+                          V3Toast().makeSplitScreenReconnectToast(
+                              context,
+                              S.of(context).main_feature_reconnecting_toast,
+                              widget.index,
+                              isWebRTC: false,
+                              state: webrtcConnector.reconnectChannelState);
+                        } else {
+                          webrtcConnector.trackSessionEvent('stop_cast');
+
+                          if (ChannelProvider.isModeratorMode) {
+                            HybridConnectionList()
+                                .stopPresenterBy(widget.index);
+                          } else {
+                            SplitScreenFunction.isMenuOnList.value.fillRange(
+                                0,
+                                SplitScreenFunction.isMenuOnList.value.length,
+                                false);
+                            HybridConnectionList()
+                                .removePresenterBy(widget.index);
+                          }
+                        }
+                      },
                     ),
-                  );
-                },
+                  ),
+                ),
               ),
               V3Focus(
                 child: SizedBox(
                   width: 27,
                   height: 27,
                   child: IconButton(
-                    icon: const Image(
-                      image: Svg('assets/images/ic_streaming_stop.svg'),
+                    icon: SvgPicture.asset(
+                      isCollapsed
+                          ? 'assets/images/ic_expend.svg'
+                          : 'assets/images/ic_minimize.svg',
+                      semanticsLabel: isCollapsed ? 'Expand' : 'Minimize',
                     ),
+                    focusColor: Colors.transparent,
                     padding: EdgeInsets.zero,
                     constraints: const BoxConstraints(),
                     onPressed: () {
-                      if (HybridConnectionList()
-                          .isMirrorRequest(widget.index)) {
-                        var connection = HybridConnectionList()
-                            .getConnection<MirrorRequest>(widget.index);
-                        if (connection.mirrorState == MirrorState.mirroring) {
-                          HybridConnectionList().stopPresenterBy(widget.index);
-                          return;
-                        }
-                      }
-                      RTCConnector webrtcConnector = HybridConnectionList()
-                          .getConnection<RTCConnector>(widget.index);
-                      if (webrtcConnector.isChannelReconnect()) {
-                        webrtcConnector.clickButtonWhenReconnect = true;
-                        V3Toast().makeSplitScreenReconnectToast(
-                            context,
-                            S.of(context).main_feature_reconnecting_toast,
-                            widget.index,
-                            isWebRTC: false,
-                            state: webrtcConnector.reconnectChannelState);
+                      _toggleCollapse();
+                      if (isCollapsed) {
+                        autoCollapseTimer?.cancel();
                       } else {
-                        webrtcConnector.trackSessionEvent('stop_cast');
-
-                        if (ChannelProvider.isModeratorMode) {
-                          HybridConnectionList().stopPresenterBy(widget.index);
-                        } else {
-                          SplitScreenFunction.isMenuOnList.value.fillRange(
-                              0,
-                              SplitScreenFunction.isMenuOnList.value.length,
-                              false);
-                          HybridConnectionList()
-                              .removePresenterBy(widget.index);
-                        }
+                        _startAutoCollapseTimer();
                       }
                     },
                   ),
                 ),
-              ),
-              V3Focus(
-                child: SizedBox(
-                    width: 27,
-                    height: 27,
-                    child: IconButton(
-                      icon: const Image(
-                        image: Svg('assets/images/ic_minimize.svg'),
-                        semanticLabel: 'Minimize',
-                      ),
-                      padding: EdgeInsets.zero,
-                      constraints: const BoxConstraints(),
-                      onPressed: widget.onCollapse,
-                    )),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _startAutoCollapseTimer() {
+    autoCollapseTimer?.cancel();
+    autoCollapseTimer = Timer(const Duration(seconds: 10), () {
+      setState(() {
+        isCollapsed = true;
+      });
+    });
+  }
+
+  void _toggleCollapse() {
+    setState(() {
+      isCollapsed = !isCollapsed;
+    });
   }
 
   _updateSizeForSelected(int selection) {
