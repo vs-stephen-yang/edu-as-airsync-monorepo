b7ae06bef57a463f3a0a027239be42ab8a713511
benjamin.lee <benjamin.lee@viewsonic.com>
2025-01-14 15:25:49 +0800

    feature(macos): select virtual audio device to default output when casting

--- apps/sender/macos/Runner/AppDelegate.swift
+++ apps/sender/macos/Runner/AppDelegate.swift
@@ -3,29 +3,30 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+  
+  override func applicationDidFinishLaunching(_ notification: Notification) {
+    let controller : FlutterViewController = mainFlutterWindow!.contentViewController as! FlutterViewController
+    let window = mainFlutterWindow!
     
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-
-    override func applicationDidFinishLaunching(_ notification: Notification) {
-        let controller : FlutterViewController = mainFlutterWindow!.contentViewController as! FlutterViewController
-        let window = mainFlutterWindow!
-
-        let windowManagerChannel = FlutterMethodChannel(
-            name: "com.viewsonic.display.cast/window_manager",
-            binaryMessenger: controller.engine.binaryMessenger)
-
-        windowManagerChannel.setMethodCallHandler({
-          (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-          if call.method == "minimizeWindow" {
-            window.miniaturize(nil) // 這行代碼實現窗口最小化
-            result(nil)
-          } else {
-            result(FlutterMethodNotImplemented)
-          }
-        })
-
-        super.applicationDidFinishLaunching(notification)
+    let windowManagerChannel = FlutterMethodChannel(
+      name: "com.viewsonic.display.cast/window_manager",
+      binaryMessenger: controller.engine.binaryMessenger)
+    
+    windowManagerChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      if call.method == "minimizeWindow" {
+        window.miniaturize(nil) // 這行代碼實現窗口最小化
+        result(nil)
+      } else {
+        result(FlutterMethodNotImplemented)
       }
+    })
+    
+    AudioSwitch.register(with: controller.registrar(forPlugin: "audio_switch"))
+    super.applicationDidFinishLaunching(notification)
+  }
 }
