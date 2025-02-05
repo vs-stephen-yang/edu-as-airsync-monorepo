1ba107a12413bafbfbd8284c3cc9f820dccd6eaf
Vladka Huang <huangvl@VI614697-11.local>
2025-02-05 16:03:11 +0800

    feat: [USER STORY #79586] add tracktrace rtc inbound stats

--- apps/receiver/lib/utility/app_analytics_util.dart
+++ apps/receiver/lib/utility/app_analytics_util.dart
@@ -2,6 +2,8 @@ import 'package:display_flutter/app_analytics.dart';
 import 'package:display_flutter/model/rtc_stats.dart';
 import 'package:display_flutter/utility/rtc_stats_monitor.dart';
 
+import 'list_util.dart';
+
 class RtcVideoInboundStatsLists {
   List<double?> framesPerSecond;
 
@@ -90,4 +92,31 @@ void trackRtcSummary(RtcStatsSummary summary) {
       'totalInboundVideoBytes': summary.totalInboundVideoBytes ?? '',
     },
   );
+}
+
+void trackInboundStats(
+  String? clientId,
+  List<RtcVideoInboundStats> stats,
+) {
+  final statsLists = RtcVideoInboundStatsLists.fromStatsList(stats);
+  //  formats each double value to 2 precision
+  const precision = 2;
+  final jitterBufferDelay =
+      formatDoubleList(statsLists.jitterBufferDelay, precision);
+  final decodeTime = formatDoubleList(statsLists.decodeTime, precision);
+
+  AppAnalytics.instance.trackTrace('video_inbound_stats', properties: {
+    'clientId': clientId ?? '',
+    'framesPerSecond': statsLists.framesPerSecond.join(','),
+    'framesReceivedPerSecond': statsLists.framesReceivedPerSecond.join(','),
+    'framesDecodedPerSecond': statsLists.framesDecodedPerSecond.join(','),
+    'framesDroppedPerSecond': statsLists.framesDroppedPerSecond.join(','),
+    'bytesPerSecond': statsLists.bytesPerSecond.join(','),
+    'packetsLost': statsLists.packetsLost.join(','),
+    'packetsReceived': statsLists.packetsReceived.join(','),
+    'jitter': statsLists.jitter.join(','),
+    'pauseCount': statsLists.pauseCount.join(','),
+    'jitterBufferDelay': jitterBufferDelay.join(','),
+    'decodeTime': decodeTime.join(','),
+  });
 }
