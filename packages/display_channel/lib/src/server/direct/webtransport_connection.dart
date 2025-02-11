ef12fb3fc6672a28f7cdfd3d5872ce2889ad9855
Vladka Huang <huangvl@VI614697-11.local>
2025-02-11 14:33:08 +0800

    feat: [USER STORY #79246] implement webtransport server and connection

---
+++ packages/display_channel/lib/src/server/direct/webtransport_connection.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'dart:convert';
+import 'package:display_channel/src/server/connection.dart';
+import 'package:display_channel/src/server/idle_connection_timer.dart';
+import 'package:flutter_golang_server/flutter_webtransport.dart';
+
+class WebTransportConnection implements Connection {
+  @override
+  void Function(Connection connection)? onClosed;
+
+  @override
+  void Function(Connection connection, Map<String, dynamic> message)? onMessage;
+
+  late String _connId;
+
+  @override
+  late Map<String, String> queryParameters;
+
+  late FlutterWebtransport _webTransport;
+  late IdleConnectionTimer _idleTimer;
+
+  WebTransportConnection(
+    FlutterWebtransport webTransport, {
+    required String connId,
+    required Duration idleConnectionTimeout,
+    required Map<String, String> queryParam,
+  }) {
+    _webTransport = webTransport;
+    queryParameters = queryParam;
+    _connId = connId;
+
+    _idleTimer = IdleConnectionTimer(
+      _onIdleTimeout,
+      idleConnectionTimeout,
+    );
+  }
+
+  @override
+  void send(Map<String, dynamic> message) {
+    final data = jsonEncode(message);
+    _webTransport.sendMessage(_connId, data);
+  }
+
+  @override
+  void close() {
+    _webTransport.closeWebTransportConn(_connId);
+  }
+
+  _onIdleTimeout() {
+    _idleTimer.stop();
+    onClosed?.call(this);
+  }
+}
---
+++ packages/display_channel/lib/src/webtransport_direct_server.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:display_channel/src/channel_store.dart';
+import 'package:display_channel/src/rate_limit/rate_limiter.dart';
+import 'package:display_channel/src/server/connection.dart';
+import 'package:display_channel/src/server/connection_request.dart';
+import 'package:display_channel/src/server/direct/webtransport_connection_server.dart';
+import 'package:flutter_golang_server/flutter_webtransport.dart';
+import 'package:flutter_golang_server/flutter_webtransport_config.dart';
+import 'package:flutter_golang_server/flutter_webtransport_listener.dart';
+
+class WebTransportDirectServer implements FlutterWebtransportListener {
+  late ChannelStore _store;
+  WebTransportConnectionServer? _connectionServer;
+  HttpServer? _httpServer;
+  final _webTransportServer = FlutterWebtransport();
+  late RateLimiter _rateLimiter;
+
+  late Duration _idleConnectionTimeout;
+
+  int? get port {
+    return _httpServer?.port;
+  }
+
+  WebTransportDirectServer(
+    OnNewChannel onNewChannel,
+    VerifyConnectRequest verifyConnectRequest, {
+    int maxBurstyRequests = 5,
+    double requestsPerSecond = 5,
+    Duration heartbeatInterval = const Duration(seconds: 10),
+    Duration heartbeatTimeout = const Duration(seconds: 10),
+    Duration reconnectTimeout = const Duration(seconds: 2),
+  }) {
+    _store = ChannelStore(
+      onNewChannel,
+      verifyConnectRequest,
+      heartbeatInterval: heartbeatInterval,
+      heartbeatTimeout: heartbeatTimeout,
+      reconnectTimeout: reconnectTimeout,
+    );
+    _rateLimiter = RateLimiter(
+      maxBurstyRequests,
+      requestsPerSecond,
+    );
+    _idleConnectionTimeout = heartbeatInterval + heartbeatTimeout;
+    _webTransportServer.registerListener(this);
+  }
+
+  Future<void> start(
+    int port, {
+    required List<String> certPem,
+    required List<String> keyPem,
+  }) async {
+    // direct server
+    _connectionServer = WebTransportConnectionServer(
+      _webTransportServer,
+      (String clientId, Connection connection) {
+        _store.handleNewConnection(clientId, connection);
+      },
+      (ConnectionRequest connectionRequest) {
+        //TODO: remove?
+        // Apply rate limiting to connection requests.
+        if (connectionRequest.clientIpAddress != null) {
+          if (!_rateLimiter.allowRequest(connectionRequest.clientIpAddress!)) {
+            return ConnectRequestStatus.rateLimitExceeded;
+          }
+        }
+
+        return _store.verifyConnectionRequest(connectionRequest);
+      },
+      idleConnectionTimeout: _idleConnectionTimeout,
+    );
+
+    final config = FlutterWebtransportConfig(
+        port: port, cert: certPem, key: keyPem, allowOrigins: []);
+
+    await _webTransportServer.startWebtransportServer(config);
+
+    // rate limiter
+    _rateLimiter.start();
+  }
+
+  void stop() {
+    _rateLimiter.stop();
+
+    _webTransportServer.stopServer();
+  }
+
+  @override
+  void onConnect(String connId, String queryStr) {
+    _connectionServer?.onConnect(connId, queryStr);
+  }
+
+  @override
+  void onClose(String connId) {
+    // TODO: implement onClose
+  }
+
+  @override
+  void onMessage(String connId, String message) {
+    // TODO: implement onMessage
+  }
+}
