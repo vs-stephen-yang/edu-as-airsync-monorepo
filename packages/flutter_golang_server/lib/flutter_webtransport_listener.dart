16241bc2f20ae1fcd0439b6c5b6214f5cc3f13a1
Vladka Huang <huangvl@VI614697-11.local>
2025-02-12 15:22:11 +0800

    feat: [USER STORY #79246] add cron job to notify listener check certificate

--- packages/flutter_golang_server/lib/flutter_webtransport_listener.dart
+++ packages/flutter_golang_server/lib/flutter_webtransport_listener.dart
@@ -2,4 +2,5 @@ abstract class FlutterWebtransportListener {
   void onMessage(String connId, String message);
   void onClose(String connId);
   void onConnect(String connId, String queryStr, String clientIp);
+  void onRequestCertificate();
 }
