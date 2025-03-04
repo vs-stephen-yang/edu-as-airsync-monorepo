1e24114d2a02272167e733c89ce7412a36b5e153
Stephen Yang <stephen.tw.yang@viewsonic.com>
2025-03-04 15:21:55 +0800

    feat(android): #59648 implement input injection via AccessibilityService

---
+++ packages/flutter_input_injection/android/src/main/java/com/viewsonic/flutter_input_injection/Constants.java
@@ -0,0 +1,9 @@
+package com.viewsonic.flutter_input_injection;
+
+public final class Constants {
+  public static final String ACTION_GESTURE = "com.viewsonic.airsync.ACTION_GESTURE";
+
+  // Private constructor prevents instantiation.
+  private Constants() {
+  }
+}
