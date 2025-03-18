8322922d0e103146661cacbc460e6ff36d9b5096
Stephen Yang <stephen.tw.yang@viewsonic.com>
2025-03-18 18:01:13 +0800

    feat: resend cached events on connectivity restoration
    When the network becomes available, automatically flush pending events,
    ensuring reliable delivery in offline scenarios.

--- apps/sender/lib/utilities/app_analytics.dart
+++ apps/sender/lib/utilities/app_analytics.dart
@@ -1,5 +1,6 @@
 import 'package:azure_application_insights/azure_application_insights.dart';
 import 'package:display_cast_flutter/utilities/caching_http_client.dart';
+import 'package:display_cast_flutter/utilities/offline_http_client.dart';
 import 'package:display_cast_flutter/utilities/client_device_info.dart';
 import 'package:display_cast_flutter/utilities/log.dart';
 import 'package:http/http.dart';
@@ -39,14 +40,16 @@ class AppAnalytics {
   }) {
     const timeout = Duration(seconds: 10);
 
+    final cachingClient = CachingHttpClient(
+      innerClient: Client(),
+      requestTimeout: timeout,
+    );
+
     final processor = BufferedProcessor(
       next: TransmissionProcessor(
         instrumentationKey: instrumentationKey,
         ingestionEndpoint: ingestionEndpoint,
-        httpClient: CachingHttpClient(
-          innerClient: Client(),
-          requestTimeout: timeout,
-        ),
+        httpClient: OfflineHttpClient(cachingClient),
         timeout: timeout,
       ),
     );
