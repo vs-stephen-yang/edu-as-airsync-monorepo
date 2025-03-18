2f41e8a59f44d3a93342a4aceb450aa5d637c35d
Vladka Huang <huangvl@VI614697-11.local>
2025-03-18 10:34:09 +0800

    feat: [USER STORY #81547] add date field to WebTransportCertificate

--- packages/display_channel/lib/src/webtransport_certificate.dart
+++ packages/display_channel/lib/src/webtransport_certificate.dart
@@ -1,6 +1,7 @@
 class WebTransportCertificate {
+  late String date;
   late List<String> certPem;
   late List<String> keyPem;
 
-  WebTransportCertificate(this.certPem, this.keyPem);
+  WebTransportCertificate(this.date, this.certPem, this.keyPem);
 }
