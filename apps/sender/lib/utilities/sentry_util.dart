c654edface1814772da488414d968c213904164c
Loren Chang <loren.chang@tw.viewsonic.com>
2025-03-18 16:21:54 +0800

    feat(sentry): #82044 Disable exception recording for failed HTTP requests to reduce Sentry events

--- apps/sender/lib/utilities/sentry_util.dart
+++ apps/sender/lib/utilities/sentry_util.dart
@@ -15,6 +15,9 @@ initSentry(SentryConfig config) async {
       options.addIntegration(LoggingIntegration());
 
       options.tracesSampleRate = config.tracesSampleRate;
+
+      // Disable recording of exceptions for failed HTTP requests.
+      options.captureFailedRequests = false;
     },
   );
 }
