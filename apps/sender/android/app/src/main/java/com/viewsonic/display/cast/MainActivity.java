fe0d38eba8548762e058923b92a099caa21d9c05
Ken Chung <ken.mh.chung@tw.viewsonic.com>
2025-03-19 16:18:43 +0800

    fix: [BUG #81325] [Sender][Android] 註解工具開啟後縮小時將 Airsync 從「最近應用程式」滑掉移除，註解工具還會存在

--- apps/sender/android/app/src/main/java/com/viewsonic/display/cast/MainActivity.java
+++ apps/sender/android/app/src/main/java/com/viewsonic/display/cast/MainActivity.java
@@ -1,13 +1,20 @@
 package com.viewsonic.display.cast;
 
+import android.content.Intent;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
+import qiuxiang.android_window.WindowService;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity implements DefaultLifecycleObserver{
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
@@ -32,5 +39,21 @@ public class MainActivity extends FlutterActivity {
                             }
                         }
                 );
+        getLifecycle().addObserver(this);
+    }
+
+    @Override
+    public void onDestroy(@NonNull LifecycleOwner owner) {
+        boolean windowServiceRunning = FlutterEngineCache.getInstance().get("flutter-android-window") != null;
+        if (windowServiceRunning) {
+            MainActivity.this.stopService(new Intent(MainActivity.this, WindowService.class));
+        }
+        DefaultLifecycleObserver.super.onDestroy(owner);
+    }
+
+    @Override
+    protected void onDestroy() {
+        getLifecycle().removeObserver(this);
+        super.onDestroy();
     }
 }
