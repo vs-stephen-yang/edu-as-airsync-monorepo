2cc44cd5941631565726af3d6985eefdd893688a
Vladka Huang <huangvl@VI614697-11.local>
2025-03-25 14:59:12 +0800

    test: [USER STORY #81549] add unit test for outbound rtp parse

---
+++ apps/sender/test/rtc_stats_parse_test.dart
@@ -0,0 +1,58 @@
+import 'package:display_cast_flutter/model/rtc_stats.dart';
+import 'package:display_cast_flutter/model/rtc_stats_parser.dart';
+import 'package:display_cast_flutter/model/rtc_stats_reporter.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+import 'rtc_stats_parse_test.mocks.dart';
+@GenerateMocks([RtcStatsReporter])
+
+void main() {
+  group('RtcStatsParser - _onStatsReports', () {
+    late RtcStatsParser parser;
+    late MockRtcStatsReporter mockReporter;
+
+    setUp(() {
+      mockReporter = MockRtcStatsReporter();
+      parser = RtcStatsParser((int? x, int? y){});
+      parser.addSubscriber(mockReporter);
+    });
+
+    test('Should process video outbound-rtp report', () {
+      // Arrange
+      final videoReport = StatsReport('OR01', 'outbound-rtp', 0, {
+        'kind': 'video',
+        'encoderImplementation': 'vp8',
+        'frameWidth': 1280,
+        'frameHeight': 720,
+        'framesPerSecond': 30.0,
+        'contentType': 'realtime',
+        'qualityLimitationReason': 'bandwidth',
+        'totalEncodeTime': 25.5,
+        'pliCount': 3,
+        'targetBitrate': 1200000.0,
+        'powerEfficientEncoder': true,
+      });
+
+      final reports = [videoReport];
+
+      // Act
+      parser.onVideoStatsReports(reports);
+
+      // Assert
+      verify(mockReporter.onVideoStatsReports(argThat(isA<RtcVideoOutboundStats>()
+          .having((s) => s.encoderImplementation, 'encoderImplementation', 'vp8')
+          .having((s) => s.frameWidth, 'frameWidth', 1280)
+          .having((s) => s.frameHeight, 'frameHeight', 720)
+          .having((s) => s.framesPerSecond, 'framesPerSecond', 30.0)
+          .having((s) => s.contentType, 'contentType', 'realtime')
+          .having((s) => s.qualityLimitationReason, 'qualityLimitationReason', 'bandwidth')
+          .having((s) => s.pliCount, 'pliCount', 3)
+          .having((s) => s.targetBitrate, 'targetBitrate', 1200000.0)
+          .having((s) => s.powerEfficientEncoder, 'powerEfficientEncoder', true))))
+          .called(1);
+    });
+  });
+}
