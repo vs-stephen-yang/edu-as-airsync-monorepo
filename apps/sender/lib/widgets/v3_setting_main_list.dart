d14a39f767ede18f8ca0d9eb3c222e08412af961
Ken Chung <ken.mh.chung@tw.viewsonic.com>
2025-05-12 17:00:37 +0800

    feat: [USER STORY #83202] [Sender] [A11y] Stage 2 - 1.4.4 Resize text(p3)

--- apps/sender/lib/widgets/v3_setting_main_list.dart
+++ apps/sender/lib/widgets/v3_setting_main_list.dart
@@ -27,96 +27,95 @@ class V3SettingMainList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
+    return Column(
       children: [
-        Consumer<SettingsProvider>(builder: (_, settingsProvider, __) {
-          List<SettingsItems> settings =
-              _addSettingsToList(context, settingsProvider);
-          return ListView.separated(
-            itemCount: settings.length,
-            itemBuilder: (BuildContext context, int index) {
-              return SizedBox(
-                width: 204,
-                child: V3Focus(
-                  label: sprintf(S.current.v3_lbl_setting_select,
-                      [settings[index].itemTitle]),
-                  identifier: 'v3_qa_setting_select_$index',
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(0, 44),
-                      shadowColor: Colors.transparent,
-                      foregroundColor:
-                          context.tokens.color.vsdswColorOnSurfaceInverse,
-                      backgroundColor: !isAppMode &&
-                              SettingsProvider.currentTittlePage.index == index
-                          ? context.tokens.color.vsdswColorPrimary
-                          : context.tokens.color.vsdswColorSurface1000,
-                      textStyle: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
+        Expanded(
+          child: Consumer<SettingsProvider>(builder: (_, settingsProvider, __) {
+            List<SettingsItems> settings =
+                _addSettingsToList(context, settingsProvider);
+            return ListView.separated(
+              itemCount: settings.length,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  width: 204,
+                  child: V3Focus(
+                    label: sprintf(S.current.v3_lbl_setting_select,
+                        [settings[index].itemTitle]),
+                    identifier: 'v3_qa_setting_select_$index',
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(0, 44),
+                        shadowColor: Colors.transparent,
+                        foregroundColor:
+                            context.tokens.color.vsdswColorOnSurfaceInverse,
+                        backgroundColor: !isAppMode &&
+                                SettingsProvider.currentTittlePage.index ==
+                                    index
+                            ? context.tokens.color.vsdswColorPrimary
+                            : context.tokens.color.vsdswColorSurface1000,
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: context.tokens.spacing.vsdswSpacingXs.top,
+                          horizontal:
+                              context.tokens.spacing.vsdswSpacingSm.left,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
                       ),
-                      padding: EdgeInsets.symmetric(
-                        vertical: context.tokens.spacing.vsdswSpacingXs.top,
-                        horizontal: context.tokens.spacing.vsdswSpacingSm.left,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    onPressed: () {
-                      settings[index].callback?.call();
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: AutoSizeText(
-                            settings[index].itemTitle,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
+                      onPressed: () {
+                        settings[index].callback?.call();
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: AutoSizeText(
+                              settings[index].itemTitle,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
                             ),
                           ),
-                        ),
-                        if (settings[index].itemIcon != null)
-                          SizedBox(
-                            width: 24,
-                            height: 24,
-                            child: settings[index].itemIcon!,
-                          ),
-                      ],
+                          if (settings[index].itemIcon != null)
+                            SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: settings[index].itemIcon!,
+                            ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return Divider(
-                thickness: 1,
-                height: context.tokens.spacing.vsdswSpacingSm.vertical,
-                color: context.tokens.color.vsdswColorOutlineVariant,
-              );
-            },
-          );
-        }),
-        Positioned(
-          bottom: isAppMode ? 40 : 50,
-          left: isAppMode ? null : 0,
-          child: AutoSizeText(
-            VersionUtil.isOpenVersion
-                ? S.of(context).v3_setting_app_version_independent(
-                    DateTime.now().year, AppConfig.of(context)?.appVersion)
-                : S.of(context).v3_setting_app_version(
-                    DateTime.now().year, AppConfig.of(context)?.appVersion),
-            minFontSize: 9,
-            style: TextStyle(
-              fontSize: isAppMode ? 14 : 10,
-              fontWeight: FontWeight.w400,
-              color: context.tokens.color.vsdswColorOnSurfaceVariant,
-            ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider(
+                  thickness: 1,
+                  height: context.tokens.spacing.vsdswSpacingSm.vertical,
+                  color: context.tokens.color.vsdswColorOutlineVariant,
+                );
+              },
+            );
+          }),
+        ),
+        AutoSizeText(
+          VersionUtil.isOpenVersion
+              ? S.of(context).v3_setting_app_version_independent(
+                  DateTime.now().year, AppConfig.of(context)?.appVersion)
+              : S.of(context).v3_setting_app_version(
+                  DateTime.now().year, AppConfig.of(context)?.appVersion),
+          minFontSize: 9,
+          style: TextStyle(
+            fontSize: isAppMode ? 14 : 10,
+            fontWeight: FontWeight.w400,
+            color: context.tokens.color.vsdswColorOnSurfaceVariant,
           ),
         ),
       ],
