a6d689abfc5c345f7f42af70e93d939a90f880e8
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2025-06-19 13:13:33 +0800

    fix: [BUG #86949][iOS & Android Sender] Size=2，主持人模式下連線等候投屏的Disconnect鍵超框

--- apps/sender/lib/widgets/v3_present_wait_prompt.dart
+++ apps/sender/lib/widgets/v3_present_wait_prompt.dart
@@ -6,6 +6,7 @@ import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/channel_provider.dart';
 import 'package:display_cast_flutter/utilities/app_analytics.dart';
 import 'package:display_cast_flutter/widgets/v3_custom_white_button.dart';
+import 'package:display_cast_flutter/widgets/v3_scroll_bar.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -18,7 +19,7 @@ class V3PresentWaitPrompt extends StatelessWidget {
   Widget build(BuildContext context) {
     ChannelProvider channelProvider = Provider.of<ChannelProvider>(context);
     bool isMobile = !kIsWeb && (Platform.isAndroid || Platform.isIOS);
-
+    final sc = ScrollController();
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -32,61 +33,71 @@ class V3PresentWaitPrompt extends StatelessWidget {
               context.tokens.shadow.vsdswShadowNeutralLg[0],
             ],
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (!isMobile)
-                WaitingText(isModerator: channelProvider.moderatorStatus),
-              SizedBox(
-                width: 115,
-                height: 115,
-                child:
-                    Lottie.asset('assets/lottie_files/vsdsl-spinner-sty1.json'),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 40)),
-              if (isMobile)
-                WaitingText(isModerator: channelProvider.moderatorStatus),
-              if (!channelProvider.moderatorStatus)
-                Container(
-                  width: isMobile ? 300 : 240,
-                  height: 48,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: context.tokens.color.vsdswColorSurface100,
-                      border: Border.all(
-                        color: context.tokens.color.vsdswColorSecondary,
-                        width: 1,
-                      ),
-                      borderRadius: context.tokens.radii.vsdswRadiusFull,
-                      boxShadow: [
-                        BoxShadow(
-                          offset: const Offset(0.0, 8.0),
-                          blurRadius: 16.0,
-                          spreadRadius: 0.0,
-                          color: context.tokens.color.vsdswColorSecondary
-                              .withOpacity(0.2),
-                        ),
-                      ]),
-                  child: AutoSizeText(
-                    channelProvider.randomName,
-                    style: TextStyle(
-                      color: context.tokens.color.vsdswColorOnSurface,
-                      fontSize: 16,
+          child: Center(
+            child: V3Scrollbar(
+              controller: sc,
+              mainAxisMargin: 10,
+              child: SingleChildScrollView(
+                controller: sc,
+                child: IntrinsicHeight(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (!isMobile)
+                      WaitingText(isModerator: channelProvider.moderatorStatus),
+                    SizedBox(
+                      width: 115,
+                      height: 115,
+                      child: Lottie.asset(
+                          'assets/lottie_files/vsdsl-spinner-sty1.json'),
                     ),
-                  ),
-                ),
-              if (channelProvider.moderatorStatus)
-                V3CustomWhiteButton(
-                  label: S.of(context).v3_lbl_moderator_disconnect,
-                  identifier: 'v3_qa_moderator_disconnect',
-                  buttonSize: Size(isMobile ? 300 : 240, 48),
-                  text: S.of(context).v3_main_moderator_disconnect,
-                  onPressed: () {
-                    trackEvent('click_disconnect', EventCategory.session);
-                    channelProvider.presentEnd();
-                  },
-                ),
-            ],
+                    const Padding(padding: EdgeInsets.only(top: 40)),
+                    if (isMobile)
+                      WaitingText(isModerator: channelProvider.moderatorStatus),
+                    if (!channelProvider.moderatorStatus)
+                      Container(
+                        width: isMobile ? 300 : 240,
+                        height: 48,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: context.tokens.color.vsdswColorSurface100,
+                            border: Border.all(
+                              color: context.tokens.color.vsdswColorSecondary,
+                              width: 1,
+                            ),
+                            borderRadius: context.tokens.radii.vsdswRadiusFull,
+                            boxShadow: [
+                              BoxShadow(
+                                offset: const Offset(0.0, 8.0),
+                                blurRadius: 16.0,
+                                spreadRadius: 0.0,
+                                color: context.tokens.color.vsdswColorSecondary
+                                    .withOpacity(0.2),
+                              ),
+                            ]),
+                        child: AutoSizeText(
+                          channelProvider.randomName,
+                          style: TextStyle(
+                            color: context.tokens.color.vsdswColorOnSurface,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    if (channelProvider.moderatorStatus)
+                      V3CustomWhiteButton(
+                        label: S.of(context).v3_lbl_moderator_disconnect,
+                        identifier: 'v3_qa_moderator_disconnect',
+                        buttonSize: Size(isMobile ? 300 : 240, 48),
+                        text: S.of(context).v3_main_moderator_disconnect,
+                        onPressed: () {
+                          trackEvent('click_disconnect', EventCategory.session);
+                          channelProvider.presentEnd();
+                        },
+                      ),
+                  ],
+                )),
+              ),
+            ),
           ),
         ),
       ],
@@ -105,7 +116,7 @@ class WaitingText extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
         children: [
-          AutoSizeText(
+          Text(
             isModerator
                 ? S.of(context).v3_main_moderator_wait
                 : S.of(context).v3_main_authorize_wait,
