56e60272e9e125fd6a0dca506af0e528a9970110
Vladka Huang <huangvl@VI614697-11.local>
2025-06-23 17:08:10 +0800

    feat: [USER STORY #88397] Implement frame timing control from MediaProjection to OpenGL to MediaCodec encoder for consistent output FPS

---
+++ packages/flutter_multicast_plugin/android/src/main/java/com/viewsonic/flutter_multicast_plugin/NativeBridge.java
@@ -0,0 +1,13 @@
+package com.viewsonic.flutter_multicast_plugin;
+
+public class NativeBridge {
+    static {
+        System.loadLibrary("uvgrtp_android");
+    }
+
+    public static native boolean startRtpStream(String ip, int port, byte[] key, byte[] salt, int ssrc);
+    public static native void sendRtpFrame(byte[] data);
+    public static native void stopRtpStream();
+    public static native void receiveStart(Object surface, long roc);
+    public static native void receiveStop();
+}
