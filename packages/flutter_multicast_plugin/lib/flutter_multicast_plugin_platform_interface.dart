a01810ffd4713583112b7cef245d10d0d0db90a1
Vladka Huang <huangvl@VI614697-11.local>
2025-06-23 17:06:46 +0800

    feat: [USER STORY #88397] add method channel

--- packages/flutter_multicast_plugin/lib/flutter_multicast_plugin_platform_interface.dart
+++ packages/flutter_multicast_plugin/lib/flutter_multicast_plugin_platform_interface.dart
@@ -3,27 +3,46 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'flutter_multicast_plugin_method_channel.dart';
 
 abstract class FlutterMulticastPluginPlatform extends PlatformInterface {
-  /// Constructs a FlutterMulticastPluginPlatform.
   FlutterMulticastPluginPlatform() : super(token: _token);
 
   static final Object _token = Object();
 
   static FlutterMulticastPluginPlatform _instance = MethodChannelFlutterMulticastPlugin();
 
-  /// The default instance of [FlutterMulticastPluginPlatform] to use.
-  ///
-  /// Defaults to [MethodChannelFlutterMulticastPlugin].
   static FlutterMulticastPluginPlatform get instance => _instance;
 
-  /// Platform-specific implementations should set this with their own
-  /// platform-specific class that extends [FlutterMulticastPluginPlatform] when
-  /// they register themselves.
   static set instance(FlutterMulticastPluginPlatform instance) {
     PlatformInterface.verifyToken(instance, _token);
     _instance = instance;
   }
 
-  Future<String?> getPlatformVersion() {
-    throw UnimplementedError('platformVersion() has not been implemented.');
+  Future<bool> startRtpStream({
+    required String ip,
+    required int port,
+    required int ssrc,
+    required List<int> key,
+    required List<int> salt,
+  }) {
+    throw UnimplementedError('startRtpStream() has not been implemented.');
+  }
+
+  Future<void> stopRtpStream() {
+    throw UnimplementedError('stopRtpStream() has not been implemented.');
+  }
+
+  Future<void> startCapture() {
+    throw UnimplementedError('startCapture() has not been implemented.');
+  }
+
+  Future<void> stopCapture() {
+    throw UnimplementedError('stopCapture() has not been implemented.');
+  }
+
+  Future<int> receiveStart({required int roc}) {
+    throw UnimplementedError('receiveStart() has not been implemented.');
+  }
+
+  Future<void> receiveStop() {
+    throw UnimplementedError('receiveStop() has not been implemented.');
   }
 }
