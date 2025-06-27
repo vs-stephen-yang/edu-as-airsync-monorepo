af6881fe3ec711aa3866e5f8030452baf9aa5b5b
Vladka Huang <huangvl@VI614697-11.local>
2025-06-27 10:50:48 +0800

    feat: [USER STORY #88397] add android get all interface local IP

---
+++ packages/flutter_multicast_plugin/android/src/main/java/com/viewsonic/flutter_multicast_plugin/NetworkUtils.java
@@ -0,0 +1,34 @@
+package com.viewsonic.flutter_multicast_plugin;
+
+import java.net.NetworkInterface;
+import java.net.InetAddress;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkUtils {
+
+    public static List<String> getAllLocalIPv4s() {
+        List<String> ipList = new ArrayList<>();
+
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+            for (NetworkInterface iface : Collections.list(interfaces)) {
+                if (!iface.isUp() || iface.isLoopback()) continue;
+
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                for (InetAddress addr : Collections.list(addresses)) {
+                    if (!addr.isLoopbackAddress() && addr instanceof java.net.Inet4Address) {
+                        ipList.add(addr.getHostAddress());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return ipList;
+    }
+}
