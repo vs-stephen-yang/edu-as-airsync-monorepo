4696fa10f3572ca971b5d0ee57ed1b9313989087
Ken Chung <ken.mh.chung@tw.viewsonic.com>
2025-07-02 15:15:20 +0800

    feat: [USER STORY #88587] [Receiver][A11y][Moderator Mode][Cast to Devices] Button 優化

--- apps/receiver/lib/widgets/v3_cast_device_item.dart
+++ apps/receiver/lib/widgets/v3_cast_device_item.dart
@@ -3,6 +3,7 @@ import 'package:display_flutter/app_preferences.dart';
 import 'package:display_flutter/assets/tokens/tokens.g.dart';
 import 'package:display_flutter/generated/l10n.dart';
 import 'package:display_flutter/model/remote_screen_connector.dart';
+import 'package:display_flutter/model/text_scale_option.dart';
 import 'package:display_flutter/providers/channel_provider.dart';
 import 'package:display_flutter/widgets/v3_auto_hyphenating_text.dart';
 import 'package:display_flutter/widgets/v3_focus.dart';
@@ -11,22 +12,18 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 
-class V3CastDeviceItem extends StatefulWidget {
+class V3CastDeviceItem extends _TextSizeAwareStateful {
   const V3CastDeviceItem({super.key, required this.index});
 
   final int index;
 
   @override
-  State<StatefulWidget> createState() => _V3CastDeviceItemState();
-}
-
-class _V3CastDeviceItemState extends State<V3CastDeviceItem> {
-  @override
-  Widget build(BuildContext context) {
+  Widget buildWithTextSize(
+      BuildContext context, void Function() refreshWidget) {
     ChannelProvider channelProvider =
         Provider.of<ChannelProvider>(context, listen: false);
     RemoteScreenConnector remoteScreenConnector =
-        channelProvider.remoteScreenConnectors[widget.index];
+        channelProvider.remoteScreenConnectors[index];
 
     String status = S.of(context).v3_cast_to_device_Receiving;
     if (remoteScreenConnector.isTouchEnabled) {
@@ -81,56 +78,82 @@ class _V3CastDeviceItemState extends State<V3CastDeviceItem> {
                       ? S.of(context).v3_lbl_cast_device_touchback_disable
                       : S.of(context).v3_lbl_cast_device_touchback_enable,
                   identifier: remoteScreenConnector.isTouchEnabled
-                      ? 'v3_qa_cast_device_touchback_disable_${widget.index}'
-                      : 'v3_qa_cast_device_touchback_enable_${widget.index}',
+                      ? 'v3_qa_cast_device_touchback_disable_$index'
+                      : 'v3_qa_cast_device_touchback_enable_$index',
                   child: SizedBox(
                     height: 27 * AppPreferences().textScale,
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        if (remoteScreenConnector.isTouchEnabled) {
-                          remoteScreenConnector.isTouchEnabled = false;
-                        } else {
-                          remoteScreenConnector.isTouchEnabled = true;
-                        }
-                        channelProvider.remoteScreenServe
-                            .enableRemoteControlBySessionId(
-                                remoteScreenConnector.sessionId!,
-                                remoteScreenConnector.isTouchEnabled);
-                        setState(() {});
-                      },
-                      icon: SvgPicture.asset(
-                        remoteScreenConnector.isTouchEnabled
-                            ? 'assets/images/ic_finger_disable.svg'
-                            : 'assets/images/ic_finger_touch.svg',
-                        excludeFromSemantics: true,
-                        width: 16,
-                        height: 16,
-                      ),
-                      label: V3AutoHyphenatingText(
-                        remoteScreenConnector.isTouchEnabled
-                            ? S.of(context).v3_cast_to_device_touch_back_disable
-                            : S.of(context).v3_cast_to_device_touch_back,
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
-                          color: remoteScreenConnector.isTouchEnabled
-                              ? context.tokens.color.vsdslColorError
-                              : context.tokens.color.vsdslColorOnSurface,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        elevation: 5,
-                        backgroundColor:
-                            context.tokens.color.vsdslColorOnSurfaceInverse,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: context.tokens.radii.vsdslRadiusFull,
-                        ),
-                        padding: EdgeInsets.symmetric(
-                            horizontal:
-                                context.tokens.spacing.vsdslSpacingSm.left),
-                        shadowColor: context.tokens.color.vsdslColorNeutral,
-                      ),
-                    ),
+                    child: showIcon
+                        ? InkWell(
+                            onTap: () {
+                              if (remoteScreenConnector.isTouchEnabled) {
+                                remoteScreenConnector.isTouchEnabled = false;
+                              } else {
+                                remoteScreenConnector.isTouchEnabled = true;
+                              }
+                              channelProvider.remoteScreenServe
+                                  .enableRemoteControlBySessionId(
+                                      remoteScreenConnector.sessionId!,
+                                      remoteScreenConnector.isTouchEnabled);
+                              refreshWidget();
+                            },
+                            child: SvgPicture.asset(
+                              remoteScreenConnector.isTouchEnabled
+                                  ? 'assets/images/ic_moderator_untouchback.svg'
+                                  : 'assets/images/ic_moderator_touchback.svg',
+                              width: 26,
+                              height: 26,
+                            ),
+                          )
+                        : ElevatedButton.icon(
+                            onPressed: () {
+                              if (remoteScreenConnector.isTouchEnabled) {
+                                remoteScreenConnector.isTouchEnabled = false;
+                              } else {
+                                remoteScreenConnector.isTouchEnabled = true;
+                              }
+                              channelProvider.remoteScreenServe
+                                  .enableRemoteControlBySessionId(
+                                      remoteScreenConnector.sessionId!,
+                                      remoteScreenConnector.isTouchEnabled);
+                              refreshWidget();
+                            },
+                            icon: SvgPicture.asset(
+                              remoteScreenConnector.isTouchEnabled
+                                  ? 'assets/images/ic_finger_disable.svg'
+                                  : 'assets/images/ic_finger_touch.svg',
+                              excludeFromSemantics: true,
+                              width: 16,
+                              height: 16,
+                            ),
+                            label: V3AutoHyphenatingText(
+                              remoteScreenConnector.isTouchEnabled
+                                  ? S
+                                      .of(context)
+                                      .v3_cast_to_device_touch_back_disable
+                                  : S.of(context).v3_cast_to_device_touch_back,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                                color: remoteScreenConnector.isTouchEnabled
+                                    ? context.tokens.color.vsdslColorError
+                                    : context.tokens.color.vsdslColorOnSurface,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              elevation: 5,
+                              backgroundColor: context
+                                  .tokens.color.vsdslColorOnSurfaceInverse,
+                              shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    context.tokens.radii.vsdslRadiusFull,
+                              ),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: context
+                                      .tokens.spacing.vsdslSpacingSm.left),
+                              shadowColor:
+                                  context.tokens.color.vsdslColorNeutral,
+                            ),
+                          ),
                   ),
                 ),
               ],
@@ -165,5 +188,44 @@ class _V3CastDeviceItemState extends State<V3CastDeviceItem> {
         ],
       ),
     );
+  }
+}
+
+abstract class _TextSizeAwareStateful extends StatefulWidget {
+  const _TextSizeAwareStateful({super.key});
+
+  ResizeTextSizeOption get textSize => AppPreferences().textSizeOption;
+
+  bool get showIcon =>
+      AppPreferences().textSizeOption != ResizeTextSizeOption.normal;
+
+  @override
+  State<_TextSizeAwareStateful> createState() => _TextSizeAwareStatefulState();
+
+  /// 子類需要實作這個方法
+  Widget buildWithTextSize(BuildContext context, void Function() refreshWidget);
+}
+
+class _TextSizeAwareStatefulState extends State<_TextSizeAwareStateful> {
+  late VoidCallback listener;
+
+  @override
+  void initState() {
+    super.initState();
+    listener = () {
+      setState(() {}); // 只要 textSizeOptionNotifier 改變就重新 build
+    };
+    AppPreferences().textSizeOptionNotifier.addListener(listener);
+  }
+
+  @override
+  void dispose() {
+    AppPreferences().textSizeOptionNotifier.removeListener(listener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.buildWithTextSize(context, () => setState(() {}));
   }
 }
