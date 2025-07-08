c1eaf65b0f8328e313dc35f970b24fbbca9571a5
Vladka Huang <huangvl@VI614697-11.local>
2025-07-08 17:22:37 +0800

    feat: [USER STORY #88400] add iOS audio receiver

--- packages/flutter_multicast_plugin/native_libs/common/gst_audio_pipeline.cpp
+++ packages/flutter_multicast_plugin/native_libs/common/gst_audio_pipeline.cpp
@@ -38,11 +38,16 @@ bool GstAudioPipeline::init() {
 #endif
 
 #ifdef __APPLE__
-    const char* desc = "";
+    const char* desc = "appsrc name=appsrc is-live=true format=time stream-type=stream caps=audio/x-opus,rate=48000,channels=1,channel-mapping-family=0 ! "
+                       "queue name=decode_queue max-size-buffers=20 leaky=2 ! "
+                       "opusdec name=opusdec ! "
+                       "queue name=audio_buffer max-size-buffers=10 max-size-time=200000000 leaky=1 ! "
+                       "audioconvert name=audioconvert ! "
+                       "osxaudiosink sync=false async=false";
 #else
     const char* desc =
         "appsrc name=appsrc is-live=true format=time stream-type=stream caps=audio/x-opus,rate=48000,channels=1,channel-mapping-family=0 ! "
-        "opusdec name=opusdec ! identity name=identity ! audioconvert name=audioconvert ! autoaudiosink";
+        "opusdec name=opusdec ! queue name=audio_buffer max-size-buffers=10 max-size-time=200000000 leaky=0 ! audioconvert name=audioconvert ! autoaudiosink";
 #endif
 
     gst_debug_set_default_threshold(GST_LEVEL_WARNING);
