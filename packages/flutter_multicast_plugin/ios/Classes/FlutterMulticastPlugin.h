dffffa99e905ec10d89d5dffb8359469de8e90e4
Vladka Huang <huangvl@VI614697-11.local>
2025-07-08 16:16:49 +0800

    feat: [USER STORY #88400] add ios video receiver

--- packages/flutter_multicast_plugin/ios/Classes/FlutterMulticastPlugin.h
+++ packages/flutter_multicast_plugin/ios/Classes/FlutterMulticastPlugin.h
@@ -1,4 +1,7 @@
 #import <Flutter/Flutter.h>
+#import <Foundation/Foundation.h>
 
-@interface FlutterMulticastPlugin : NSObject<FlutterPlugin>
+@interface FlutterMulticastPlugin : NSObject <FlutterPlugin>
+@property(nonatomic, weak) NSObject<FlutterPluginRegistrar> *registrar;
+
 @end
