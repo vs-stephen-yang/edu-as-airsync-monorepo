4c99e5ad4c9a4e07ef1fa56d6129b4cd8d9d8451
Vladka Huang <huangvl@VI614697-11.local>
2025-07-11 15:46:44 +0800

    feat: [USER STORY #88402] add gstreamer pipeline observer for error handling

--- packages/flutter_multicast_plugin/native_libs/common/gst_audio_pipeline.h
+++ packages/flutter_multicast_plugin/native_libs/common/gst_audio_pipeline.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "gst_pipeline_observer.h"
 #include <gst/app/gstappsrc.h>
 #include <gst/gst.h>
+#include <mutex>
 #include <vector>
 
-class GstAudioPipeline {
+class GstAudioPipeline : public GstPipelineObserver {
   public:
     GstAudioPipeline();
     ~GstAudioPipeline();
@@ -13,9 +15,12 @@ class GstAudioPipeline {
     void push_opus_frame(const std::vector<uint8_t>& opus_data);
     void stop();
 
+    void on_pipeline_error() override;
+
   private:
     GstElement* pipeline_ = nullptr;
     GstElement* appsrc_ = nullptr;
+    std::mutex pipeline_mutex_;
     bool first_audio_received_ = false;
     GstClockTime timestamp_ = 0;
     GstClockTime sync_to_pipeline_time_();
