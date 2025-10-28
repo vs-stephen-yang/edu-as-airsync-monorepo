3f6443e6595490ff1721c621c557a082a5338427
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2025-10-28 17:32:04 +0800

    feat: [USER STORY #96455] [Receiver][Launcher] Toasts - Streaming screen reconnect toast

--- apps/receiver/lib/utility/toast.dart
+++ apps/receiver/lib/utility/toast.dart
@@ -14,32 +14,33 @@ class Toast {
 
   static OverlayEntry makeSplitScreenReconnectToast(
       BuildContext context, String message, int index,
-      {bool isWebRTC = true}) {
+      {bool isWebRTC = true, bool hasNameLabel = false}) {
     Size screenSize = MediaQuery.of(context).size;
     double sectionWidth = screenSize.width / 2;
     double sectionHeight = screenSize.height / 2;
+    double toastPadding = hasNameLabel ? 35 : 5;
     double? top, left;
     if (Home.enlargedScreenPositionIndex.value == null) {
       if (index == 1) {
-        top = sectionHeight - 80;
+        top = 0 + toastPadding; // 改：第一行頂部
         left = sectionWidth + (sectionWidth / 2 - 80);
       } else if (index == 2) {
-        top = sectionHeight * 2 - 80;
+        top = sectionHeight + toastPadding; // 改：第二行頂部
         left = sectionWidth / 2 - 80;
       } else if (index == 3) {
-        top = sectionHeight * 2 - 80;
+        top = sectionHeight + toastPadding; // 改：第二行頂部
         left = sectionWidth + (sectionWidth / 2 - 80);
       } else {
         if (HybridConnectionList.hybridSplitScreenCount.value > 1) {
-          top = sectionHeight - 80;
+          top = 0 + toastPadding; // 改：第一行頂部
           left = sectionWidth / 2 - 80;
         } else {
-          top = screenSize.height - 80;
+          top = toastPadding; // 改：頂部
           left = screenSize.width / 2 - 80;
         }
       }
     } else {
-      top = screenSize.height - 80;
+      top = toastPadding; // 改：全屏模式放在頂部
       left = screenSize.width / 2 - 80;
     }
 
@@ -72,14 +73,17 @@ class Toast {
 
   static void showSplitScreenReconnectToast(
       BuildContext context, String message, int index,
-      {bool isWebRTC = true, ReconnectState state = ReconnectState.idle}) {
+      {bool isWebRTC = true,
+      ReconnectState state = ReconnectState.idle,
+      bool hasNameLabel = false}) {
     if (!isWebRTC) {
       if (!shouldSplitScreenToast(index, state)) {
         return;
       }
     }
 
-    OverlayEntry toast = makeSplitScreenReconnectToast(context, message, index);
+    OverlayEntry toast = makeSplitScreenReconnectToast(context, message, index,
+        hasNameLabel: hasNameLabel);
 
     if (!isWebRTC) {
       _splitScreenLastToastTimes[index][state] = DateTime.now();
