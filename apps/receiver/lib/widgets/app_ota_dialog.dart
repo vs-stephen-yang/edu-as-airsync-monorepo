b5c8ea3df35b6233d0260461fd90eb3efa499445
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2025-11-24 21:05:05 +0800

    fix: [BUG #98358]【IFP53】【AirSync】触发AirSync升级后，升级进度条加载时，会来回闪烁 - 複製自ViewBoardOS

--- apps/receiver/lib/widgets/app_ota_dialog.dart
+++ apps/receiver/lib/widgets/app_ota_dialog.dart
@@ -1,17 +1,6 @@
-import 'dart:io';
-
-import 'package:app_ota_flutter/app_ota_flutter.dart';
-import 'package:app_ota_flutter/model/ota_info.dart';
-import 'package:display_flutter/app_analytics.dart';
 import 'package:display_flutter/app_update_helper.dart';
-import 'package:display_flutter/assets/tokens/tokens.g.dart';
-import 'package:display_flutter/generated/l10n.dart';
-import 'package:display_flutter/widgets/v3_auto_hyphenating_text.dart';
+import 'package:display_flutter/utility/log.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
-import 'package:gap/gap.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 class AppOTADialog extends StatefulWidget {
@@ -23,19 +12,22 @@ class AppOTADialog extends StatefulWidget {
   State createState() => AppOTADialogState();
 }
 
-class AppOTADialogState extends State<AppOTADialog>
-    implements AppUpdateListener {
-  ValueNotifier<double> progress = ValueNotifier(0);
-  BuildContext? ctxDownloading; //variable for downloading dialog context
-
+class AppOTADialogState extends State<AppOTADialog> {
   late AppUpdateHelper _appUpdateHelper;
 
   @override
   void initState() {
+    log.info(
+        '[OTA Dialog] INIT: instance=$hashCode route=${ModalRoute.of(context)?.settings.name}');
     _appUpdateHelper = context.read<AppUpdateHelper>();
-
-    _appUpdateHelper.initializeChecking(listener: this);
+    _appUpdateHelper.initializeChecking();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    log.info('[OTA Dialog] DISPOSE: instance=$hashCode');
+    super.dispose();
   }
 
   @override
@@ -43,188 +35,6 @@ class AppOTADialogState extends State<AppOTADialog>
     return widget.child;
   }
 
-  @override
-  Future<void> onUpdateCheckFinished(UpdateStatus status, OtaInfo? info,
-      {double? progress}) async {
-    switch (status) {
-      case UpdateStatus.updateDownloading:
-        if (progress == -1) {
-          FlutterNativeSplash.remove();
-          // [USER STORY 90944]Silent software OTA，不顯示UI
-          // _showOTADialog(status, info);
-        } else if (progress != null) {
-          this.progress.value = progress;
-        }
-        // 標記本次為下載，不安裝。
-        _appUpdateHelper.newVersionDownloaded = true;
-        break;
-      case UpdateStatus.updateDownloaded:
-        if (_appUpdateHelper.otaFlavor == OtaFlavor.ifp ||
-            _appUpdateHelper.otaFlavor == OtaFlavor.edla) {
-          // 如果這次是下載，則略過安裝流程。如果是Alarm就安裝。
-          if (!_appUpdateHelper.newVersionDownloaded ||
-              _appUpdateHelper.appAlarmOTA) {
-            AppOtaFlutter().isNeedInAppUpdate = true;
-            await _installNow(info);
-            if (_appUpdateHelper.otaFlavor == OtaFlavor.edla) {
-              FlutterNativeSplash.remove();
-            }
-          }
-        } else {
-          _showOTADialog(status, info);
-        }
-        break;
-      case UpdateStatus.updateInApp:
-        _appUpdateHelper.startInAppUpdate();
-        break;
-      case UpdateStatus.updateToDate:
-      case UpdateStatus.unknown:
-        // Not available
-        break;
-    }
-  }
-
-  _showOTADialog(UpdateStatus status, OtaInfo? info) {
-    if (ctxDownloading != null) Navigator.pop(ctxDownloading!);
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        ctxDownloading = context;
-        return PopScope(
-          // Using canPop=false to block back key return,
-          // it will break "Show Prompt mechanism"
-          canPop: false,
-          child: AlertDialog(
-            title: Semantics(
-              identifier: 'v3_qa_ota_dialog_tittle',
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const SizedBox(
-                    width: 30,
-                    height: 30,
-                    child: Image(
-                      image: Svg('assets/images/ic_logo_airsync_icon.svg'),
-                    ),
-                  ),
-                  const Gap(20),
-                  // title will expand the dialog, no need to use -
-                  Text(
-                    S.of(context).update_title,
-                    style: TextStyle(
-                      color: context.tokens.color.vsdslColorOnSurface,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // message will expand the dialog, no need to use -
-                Text(
-                  S.of(context).update_message,
-                  style: TextStyle(
-                    color: context.tokens.color.vsdslColorInfo,
-                    fontSize: 14,
-                  ),
-                ),
-                const Gap(40),
-                if (status == UpdateStatus.updateDownloading)
-                  ValueListenableBuilder<double>(
-                    valueListenable: progress,
-                    builder:
-                        (BuildContext context, double value, Widget? child) {
-                      return LinearProgressIndicator(
-                        value: value,
-                        backgroundColor:
-                            context.tokens.color.vsdslColorSurface300,
-                        color: context.tokens.color.vsdslColorSecondary,
-                      );
-                    },
-                  ),
-                const Gap(10),
-                if (status == UpdateStatus.updateDownloading)
-                  ValueListenableBuilder<double>(
-                    valueListenable: progress,
-                    builder:
-                        (BuildContext context, double value, Widget? child) {
-                      return Align(
-                        alignment: Alignment.bottomRight,
-                        // Only for percentage, no need to use -
-                        child: Text(
-                          '${(value * 100).toInt()}%',
-                          style: TextStyle(
-                            color: context.tokens.color.vsdslColorInfo,
-                            fontSize: 14,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-              ],
-            ),
-            actions: [
-              if (status == UpdateStatus.updateDownloaded)
-                _updateDialogButton(
-                  text: S.of(context).update_install_now,
-                  textColor: Colors.white,
-                  backgroundColor: context.tokens.color.vsdslColorPrimary,
-                  onPressed: () {
-                    trackEvent('start_ota', EventCategory.system);
-                    _installNow(info);
-                  },
-                ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
-  _installNow(OtaInfo? info) async {
-    var folder = await getExternalStorageDirectory();
-    var otaFile = File("${folder?.path}/${info?.fileName}");
-    await AppUpdateHelper().startAppUpdate(otaFile.path);
-  }
-
-  Widget _updateDialogButton(
-      {required String text,
-      required Color textColor,
-      required Color backgroundColor,
-      required GestureTapCallback onPressed}) {
-    return InkWell(
-      onTap: onPressed,
-      child: Container(
-        alignment: Alignment.center,
-        width: 105,
-        height: 40,
-        clipBehavior: Clip.antiAlias,
-        decoration: ShapeDecoration(
-          color: backgroundColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(9999),
-          ),
-          shadows: backgroundColor != Colors.transparent
-              ? [
-                  BoxShadow(
-                    color: backgroundColor.withOpacity(0.31),
-                    blurRadius: 24,
-                    offset: const Offset(0, 16),
-                    spreadRadius: 0,
-                  )
-                ]
-              : null,
-        ),
-        child: V3AutoHyphenatingText(
-          text,
-          style: TextStyle(color: textColor),
-        ),
-      ),
-    );
-  }
+// [USER STORY 90944] Silent software OTA，所有 OTA 邏輯已移至 AppUpdateHelper
+// 如果未來需要顯示 OTA UI，可以在這裡添加相關方法
 }
