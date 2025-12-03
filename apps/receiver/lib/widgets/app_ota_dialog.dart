c532bfb0fedc12d5caad145ba7e4bc946f491976
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2025-12-03 19:55:15 +0800

    fix: [BUG #98358]【IFP53】【AirSync】触发AirSync升级后，升级进度条加载时，会来回闪烁 - 複製自ViewBoardOS - The following assertion was thrown building Builder: dependOnInheritedWidgetOfExactType<_ModalScopeStatus>() or  dependOnInheritedElement() was called before AppOTADialogState.initState()completed.

--- apps/receiver/lib/widgets/app_ota_dialog.dart
+++ apps/receiver/lib/widgets/app_ota_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:display_flutter/app_update_helper.dart';
 import 'package:display_flutter/utility/log.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 
 class AppOTADialog extends StatefulWidget {
@@ -13,14 +14,13 @@ class AppOTADialog extends StatefulWidget {
 }
 
 class AppOTADialogState extends State<AppOTADialog> {
-  late AppUpdateHelper _appUpdateHelper;
-
   @override
   void initState() {
-    log.info(
-        '[OTA Dialog] INIT: instance=$hashCode route=${ModalRoute.of(context)?.settings.name}');
-    _appUpdateHelper = context.read<AppUpdateHelper>();
-    _appUpdateHelper.initializeChecking();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      log.info(
+          '[OTA Dialog] INIT: instance=$hashCode route=${ModalRoute.of(context)?.settings.name}');
+      context.read<AppUpdateHelper>().initializeChecking();
+    });
     super.initState();
   }
 
