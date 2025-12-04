630f7929ba6a9369aaf136af0817da832837f40f
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2025-12-04 21:28:37 +0800

    fix: [BUG #98745] NoSuchMethodError: Null check operator used on a null value

--- apps/sender/lib/screens/v3_home.dart
+++ apps/sender/lib/screens/v3_home.dart
@@ -3,23 +3,21 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:auto_hyphenating_text/auto_hyphenating_text.dart';
-import 'package:display_cast_flutter/generated/l10n.dart';
 import 'package:display_cast_flutter/providers/channel_provider.dart';
 import 'package:display_cast_flutter/providers/present_state_provider.dart';
 import 'package:display_cast_flutter/screens/v3_home_app.dart';
 import 'package:display_cast_flutter/screens/v3_home_web.dart';
+import 'package:display_cast_flutter/screens/v3_mobile_unsupported.dart';
 import 'package:display_cast_flutter/utilities/audio_switch_manager.dart';
 import 'package:display_cast_flutter/utilities/v3_network_status_detector.dart';
 import 'package:display_cast_flutter/utilities/v3_update_manager.dart';
 import 'package:display_cast_flutter/widgets/app_retain.dart';
 import 'package:display_cast_flutter/widgets/v3_exit_dialog.dart';
-import 'package:display_cast_flutter/widgets/v3_scroll_bar.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_window_close/flutter_window_close.dart';
 import 'package:provider/provider.dart';
 import "package:universal_html/html.dart" as html;
-import 'package:url_launcher/url_launcher.dart';
 
 class V3Home extends StatefulWidget {
   const V3Home({super.key});
@@ -79,12 +77,12 @@ class _V3HomeState extends State<V3Home> {
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      if (!_isWebSupportPlatform()) {
-        _showNotSupportDialog();
-        return;
-      }
+    // Check if this is a mobile device on web
+    if (!_isWebSupportPlatform()) {
+      return const V3MobileUnsupported();
+    }
 
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       if (!kIsWeb && V3NetworkStatusDetector().isConnected()) {
         V3UpdateManager().checkUpdateVersion(context, (value) {
           if (value != CompareVersionResult.noUpdate) {
@@ -168,59 +166,5 @@ class _V3HomeState extends State<V3Home> {
     } else {
       return true;
     }
-  }
-
-  void _showNotSupportDialog() {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        final sc = ScrollController();
-        return AlertDialog(
-          backgroundColor: Colors.white,
-          // Can not use V3AutoHyphenatingText
-          title: Text(S.of(context).main_notice_title),
-          content: SizedBox(
-            width: 100,
-            height: 100,
-            child: V3Scrollbar(
-              controller: sc,
-              child: SingleChildScrollView(
-                controller: sc,
-                // Can not use V3AutoHyphenatingText
-                child: Text(S.of(context).main_notice_not_support_description),
-              ),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-                // 设置按钮背景颜色
-                foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                // 设置按钮文字颜色
-                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10), // 设置按钮圆角
-                    side: const BorderSide(color: Colors.blue), // 设置按钮边框
-                  ),
-                ),
-              ),
-              onPressed: () async {
-                if (defaultTargetPlatform == TargetPlatform.android) {
-                  unawaited(launchUrl(Uri.parse(
-                      'https://play.google.com/store/apps/details?id=com.viewsonic.display.cast&pcampaignid=web_share')));
-                } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-                  unawaited(launchUrl(Uri.parse(
-                      'https://apps.apple.com/tw/app/airsync-sender/id6453759985')));
-                }
-              },
-              // Can not use V3AutoHyphenatingText
-              child: Text(S.of(context).main_notice_positive_button),
-            ),
-          ],
-        );
-      },
-    );
   }
 }
