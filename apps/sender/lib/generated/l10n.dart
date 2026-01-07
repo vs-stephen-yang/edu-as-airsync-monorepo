ea880f72417dc026b1e030a1a3090fa4bb610e93
Ingrid Wang <ingrid.ys.wang@myviewboard.com>
2026-01-07 21:05:54 +0800

    chore: fix lbl for sender annotation

--- apps/sender/lib/generated/l10n.dart
+++ apps/sender/lib/generated/l10n.dart
@@ -2046,6 +2046,26 @@ class S {
     );
   }
 
+  /// `Start annotation`
+  String get v3_lbl_sharing_annotation_start {
+    return Intl.message(
+      'Start annotation',
+      name: 'v3_lbl_sharing_annotation_start',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Stop annotation`
+  String get v3_lbl_sharing_annotation_stop {
+    return Intl.message(
+      'Stop annotation',
+      name: 'v3_lbl_sharing_annotation_stop',
+      desc: '',
+      args: [],
+    );
+  }
+
   /// `Pause on`
   String get v3_lbl_sharing_pause_on {
     return Intl.message(
