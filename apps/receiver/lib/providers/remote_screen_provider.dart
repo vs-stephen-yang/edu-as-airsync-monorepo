0cb193f863db6a5a0a8e10a4b7d27e88d8f7f1a3
zoey.chen <zoey.chen@viewsonic.com>
2026-03-06 16:23:43 +0800

    feat: #102762 trace connection flow in remote_screen_provider.dart

--- apps/receiver/lib/providers/remote_screen_provider.dart
+++ apps/receiver/lib/providers/remote_screen_provider.dart
@@ -3,6 +3,7 @@ import 'package:display_flutter/model/multicast_presenter.dart';
 import 'package:display_flutter/model/remote_screen.dart';
 import 'package:display_flutter/model/remote_screen_connector.dart';
 import 'package:display_flutter/model/remote_screen_server.dart';
+import 'package:display_flutter/utility/log.dart';
 
 class RemoteScreenProvider {
   RemoteScreenServer get server => _server;
@@ -32,19 +33,38 @@ class RemoteScreenProvider {
     Channel channel,
     JoinDisplayMessage msg,
   ) async {
+    log.info(
+      'RemoteScreenProvider: Creating connector - '
+      'Type: $remoteScreenType, '
+      'ClientId: ${msg.clientId}, '
+      'SenderName: ${msg.name}',
+    );
+
     RemoteScreenConnector connector;
     switch (remoteScreenType) {
       case RemoteScreenType.rtc:
         connector = RtcScreenConnector(
             channel, _server.roomId, _ipAddress, _server.roomPort, msg);
         _server.addConnector(connector as RtcScreenConnector);
+        log.info(
+            'RemoteScreenProvider: RTC connector created and added to server - '
+            'RoomId: ${_server.roomId}, '
+            'Host: $_ipAddress:${_server.roomPort}, ');
         break;
       case RemoteScreenType.multicast:
         connector = MulticastScreenConnector(channel, msg);
+        log.info(
+          'RemoteScreenProvider: Multicast connector created',
+        );
         break;
     }
 
     connector.onDisconnect = (() async {
+      log.info(
+        'RemoteScreenProvider: Connector disconnect triggered - '
+        'ClientId: ${msg.clientId}, '
+        'SenderName: ${msg.name}',
+      );
       _connectorDisconnectCallback(
         fromSender: true,
         remoteScreenConnector: connector,
@@ -52,10 +72,17 @@ class RemoteScreenProvider {
       );
     });
 
+    log.info('RemoteScreenProvider: Connector creation completed');
     return connector;
   }
 
   addConnector(RemoteScreenConnector connector) {
+    log.info(
+      'RemoteScreenProvider: Adding RTC connector - '
+      'Type: $remoteScreenType, '
+      'ClientId: ${connector.clientId}, '
+      'SessionId: ${connector.sessionId}',
+    );
     if (remoteScreenType != RemoteScreenType.rtc) {
       return;
     }
@@ -65,6 +92,12 @@ class RemoteScreenProvider {
   }
 
   removeConnector(RemoteScreenConnector connector) {
+    log.info(
+      'RemoteScreenProvider: Removing RTC connector - '
+      'Type: $remoteScreenType, '
+      'ClientId: ${connector.clientId}, '
+      'SessionId: ${connector.sessionId}',
+    );
     if (remoteScreenType != RemoteScreenType.rtc) {
       return;
     }
@@ -78,6 +111,10 @@ class RemoteScreenProvider {
   }
 
   Future startSfuServer(List<RtcIceServer>? iceServers) {
+    log.info(
+      'RemoteScreenProvider: Starting SFU server - '
+      'ICE servers count: ${iceServers?.length ?? 0}',
+    );
     return _server.startSfuServer(iceServers);
   }
 
@@ -86,6 +123,9 @@ class RemoteScreenProvider {
   }
 
   Future<void> stopPublish() async {
+    log.info(
+      'RemoteScreenProvider: Stopping publish - Type: $remoteScreenType',
+    );
     switch (remoteScreenType) {
       case RemoteScreenType.rtc:
         return await _server.stopRemoteScreenPublisher();
@@ -99,36 +139,72 @@ class RemoteScreenProvider {
     StartRemoteScreenMessage message,
     List<RtcIceServer>? iceServers,
   ) async {
+    log.info(
+      'RemoteScreenProvider: Starting remote screen - '
+      'Type: $remoteScreenType, '
+      'SessionId: ${message.sessionId}, '
+      'ClientId: ${connector.clientId}, '
+      'ICE servers count: ${iceServers?.length ?? 0}',
+    );
+
     switch (remoteScreenType) {
       case RemoteScreenType.rtc:
         RtcScreenConnector c = connector as RtcScreenConnector;
         c.onStartRemoteScreen(message, iceServers);
+        log.info(
+          'RemoteScreenProvider: RTC remote screen started - '
+          'SessionId: ${message.sessionId}',
+        );
         break;
       case RemoteScreenType.multicast:
         final streamInfo = await _multicastPresenter.streamInfo;
         if (streamInfo != null) {
           MulticastScreenConnector c = connector as MulticastScreenConnector;
           c.onStartRemoteScreen(message, streamInfo);
+          log.info(
+            'RemoteScreenProvider: Multicast remote screen started - '
+            'SessionId: ${message.sessionId}',
+          );
+        } else {
+          log.warning(
+            'RemoteScreenProvider: Failed to get multicast stream info - '
+            'SessionId: ${message.sessionId}',
+          );
         }
         break;
     }
   }
 
   Future<bool> startPublish(List<RtcIceServer>? iceServers) async {
+    log.info(
+      'Starting publish - Type: $remoteScreenType, ICE servers: ${iceServers?.length ?? 0}',
+    );
+
     switch (remoteScreenType) {
       case RemoteScreenType.rtc:
         await startSfuServer(iceServers);
-        return await startRemoteScreenPublisher();
+        final result = await startRemoteScreenPublisher();
+        log.info('RemoteScreenProvider: RTC publish result - Success: $result');
+        return result;
+
       case RemoteScreenType.multicast:
-        return await _multicastPresenter.start();
+        final result = await _multicastPresenter.start();
+        log.info(
+            'RemoteScreenProvider: Multicast publish result - Success: $result');
+        return result;
     }
   }
 
   void recreatePublisher() {
+    log.info(
+      'RemoteScreenProvider: Recreating publisher - '
+      'Type: $remoteScreenType',
+    );
     switch (remoteScreenType) {
       case RemoteScreenType.rtc:
         return _server.userConfirmRecreate();
       case RemoteScreenType.multicast:
+        log.info('RemoteScreenProvider: Recreate not supported for Multicast');
         return;
     }
   }
