7e48b5f30adc7bc3878a5c4cb626b0b2d9cce1b5
Ken Chung <ken.mh.chung@tw.viewsonic.com>
2026-03-17 13:59:55 +0800

    fix: [BUG #101588] [Receiver] Size=1.5以上，Cast to boards頁面靜置3秒，大寫C、ID數字會被切一點

--- apps/receiver/lib/widgets/v3_auto_hyphenating_text.dart
+++ apps/receiver/lib/widgets/v3_auto_hyphenating_text.dart
@@ -6,6 +6,7 @@ class V3AutoHyphenatingText extends StatelessWidget {
   final String? semanticsLabel;
   final TextStyle? style;
   final TextAlign? textAlign;
+  final Color? backgroundColor;
 
   const V3AutoHyphenatingText(
     this.text, {
@@ -13,6 +14,7 @@ class V3AutoHyphenatingText extends StatelessWidget {
     this.style,
     this.textAlign,
     this.semanticsLabel,
+    this.backgroundColor,
   });
 
   @override
@@ -27,11 +29,14 @@ class V3AutoHyphenatingText extends StatelessWidget {
     // 2. For WCAG 1.1.1: Non-text Content + 1.3.1: Info and Relationships
     //    - Truncated text may fail to convey complete information, which is a barrier for both visual and non-visual users.
     // 3. If visual truncation is still required, use a Tooltip or Semantics label to provide the full text information.
-    return AutoHyphenatingText(
-      text,
-      style: style,
-      textAlign: textAlign,
-      semanticsLabel: semanticsLabel,
+    return Container(
+      color: backgroundColor,
+      child: AutoHyphenatingText(
+        text,
+        style: style,
+        textAlign: textAlign,
+        semanticsLabel: semanticsLabel,
+      ),
     );
   }
 }
